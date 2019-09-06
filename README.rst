00ccf5f8f8ff9e7f27db84617dd56e3a13e80844
Nathan <nathan.leniz@10gen.com>
2019-09-06 10:20:06 -0400

    initial commit

---
+++ README.rst
@@ -0,0 +1,58 @@
+===========================
+MongoDB Realm Documentation
+===========================
+
+This repository contains documentation regarding components of
+MongoDB Realm .
+
+This documentation builds on the work of the `MongoDB
+Manual <http://docs.mongodb.org/manual/>`. You can download and build
+this documentation locally if you already have `Sphinx
+<http://sphinx.pocoo.org/>`_ installed, with the following command: ::
+
+     pip install giza
+     git clone git://github.com/mongodb/docs-ecosystem
+     cd docs-ecosystem/
+     make html
+
+To build on Windows (using Cygwin):
+
+- Ensure you have python installed
+- install Sphinx using ``easy_install -U Sphinx``
+- install pyyamml using ``pip install pyyaml``
+- Build the docs using ``sphinx-build  -b html -d build/doctrees-html -c ./ source build/html``
+
+You may also wish to install `Pygments
+<http://pygments.org>`_ to provide syntax highlighting for code
+examples.
+
+Contribute
+----------
+
+The MongoDB Documentation Project is governed by the terms of the
+`MongoDB Contributor Agreement
+<https://www.mongodb.com/legal/contributor-agreement>`_.
+
+To contribute to the documentation,
+
+- If you have not done so already, please sign the `MongoDB Contributor Agreement <https://www.mongodb.com/legal/contributor-agreement>`_.
+
+- Fork this repository on GitHub and issue a pull request.
+
+
+Report Issues
+-------------
+
+File issue reports or requests at the `Documentation Jira Project
+<https://jira.mongodb.org/browse/DOCS>`_.
+
+License
+-------
+
+All documentation is available under the terms of a `Creative Commons
+License <http://creativecommons.org/licenses/by-nc-sa/3.0/>`_.
+
+If you have any questions, please contact `docs@10gen.com
+<mailto:docs@10gen.com>`_.
+
+-- The MongoDB/10gen Docs Team
---
+++ conf.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+#
+# MongoDB documentation build configuration file, created by
+# sphinx-quickstart on Mon Oct  3 09:58:40 2011.
+#
+# This file is execfile()d with the current directory set to its containing dir.
+
+import sys
+import os.path
+import datetime
+
+project_root = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(project_root)
+
+from giza.config.runtime import RuntimeStateConfig
+from giza.config.helper import fetch_config, get_versions, get_manual_path
+
+conf = fetch_config(RuntimeStateConfig())
+intersphinx_libs = conf.system.files.data.intersphinx
+sconf = conf.system.files.data.sphinx_local
+
+sys.path.append(os.path.join(conf.paths.projectroot, conf.paths.buildsystem, 'sphinxext'))
+
+# -- General configuration ----------------------------------------------------
+
+needs_sphinx = '1.0'
+
+extensions = [
+    'sphinx.ext.extlinks',
+    'sphinx.ext.todo',
+    'mongodb',
+    'directives',
+    'intermanual',
+    'fasthtml'
+]
+
+templates_path = ['.templates']
+exclude_patterns = []
+
+source_suffix = '.txt'
+
+master_doc = sconf.master_doc
+language = 'en'
+project = sconf.project
+copyright = u'2008-{0}'.format(datetime.date.today().year)
+
+version = '3.0'
+release = version
+
+rst_epilog = '\n'.join([
+    '.. |branch| replace:: ``{0}``'.format(conf.git.branches.current),
+    '.. |copy| unicode:: U+000A9',
+    '.. |year| replace:: {0}'.format(datetime.date.today().year),
+    '.. |ent-build| replace:: MongoDB Enterprise',
+    '.. |hardlink| replace:: http://docs.mongodb.com/realm/',
+    '.. |atlas-full| replace:: MongoDB Atlas',
+    '.. |atlas| replace:: Atlas'
+])
+
+extlinks = {
+    'issue': ('https://jira.mongodb.org/browse/%s', '' ),
+    'api': ('http://api.mongodb.com/%s', ''),
+    'manual': ('http://docs.mongodb.org/manual%s', ''),
+    'gettingstarted': ('http://docs.mongodb.org/getting-started%s', ''),
+    'atlas': ('https://docs.atlas.mongodb.com%s',''),
+    'mms-docs': ('https://docs.cloud.mongodb.com%s', ''),
+    'mms-home': ('https://cloud.mongodb.com%s', ''),
+    'guides': ('https://docs.mongodb.com/guides%s', ''),
+    'java-docs-latest': ('http://mongodb.github.io/mongo-java-driver/3.11/%s', ''),
+}
+
+intersphinx_mapping = {}
+
+try:
+    for i in intersphinx_libs:
+        intersphinx_mapping[i['name']] = ( i['url'], os.path.join(conf.paths.projectroot,
+                                                              conf.paths.output,
+                                                              i['path']))
+except:
+    for i in intersphinx_libs:
+        intersphinx_mapping[i.name] = ( i.url, os.path.join(conf.paths.projectroot,
+                                                              conf.paths.output,
+                                                              i.path))
+
+
+languages = [
+    ("ar", "Arabic"),
+    ("cn", "Chinese"),
+    ("cs", "Czech"),
+    ("de", "German"),
+    ("es", "Spanish"),
+    ("fr", "French"),
+    ("hu", "Hungarian"),
+    ("id", "Indonesian"),
+    ("it", "Italian"),
+    ("jp", "Japanese"),
+    ("ko", "Korean"),
+    ("lt", "Lithuanian"),
+    ("pl", "Polish"),
+    ("pt", "Portuguese"),
+    ("ro", "Romanian"),
+    ("ru", "Russian"),
+    ("tr", "Turkish"),
+    ("uk", "Ukrainian")
+]
+
+# -- Options for HTML output ---------------------------------------------------
+
+html_theme = sconf.theme.name
+html_theme_path = [ os.path.join(conf.paths.buildsystem, 'themes') ]
+html_title = conf.project.title
+htmlhelp_basename = 'MongoDBdoc'
+
+html_logo = ".static/logo-mongodb.png"
+html_static_path = ['source/.static']
+html_last_updated_fmt = '%b %d, %Y'
+
+html_copy_source = False
+html_domain_indices = True
+html_use_index = True
+html_split_index = False
+html_show_sourcelink = False
+html_show_sphinx = True
+html_show_copyright = True
+
+manual_edition_path = '{0}/{1}/{2}.{3}'
+
+html_theme_options = {
+    'branch': conf.git.branches.current,
+    'translations': languages,
+    'language': language,
+    'manual_path': "ecosystem",
+    'repo_name': 'docs-ecosystem',
+    'jira_project': 'DOCS',
+    'google_analytics': sconf.theme.google_analytics,
+    'project': sconf.project,
+    'nav_excluded': sconf.theme.nav_excluded,
+}
+
+html_sidebars = sconf.sidebars
