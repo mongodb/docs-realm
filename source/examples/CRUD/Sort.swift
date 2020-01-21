9cd85b697da5a58df7f4740a2dc8261542425d4e
Chris Bush <chrbsh@gmail.com>
2020-01-21 14:38:11 -0500

    (DOCSP-7078): Add support files page (#19)
    * Fix swift code error
    
    * (DOCSP-7078): Add support files page
    
    - Added under Data Model section, but that may easily change
    - Still lacking some info and this may change, so this is a
    quick surface-level attempt to get something in place.
    - The client reset section may no longer be relevant,
    and will most likely move to another page about troubleshooting.
    
    * Update source/data-model/support-files.txt
    
    Co-Authored-By: Melissa Mahoney <melissa.mahoney@mongodb.com>
    
    * Update source/data-model/support-files.txt
    
    Co-Authored-By: Melissa Mahoney <melissa.mahoney@mongodb.com>
    
    * Update source/data-model/support-files.txt
    
    Co-Authored-By: Melissa Mahoney <melissa.mahoney@mongodb.com>
    
    * Update source/data-model/support-files.txt
    
    Co-Authored-By: Melissa Mahoney <melissa.mahoney@mongodb.com>
    
    * Update source/data-model/support-files.txt
    
    Co-Authored-By: Melissa Mahoney <melissa.mahoney@mongodb.com>
    
    * Addressing comments
    
    * Update source/data-model/auxiliary-files.txt
    
    Co-Authored-By: Melissa Mahoney <melissa.mahoney@mongodb.com>
    
    Co-authored-by: Melissa Mahoney <melissa.mahoney@mongodb.com>

--- source/examples/CRUD/Sort.swift
+++ source/examples/CRUD/Sort.swift
@@ -2,5 +2,5 @@ let projectsSorted = projects.sorted(byKeyPath: "name", ascending: false)
 
 // You can also sort on the members of linked objects. In this example,
 // we sort the dogs by dog's owner's name.
-let dogs = realm.where<Dog>()
+let dogs = realm.objects(Dog.self)
 let ownersByName = dogs.sorted(byKeyPath: "owner.name")
