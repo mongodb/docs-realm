68c25d469af77b7026ccaa0d0c1aad1056355c8f
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-02-11 14:14:01 -0500

    changed updateMode to modified

--- source/examples/CRUD/Upsert.js
+++ source/examples/CRUD/Upsert.js
@@ -1,13 +1,13 @@
 realm.write(() => {
   // Add a new person to the realm. Since nobody with ID 1234
   // has been added yet, this adds the instance to the realm.
-  const drew = realm.create("Person", { id: 1234, name: "Drew" }, "updateMode");
+  const drew = realm.create("Person", { id: 1234, name: "Drew" }, "modified");
 
   // Judging by the ID, it's the same person, just with a different name.
 
   // If an object exists, setting the third parameter (`update`) to
-  // "updateMode", will only update properties where the value has
+  // "modified", will only update properties where the value has
   // changed, decreasing notifications and improving server-side
   // performance
-  const andy = realm.create("Person", { id: 1234, name: "Andy" }, "updateMode");
+  const andy = realm.create("Person", { id: 1234, name: "Andy" }, "modified");
 });
