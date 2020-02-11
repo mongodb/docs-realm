7206b24b47dd7976bd9ddb3bd85231a8fe92c280
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-02-11 13:32:26 -0500

    (DOCSP-8702): added comment and code about 'update'='updateMode' on JS Upsert

--- source/examples/CRUD/Upsert.js
+++ source/examples/CRUD/Upsert.js
@@ -1,10 +1,13 @@
 realm.write(() => {
   // Add a new person to the realm. Since nobody with ID 1234
   // has been added yet, this adds the instance to the realm.
-  const drew = realm.create("Person", { id: 1234, name: "Drew" }, true);
+  const drew = realm.create("Person", { id: 1234, name: "Drew" }, "updateMode");
 
   // Judging by the ID, it's the same person, just with a different name.
-  // When the third parameter (`update`) is true, you overwrite the original
-  // entry (i.e. Drew -> Andy).
-  const andy = realm.create("Person", { id: 1234, name: "Andy" }, true);
+
+  // If an object exists, setting the third parameter (`update`) to
+  // "updateMode", will only update properties where the value has
+  // changed, decreasing notifications and improving server-side
+  // performance
+  const andy = realm.create("Person", { id: 1234, name: "Andy" }, "updateMode");
 });
