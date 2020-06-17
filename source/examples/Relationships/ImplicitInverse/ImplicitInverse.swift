23f921b64712ec7798b5cbb878aa723b00ab029b
Chris Bush <chris.bush@mongodb.com>
2020-06-17 14:16:36 -0400

    (DOCSP-10824): Fix implicit inverse code example

--- source/examples/Relationships/ImplicitInverse/ImplicitInverse.swift
+++ source/examples/Relationships/ImplicitInverse/ImplicitInverse.swift
@@ -1,12 +1,12 @@
 class Person: Object {
-    dynamic var name: String = ""
-    dynamic var birthdate: Date = Date(timeIntervalSince1970: 1)
+    @objc dynamic var name: String = ""
+    @objc dynamic var birthdate: Date = Date(timeIntervalSince1970: 1)
     let dogs = List<Dog>()
 }
 
 class Dog: Object {
-    dynamic var name: String = ""
-    dynamic var age: Int = 0
-    dynamic var breed: String? = nil
-    dynamic var owners: LinkingObjects(fromType: Person.self, property: "dogs")
+    @objc dynamic var name: String = ""
+    @objc dynamic var age: Int = 0
+    @objc dynamic var breed: String? = nil
+    let owners = LinkingObjects(fromType: Person.self, property: "dogs")
 }
