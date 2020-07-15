7c4f2baf1bf862a2f640bcc016424a61580503f0
nate contino <nathan.contino@mongodb.com>
2020-07-15 09:51:20 -0600

    Fix AddUser kotlin example to use valid kotlin

--- source/examples/MultiUser/AddUser/AddUser.kt
+++ source/examples/MultiUser/AddUser/AddUser.kt
@@ -1,25 +1,31 @@
-val app: App = new App(new AppConfiguration.Builder("myapp-abcde").build())
+val app: App = App(AppConfiguration.Builder("<your app ID>").build())
 
 // Log in as Joe
 val joeCredentials: Credentials = Credentials.emailPassword("joe@example.com", "passw0rd")
 app.loginAsync(joeCredentials) {
-  if (it.isSuccess()) {
-    // The active user is now Joe
-    val joe: User = it.get();
-    assert joe == app.currentUser();
+  if (it.isSuccess) {
+    val joe: User = it.get()
+    if (app.currentUser().email == joe.email) {
+      Log.v(TAG,"Current user is joe")
+    } else {
+      Log.e(TAG, "Current user is not joe")
+    }
   } else {
-    Log.e(TAG, it.getError().toString());
+    Log.e(TAG, it.error));
   }
 }
 
 // Log in as Emma
 val emmaCredentials: Credentials = Credentials.emailPassword("emma@example.com", "pa55word")
 app.loginAsync(emmaCredentials) {
-  if (it.isSuccess()) {
-    // The active user is now Emma
-    val emma: User = it.get();
-    assert emma == app.currentUser();
+  if (it.isSuccess) {
+    val emma: User = it.get()
+    if (app.currentUser().email == emma.email) {
+      Log.v(TAG,"Current user is emma")
+    } else {
+      Log.e(TAG, "Current user is not emma")
+    }
   } else {
-    Log.e(TAG, it.getError().toString());
+    Log.e(TAG, it.error);
   }
 }
