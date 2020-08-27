272fb740cc738f3ae2a4395eb5347965b4bf3273
Chris Bush <chrbsh@gmail.com>
2020-08-27 18:28:46 -0400

    Update source/examples/EmbeddedObjects/ReplaceEmbeddedObject.m
    Co-authored-by: Thomas Goyne <tgoyne@gmail.com>

--- source/examples/EmbeddedObjects/ReplaceEmbeddedObject.m
+++ source/examples/EmbeddedObjects/ReplaceEmbeddedObject.m
@@ -1,9 +1,7 @@
 RLMRealm *realm = [RLMRealm defaultRealm];
 [realm transactionWithBlock: ^{
-    Contact *contact = [Contact
-        objectInRealm:realm
-        forPrimaryKey:[[RLMObjectId alloc] initWithString:@"5f481c21f634a1f4eeaa7268" error:nil]
-    ]; 
+    Contact *contact = [Contact objectInRealm:realm
+                                forPrimaryKey:[[RLMObjectId alloc] initWithString:@"5f481c21f634a1f4eeaa7268" error:nil]];
     Address *newAddress = [[Address alloc] init];
     newAddress.street = @"Hollywood Upstairs Medical College";
     newAddress.city = @"Los Angeles";
