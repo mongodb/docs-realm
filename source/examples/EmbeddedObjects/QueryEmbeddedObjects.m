5f6e26c97a85b6a5df220320eae232bc8d96c5c5
Chris Bush <chrbsh@gmail.com>
2020-08-27 18:27:51 -0400

    Update source/examples/EmbeddedObjects/QueryEmbeddedObjects.m
    Co-authored-by: Thomas Goyne <tgoyne@gmail.com>

--- source/examples/EmbeddedObjects/QueryEmbeddedObjects.m
+++ source/examples/EmbeddedObjects/QueryEmbeddedObjects.m
@@ -1,9 +1,5 @@
 RLMRealm *realm = [RLMRealm defaultRealm];
-RLMResults<Contact *> *losAngelesContacts = [Contact
-    objectsInRealm:realm
-    withPredicate:
-        [NSPredicate predicateWithFormat:@"address.city = %@" argumentArray:@[@"Los Angeles"]]
-];
+RLMResults<Contact *> *losAngelesContacts = [Contact objectsInRealm:realm where:@"address.city = %@", @"Los Angeles"];
 
 losAngelesContacts = [losAngelesContacts sortedResultsUsingKeyPath:@"address.street" ascending:YES]; 
 NSLog(@"Los Angeles Contacts: %@", losAngelesContacts);
