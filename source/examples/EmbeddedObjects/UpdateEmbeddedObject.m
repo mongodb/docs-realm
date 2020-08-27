3fb2a38506d3fb03ce166b4142f905be99ceb31f
Chris Bush <chrbsh@gmail.com>
2020-08-27 18:29:12 -0400

    Update source/examples/EmbeddedObjects/UpdateEmbeddedObject.m
    Co-authored-by: Thomas Goyne <tgoyne@gmail.com>

--- source/examples/EmbeddedObjects/UpdateEmbeddedObject.m
+++ source/examples/EmbeddedObjects/UpdateEmbeddedObject.m
@@ -1,9 +1,7 @@
 RLMRealm *realm = [RLMRealm defaultRealm];
 [realm transactionWithBlock: ^{
-    Contact *contact = [Contact
-        objectInRealm:realm
-        forPrimaryKey:[[RLMObjectId alloc] initWithString:@"5f481c21f634a1f4eeaa7268" error:nil]
-    ]; 
+    Contact *contact = [Contact objectInRealm:realm
+                                forPrimaryKey:[[RLMObjectId alloc] initWithString:@"5f481c21f634a1f4eeaa7268" error:nil]];
     contact.address.street = @"Hollywood Upstairs Medical College";
     contact.address.city = @"Los Angeles";
     contact.address.postalCode = @"90210";
