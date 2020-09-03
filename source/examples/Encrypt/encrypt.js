3964e776d780c97f1decba82b633077f6d587665
Chris Bush <chris.bush@mongodb.com>
2020-09-03 14:20:38 -0400

    Copy iOS to other Client Guides

--- source/examples/Encrypt/encrypt.js
+++ source/examples/Encrypt/encrypt.js
@@ -1,6 +1,10 @@
-const key = new Int8Array(64); // pupulate with a secure key
+// Retrieve key from secure location or create one...
+const key = new Int8Array(64); // Populate with a secure key
+// ... store key ...
+
+// Use key to open realm
 Realm.open({ schema: [DogSchema], encryptionKey: key }).then((realm) => {
-  // Use the Realm as normal
+  // Use the realm as normal
   const dogs = realm.objects("Dog");
 
   // ...
