cac558a08af7ab7892a81bf0fead194f2bc396dc
nathan-contino-mongo <nathan.contino@mongodb.com>
2020-09-10 16:59:23 -0600

    (DOCSP-11836): Embedded objects, android

---
+++ source/examples/EmbeddedObjects/ReplaceEmbeddedObject.java
@@ -0,0 +1,14 @@
+Contact oldContact = realm.where(Contact.class).findFirst();
+
+realm.executeTransaction(transactionRealm -> {
+    Address newAddress = new Address(
+        "Hollywood Upstairs Medical College",
+        "Los Angeles",
+        "USA"
+        "90210"
+        );
+    oldContact.address = newAddress;
+    Log.v("EXAMPLE", "Replaced contact: " + oldContact);
+});
+
+realm.close();
---
+++ source/examples/EmbeddedObjects/UpdateEmbeddedObject.java
@@ -0,0 +1,10 @@
+Contact resultContact = realm.where(Contact.class).findFirst();
+
+realm.executeTransaction(transactionRealm -> {
+    resultContact.address.street = "Hollywood Upstairs Medical College";
+    resultContact.address.city = "Los Angeles";
+    resultContact.address.postalCode = "90210";
+    Log.v("EXAMPLE", "Updated contact: " + resultContact);
+});
+
+realm.close();
