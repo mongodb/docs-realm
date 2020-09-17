1e08ad97f9760e5a8e5574a1f9cfe43afa5504a9
Chris Bush <chris.bush@mongodb.com>
2020-09-17 19:28:07 -0400

    Update snippets for indentation

--- source/examples/generated/code/start/ManageEmailPasswordUsers.codeblock.confirm-new-user-email-objc.m
+++ source/examples/generated/code/start/ManageEmailPasswordUsers.codeblock.confirm-new-user-email-objc.m
@@ -1,16 +1,16 @@
-    RLMApp *app = [RLMApp appWithId:YOUR_REALM_APP_ID];
-    RLMEmailPasswordAuth *client = [app emailPasswordAuth];
+RLMApp *app = [RLMApp appWithId:YOUR_REALM_APP_ID];
+RLMEmailPasswordAuth *client = [app emailPasswordAuth];
 
-    // Token and tokenId are query parameters in the confirmation
-    // link sent in the confirmation email.
-    NSString *token = @"someToken";
-    NSString *tokenId = @"someTokenId";
+// Token and tokenId are query parameters in the confirmation
+// link sent in the confirmation email.
+NSString *token = @"someToken";
+NSString *tokenId = @"someTokenId";
 
-    [client confirmUser:token tokenId:tokenId completion:^(NSError *error) {
-       if (error != nil) {
-           NSLog(@"User confirmation failed: %@", [error localizedDescription]);
-           return;
-       }
-       // User confirmed
-       NSLog(@"Successfully confirmed user.");
-    }];
+[client confirmUser:token tokenId:tokenId completion:^(NSError *error) {
+   if (error != nil) {
+       NSLog(@"User confirmation failed: %@", [error localizedDescription]);
+       return;
+   }
+   // User confirmed
+   NSLog(@"Successfully confirmed user.");
+}];
