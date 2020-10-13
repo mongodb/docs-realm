7e21e9c742ef4a8c4ed745173ad424f120472acf
Nick Larew <nick.larew@mongodb.com>
2020-10-12 22:09:30 -0400

    (DOCSP-12110): Add tested Node authentication examples (ts)

---
+++ source/examples/generated/code/start/authenticate.codeblock.anonymous-login.ts
@@ -0,0 +1,9 @@
+// Create an anonymous credential
+const credentials = Realm.Credentials.anonymous();
+try {
+  const user: Realm.User = await app.logIn(credentials);
+  console.log("Successfully logged in!", user.id);
+  return user;
+} catch(err) {
+  console.error("Failed to log in", err.message);
+}
