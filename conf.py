b055cf39d70c98b20ca75fbfb23f09f4db531fdc
Chris Bush <chrbsh@gmail.com>
2020-10-14 15:53:18 -0400

    Dotnet encrypt (#544)
    * Update README and restore unit test target in solution
    
    * (DOCSP-12481): Add C#/.NET encryption example
    
    * Update source/dotnet/encrypt.txt
    
    Co-authored-by: Nikola Irinchev <irinchev@me.com>
    
    * Update source/dotnet/encrypt.txt
    
    Co-authored-by: Nikola Irinchev <irinchev@me.com>
    
    * Address comments
    
    Co-authored-by: Nikola Irinchev <irinchev@me.com>

--- conf.py
+++ conf.py
@@ -139,6 +139,8 @@ extlinks = {
     'typescript': ('https://www.typescriptlang.org/%s', ''),
     'node-driver': ('https://mongodb.github.io/node-mongodb-native/3.6/api/%s', ''),
     'rfc': ('https://tools.ietf.org/html/%s', ''),
+    'xamarin': ('https://docs.microsoft.com/en-us/xamarin/%s', ''),
+    
 }
 
 intersphinx_mapping = {}
