06236c7b8f9caa12c2e282716f485a836d3db39e
mongocaleb <caleb.thompson@mongodb.com>
2020-10-16 11:46:05 -0700

    add additional example

--- examples/dotnet/Examples/Examples.cs
+++ examples/dotnet/Examples/Examples.cs
@@ -257,18 +257,31 @@ namespace UnitTests
         [Test]
         public async System.Threading.Tasks.Task LinksAUser()
         {
-            // :code-block-start: link
-            // 1) A user logs on anonymously:
-            var anonUser = await app.LogInAsync(Credentials.Anonymous());
-            // 2) They create some data, and then decide they want to save
-            //    it, which requires creating an Email/Password account.
-            // 3) We prompt the user to log in, and then use that new
-            //    EmailPassword credential to link the existing anonymous
-            //    account:
-            var officialUser = await anonUser.LinkCredentialsAsync(
-                Credentials.EmailPassword("caleb@example.com","shhhItsASektrit!"));
-            // :code-block-end:
-            
+            {
+                // :code-block-start: link
+                // 1) A user logs on anonymously:
+                var anonUser = await app.LogInAsync(Credentials.Anonymous());
+                // 2) They create some data, and then decide they want to save
+                //    it, which requires creating an Email/Password account.
+                // 3) We prompt the user to log in, and then use that info to
+                //    register the new EmailPassword user, and then generate an
+                //    EmailPassword credential to link the existing anonymous
+                //    account:
+                var email = "caleb@example.com";
+                var password = "shhhItsASektrit!";
+                await app.EmailPasswordAuth.RegisterUserAsync(
+                    email, password);
+                var officialUser = await anonUser.LinkCredentialsAsync(
+                   Credentials.EmailPassword(email, password));
+                // :code-block-end:
+            }
+            {
+                // :code-block-start: link2
+                var anonUser = await app.LogInAsync(Credentials.Anonymous());
+                var officialUser = await anonUser.LinkCredentialsAsync(
+                   Credentials.Google("<google-token>"));
+                // :code-block-end:
+            }
             return;
         }
 
