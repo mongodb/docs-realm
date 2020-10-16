10974dfa476ec4d9a248c44dac7caa215ce89e23
Mohammad Chughtai <mohammad.hunan@Mohammads-MacBook-Pro.local>
2020-10-16 19:00:08 -0400

    (DOCSP-12479): added EmbeddedExamples test file

---
+++ examples/dotnet/Examples/Business.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using Realms;
+using Realms.Sync;
+
+namespace Examples
+{
+    public class Business : RealmObject
+    {
+        [PrimaryKey]
+        [MapTo("_id")]
+        public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+        public string Name { get; set; }
+        public IList<Address> addresses { get; }
+    }
+}
+
---
+++ examples/dotnet/Examples/EmbeddedExamples.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using NUnit.Framework;
+using Realms;
+using Realms.Sync;
+
+namespace Examples
+{
+    public class EmbeddedExamples
+    {
+        App app;
+        User user;
+        SyncConfiguration config;
+        const string myRealmAppId = "tuts-tijya";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            app = App.Create(myRealmAppId);
+            user = app.LogInAsync(Credentials.EmailPassword("foo@foo.com", "foobar")).Result;
+            config = new SyncConfiguration("myPartition", user);
+
+            // Synchronous here because setup and tear down don't support async
+            var realm = Realm.GetInstance(config);
+
+            // :code-block-start:create
+
+
+
+
+        }
+        [Test]
+        public async Task UpdateEmbeddedObject()
+        {
+            var realm = await Realm.GetInstanceAsync(config);
+
+
+
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
