5d3148a93c6e67c45d21994151ea815fe273bf72
mongocaleb <caleb.thompson@mongodb.com>
2020-10-16 08:26:47 -0700

    more rewrites for accuracy

--- source/examples/generated/code/start/ClientResetExamples.codeblock.handle.cs
+++ source/examples/generated/code/start/ClientResetExamples.codeblock.handle.cs
@@ -4,11 +4,27 @@ Session.Error += (sender, err) =>
     {
         var session = (Session)sender;
         Console.WriteLine($"Client Reset requested for {session.Path} due to {clientResetEx.Message}");
-        // Notify user that they will need to re-download the Realm.
-        // InitiateClientReset will immediately reset the Realm and download the
-        // server copy. If you don't call it, the Realm will be
-        // reset the next time the app starts.
-        var resetResult = clientResetEx.InitiateClientReset();
-        Console.WriteLine(resetResult ? "Success" : "Reset failed");
+
+        // Prompt user to perform client reset immediately. If they don't do it,
+        // they won't receive any data from the server until they restart the app
+        // and all changes they make will be discarded when the app restarts.
+        var didUserConfirmReset = true;
+        if (didUserConfirmReset)
+        {
+            // Close the Realm before doing the reset as it'll need
+            // to be deleted and all objects obtained from it will be
+            // invalidated.
+            realm.Dispose();
+            var didReset = clientResetEx.InitiateClientReset();
+            if (didReset)
+            {
+                // Navigate the user back to the main page or reopen the
+                // the Realm and reinitialize the current page.
+            }
+            else
+            {
+                // Reset failed - notify user that they'll need to restart the app.
+            }
+        }
     }
 };
