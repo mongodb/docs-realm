0e3993116b9cf7a722fedaa72c27cf28ce3cd55a
Chris Bush <chris.bush@mongodb.com>
2020-10-22 17:26:22 -0400

    Additional refinement
    - Adds a logout button to keep the AppState out of child views that don't really need it
    - Changes item to plain var (instead of @ObservedObject) in leaf views as they should not update upon change. This was causing a crash upon delete of an edited item.
    - AppState does not retain the realm, but only keeps the group's items
    - Solidifying the shouldIndicateActivity logic and ensuring no unwanted activity indicators

--- examples/ios/QuickStartSwiftUI/QuickStart.swift
+++ examples/ios/QuickStartSwiftUI/QuickStart.swift
@@ -5,7 +5,7 @@ import RealmSwift
 import Combine
 import SwiftUI
 
-// --- MODELS ---
+// MARK: Models
 
 /// Random adjectives for more interesting demo item names
 let randomAdjectives = [
@@ -22,9 +22,8 @@ let randomNouns = [
     "cork", "mouse pad"
 ]
 
-// MARK: Item
 /// An individual item. Part of a `Group`.
-final class Item: Object, ObjectKeyIdentifiable {    
+final class Item: Object, ObjectKeyIdentifiable {
     /// The unique ID of the Item.
     @objc dynamic var _id = ObjectId.generate()
 
@@ -40,12 +39,11 @@ final class Item: Object, ObjectKeyIdentifiable {
     }
 }
 
-// MARK: Group
 /// Represents a collection of items.
 final class Group: Object, ObjectKeyIdentifiable {
     /// The unique ID of the Group.
     @objc dynamic var _id = ObjectId.generate()
-    
+
     /// The collection of Items in this group.
     let items = RealmSwift.List<Item>()
 
@@ -56,7 +54,7 @@ final class Group: Object, ObjectKeyIdentifiable {
 }
 
 
-// --- MANAGERS / VIEWMODELS ---
+// MARK: State Objects
 
 /// The Realm app.
 let app = App(id: YOUR_REALM_APP_ID_HERE)
@@ -64,8 +62,6 @@ let app = App(id: YOUR_REALM_APP_ID_HERE)
 /// State object for managing App flow.
 /// As applications grow, this object may have to be broken out further.
 class AppState: ObservableObject {
-    /// The app's realm. Asynchronously set on login and unset on logout.
-    var realm: Realm?
     /// Publisher that monitors log in state.
     @Published var loginPublisher = PassthroughSubject<User, Error>()
     /// Publisher that monitors log out state.
@@ -74,83 +70,76 @@ class AppState: ObservableObject {
     @Published var shouldIndicateActivity = false
     /// Cancellables to be retained for any Future.
     @Published var cancellables = Set<AnyCancellable>()
+    /// The list of items in the first group in the realm that will be displayed to the user.
+    @Published var items: RealmSwift.List<Item>?
 
     init() {
-        // if we have a current user, check if a Realm already exists
-        // for the given configuration. if not, asynchronously open
-        // one. else, assign the existing one
+        // If we have a current user, check if a Realm already exists
+        // for the given configuration.
         if let user = app.currentUser {
-            let configuration = user.configuration(partitionValue: user.id)
-            if Realm.fileExists(for: configuration) {
-                self.realm = try! Realm(configuration: configuration)
-            } else {
-                shouldIndicateActivity = true
-                openRealm(user: user)
-            }
+            openRealm(user: user)
         }
 
         // monitor log in state and open a realm on login
         loginPublisher.receive(on: DispatchQueue.main).sink(
             receiveCompletion: { _ in },
             receiveValue: { user in
-                self.shouldIndicateActivity = true
                 self.openRealm(user: user)
             }
-        ).store(in: &cancellables)
+        ).store(in: &cancellables);
 
         // monitor log out state and unset the realm on log out
-        logoutPublisher.receive(on: DispatchQueue.main).sink(
-            receiveCompletion: { _ in },
-            receiveValue: { user in
-                self.realm = nil
-            }
-        ).store(in: &cancellables)
+        logoutPublisher.receive(on: DispatchQueue.main).sink(receiveCompletion: { _ in }, receiveValue: { _ in
+                self.items = nil
+            }).store(in: &cancellables)
     }
 
     /// Open a Realm for a given User.
     private func openRealm(user: User) {
-        Realm.asyncOpen(configuration: user.configuration(partitionValue: user.id)) { result in
-            guard case let .success(realm) = result else {
-                return
-            }
-
-            // if no Group has been created for this app, create one
-            if realm.objects(Group.self).count == 0 {
-                try! realm.write {
-                     realm.add(Group())
+        // If using Realm Sync, use the user configuration object with a partition value.
+        let USE_REALM_SYNC = false
+        let configuration = USE_REALM_SYNC ? user.configuration(partitionValue: user.id) : Realm.Configuration()
+        if Realm.fileExists(for: configuration) {
+            let realm = try! Realm(configuration: configuration)
+            onRealmOpened(realm)
+        } else {
+            shouldIndicateActivity = true
+            Realm.asyncOpen(configuration: configuration) { result in
+                self.shouldIndicateActivity = false
+                guard case let .success(realm) = result else {
+                    return
                 }
+                self.onRealmOpened(realm)
             }
-            self.realm = realm
-            self.shouldIndicateActivity = false
         }
     }
-}
 
-// MARK: - Views
-
-/// Simple activity indicator to telegraph that the app is active in the background.
-struct ActivityIndicator: UIViewRepresentable {
-    func makeUIView(context: Context) -> some UIView {
-        return UIActivityIndicatorView(style: .large)
-    }
-
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        (uiView as! UIActivityIndicatorView).startAnimating()
-    }
-}
-
-// MARK: ItemsView
-/// The screen containing a list of items in a group. Implements functionality for adding, rearranging, 
-/// and deleting items in the group. 
-struct ItemsView: View {
-    @EnvironmentObject var state: AppState
-    /// The items in this group.
-    @ObservedObject var items: RealmSwift.List<Item>
-
-    init(realm: Realm) {
+    /// Called internally when the realm is opened.
+    private func onRealmOpened(_ realm: Realm) {
+        // if no Group has been created for this app, create one
+        if realm.objects(Group.self).count == 0 {
+            try! realm.write {
+                realm.add(Group())
+            }
+        }
         assert(realm.objects(Group.self).count > 0)
         items = realm.objects(Group.self).first!.items
     }
+}
+
+// MARK: Views
+
+
+
+// MARK: Item Views
+/// The screen containing a list of items in a group. Implements functionality for adding, rearranging,
+/// and deleting items in the group.
+struct ItemsView: View {
+    /// The items in this group.
+    @ObservedObject var items: RealmSwift.List<Item>
+    
+    /// The button to be displayed on the top left.
+    var leadingBarButton: AnyView?
 
     var body: some View {
         NavigationView {
@@ -170,24 +159,14 @@ struct ItemsView: View {
                     .navigationBarTitle("Items", displayMode: .large)
                     .navigationBarBackButtonHidden(true)
                     .navigationBarItems(
-                        // Log out button on the left
-                        leading: Button("Log Out") {
-                            state.shouldIndicateActivity = true
-                            app.currentUser?.logOut().receive(on: DispatchQueue.main).sink(receiveCompletion: { (_) in
-                                state.shouldIndicateActivity = false
-                                state.realm = nil
-                            }, receiveValue: { _ in
-
-                            }).store(in: &state.cancellables)
-                        }.disabled(state.shouldIndicateActivity),
+                        leading: self.leadingBarButton,
                         // Edit button on the right to enable rearranging items
-                        trailing: EditButton().disabled(state.shouldIndicateActivity))
+                        trailing: EditButton())
 
                 // Action bar at bottom contains Add button.
                 HStack {
                     Spacer()
                     Button(action: addItem) { Image(systemName: "plus") }
-                        .disabled(state.shouldIndicateActivity)
                 }.padding()
             }
         }
@@ -217,7 +196,7 @@ struct ItemsView: View {
     }
 
     /// Rearranges the given item in the group.
-    /// This is persisted because the items are stored in a Realm List. 
+    /// This is persisted because the items are stored in a Realm List.
     func move(fromOffsets offsets: IndexSet, toOffset to: Int) {
         guard let realm = items.realm else {
             items.move(fromOffsets: offsets, toOffset: to)
@@ -229,36 +208,26 @@ struct ItemsView: View {
     }
 }
 
-// MARK: ItemRow
 /// Represents an Item in a list.
 struct ItemRow: View {
-    @ObservedObject var item: Item
-
+    var item: Item
     var body: some View {
-        // If the item has been invalidated, display an empty view.
-        // it will be cleaned up on next render.
-        // else, display the details view
-        if item.isInvalidated {
-            EmptyView()
-        } else {
-            // You can click an item in the list to navigate to an edit details screen.
-            NavigationLink(destination: ItemDetailsView(item)) {
-                Text(item.name)
-            }
+        // You can click an item in the list to navigate to an edit details screen.
+        NavigationLink(destination: ItemDetailsView(item)) {
+            Text(item.name)
         }
     }
 }
 
-// MARK: ItemDetailsView
-/// Represents a screen where you can edit the item's name. 
+/// Represents a screen where you can edit the item's name.
 struct ItemDetailsView: View {
-    @ObservedObject var item: Item
-    
+    var item: Item
+
     // ⚠️ Beware using a Realm object or its properties directly in a @Binding.
     // Writes to Realm objects MUST occur in a transaction (realm.write() block),
     // but a default Binding will not do that for you. Therefore, either use a
     // separate @State object to hold the data before writing (as we do here),
-    // or create a custom Binding that handles writes in a transaction.  
+    // or create a custom Binding that handles writes in a transaction.
     @State var newItemName: String = ""
 
     init(_ item: Item) {
@@ -270,13 +239,13 @@ struct ItemDetailsView: View {
 
     var body: some View {
         // Write the new name to the newItemName state variable.
-        // On commit, call our commit() function. 
-        TextField(item.name, text: $newItemName, onCommit:  { self.commit() })
+        // On commit, call our commit() function.
+        TextField(item.name, text: $newItemName, onCommit: { self.commit() })
             .navigationBarTitle(item.name)
             .padding()
     }
 
-    /// Writes the given name to the realm in a transaction.  
+    /// Writes the given name to the realm in a transaction.
     private func commit() {
         guard let realm = item.realm else {
             item.name = newItemName
@@ -288,6 +257,7 @@ struct ItemDetailsView: View {
     }
 }
 
+// MARK: Authentication Views
 /// Represents the login screen. We will just have a button to log in anonymously.
 struct LoginView: View {
     @EnvironmentObject var state: AppState
@@ -302,23 +272,49 @@ struct LoginView: View {
             Button("Log in anonymously") {
                 state.shouldIndicateActivity = true
                 app.login(credentials: .anonymous).receive(on: DispatchQueue.main).sink(receiveCompletion: {
+                    state.shouldIndicateActivity = false
                     switch ($0) {
                     case .finished:
-                        state.shouldIndicateActivity = false
                         break
                     case .failure(let error):
                         self.error = error
                     }
                 }, receiveValue: {
-                    self.error = nil
-                    state.loginPublisher.send($0)
-                }).store(in: &state.cancellables)
+                        self.error = nil
+                        state.loginPublisher.send($0)
+                    }).store(in: &state.cancellables)
             }.disabled(state.shouldIndicateActivity)
         }
     }
 }
 
-// MARK: ContentView
+/// A button that handles logout requests.
+struct LogoutButton: View {
+    @EnvironmentObject var state: AppState
+    var body: some View {
+        Button("Log Out") {
+            state.shouldIndicateActivity = true
+            app.currentUser?.logOut().receive(on: DispatchQueue.main).sink(receiveCompletion: { _ in }, receiveValue: {
+                state.shouldIndicateActivity = false
+                state.logoutPublisher.send($0)
+            }).store(in: &state.cancellables)
+        }.disabled(state.shouldIndicateActivity)
+    }
+}
+
+// MARK: General View
+/// Simple activity indicator to telegraph that the app is active in the background.
+struct ActivityIndicator: UIViewRepresentable {
+    func makeUIView(context: Context) -> some UIView {
+        return UIActivityIndicatorView(style: .large)
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        (uiView as! UIActivityIndicatorView).startAnimating()
+    }
+}
+
+// MARK: Main View
 /// The main screen that determines whether to present the LoginView or the ItemsView for the one group in the realm.
 @main
 struct ContentView: SwiftUI.App {
@@ -329,8 +325,10 @@ struct ContentView: SwiftUI.App {
         ZStack {
             // If a realm is open for a logged in user, show the ItemsView
             // else show the LoginView
-            if let realm = state.realm {
-                ItemsView(realm: realm)
+            if let items = state.items {
+                ItemsView(items: items,
+                    leadingBarButton: AnyView(LogoutButton().environmentObject(state)))
+                    .disabled(state.shouldIndicateActivity)
             } else {
                 LoginView()
             }
