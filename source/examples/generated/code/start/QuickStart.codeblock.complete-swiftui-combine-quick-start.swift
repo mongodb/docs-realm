ff6dc9e26ed6d4a833579a152c56190407fdb035
Chris Bush <chris.bush@mongodb.com>
2020-10-28 11:11:06 -0400

    Final test & update

--- source/examples/generated/code/start/QuickStart.codeblock.complete-swiftui-combine-quick-start.swift
+++ source/examples/generated/code/start/QuickStart.codeblock.complete-swiftui-combine-quick-start.swift
@@ -10,6 +10,9 @@ import SwiftUI
 let USE_REALM_SYNC = false
 
 /// The Realm app. Change YOUR_REALM_APP_ID_HERE to your Realm app ID.
+// If you don't have a Realm app and don't wish to use Sync for now,
+// change this to:
+//   let app: RealmSwift.App? = nil
 let app = USE_REALM_SYNC ? App(id: YOUR_REALM_APP_ID_HERE) : nil
 
 // MARK: Models
