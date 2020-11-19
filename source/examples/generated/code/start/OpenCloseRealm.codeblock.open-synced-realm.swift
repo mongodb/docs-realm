b1c74d3ccb3bb853df3216e7b227618d46c2ca3e
Chris Bush <chrbsh@gmail.com>
2020-11-19 17:08:19 -0500

    Update iOS docs for 10.1.4 (#624)
    - No significant change required
    - Missing "_id" now detected on sync realm classes, hence update to tests

--- source/examples/generated/code/start/OpenCloseRealm.codeblock.open-synced-realm.swift
+++ source/examples/generated/code/start/OpenCloseRealm.codeblock.open-synced-realm.swift
@@ -2,7 +2,15 @@ let app = App(id: YOUR_REALM_APP_ID)
 // Log in...
 let user = app.currentUser
 let partitionValue = "some partition value"
-Realm.asyncOpen(configuration: user!.configuration(partitionValue: partitionValue)) { result in
+
+var configuration = user!.configuration(partitionValue: partitionValue)
+
+// The following is only required if you want to specify exactly which
+// types to include in the realm. By default, Realm automatically finds
+// all subclasses of Object and EmbeddedObject to add to the realm.
+configuration.objectTypes = [Task.self]
+
+Realm.asyncOpen(configuration: configuration) { result in
     switch result {
     case .failure(let error):
         print("Failed to open realm: \(error.localizedDescription)")
