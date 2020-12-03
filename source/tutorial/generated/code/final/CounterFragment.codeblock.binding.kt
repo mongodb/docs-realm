870c3b96ebbb16ff488dca62ed44bd23b7421432
nathan-contino-mongo <nathan.contino@mongodb.com>
2020-12-03 09:49:36 -0500

    pointing livedata quickstart at the right generated files

--- source/tutorial/generated/code/final/CounterFragment.codeblock.binding.kt
+++
@@ -1,9 +0,0 @@
-val binding = CounterFragmentBinding.inflate(inflater, container, false).apply {
-    lifecycleOwner = viewLifecycleOwner
-    counterModel = model
-}.root
-
-binding.rootView.button.setOnClickListener {
-    model.incrementCounter()
-}
-return binding
