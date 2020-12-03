9be4d20a7a146a571b923e544370122a5224d1d5
nate contino <nathan.contino@mongodb.com>
2020-12-03 07:53:13 -0700

    pointing livedata quickstart at the right generated files (#636)

--- source/tutorial/generated/code/final/LiveRealmObject.codeblock.constructor.kt
+++
@@ -1,9 +0,0 @@
-init {
-    require(RealmObject.isManaged(obj)) { "LiveRealmObject only supports managed RealmModel instances!" }
-    require(RealmObject.isValid(obj)) { "The provided RealmObject is no longer valid, and therefore cannot be observed for changes." }
-    if (RealmObject.isLoaded(obj)) {
-        // we should not notify observers when results aren't ready yet (async query).
-        // however, synchronous query should be set explicitly.
-        setValue(obj)
-    }
-}
