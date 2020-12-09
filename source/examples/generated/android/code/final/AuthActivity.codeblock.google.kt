73b712e4b911546796eac0f3107ea44bd5e4995d
nate contino <nathan.contino@mongodb.com>
2020-12-09 08:58:25 -0700

    Updated Android SDK docs to 10.2.0 (#651)

--- source/examples/generated/android/code/final/AuthActivity.codeblock.google.kt
+++ source/examples/generated/android/code/final/AuthActivity.codeblock.google.kt
@@ -23,7 +23,7 @@ fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
     try {
         val account: GoogleSignInAccount? = completedTask.result
         val authorizationCode: String? = account?.serverAuthCode
-        val googleCredentials: Credentials = Credentials.google(authorizationCode)
+        val googleCredentials: Credentials = Credentials.google(authorizationCode, GoogleAuthType.AUTH_CODE)
         app.loginAsync(googleCredentials) {
             if (it.isSuccess) {
                 Log.v(
