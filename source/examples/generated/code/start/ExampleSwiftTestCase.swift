848cfd8ab536bcba81cface94a69b511edca5161
Chris Bush <chris.bush@mongodb.com>
2020-12-14 18:23:13 -0500

    (DOCSP-13324): Read & Write Data Swift & Obj-C examples

--- source/examples/generated/code/start/ExampleSwiftTestCase.swift
+++ source/examples/generated/code/start/ExampleSwiftTestCase.swift
@@ -1,7 +1,7 @@
 import XCTest
 import RealmSwift
 
-class ExampleSwiftTestCase: XCTestCase {
+class ExampleTestCase: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
