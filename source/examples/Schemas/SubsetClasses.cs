6980500b883160521e23ce3a03f501a9a32ec0fe
Katie Sadoff <katie.sadoff@mongodb.com>
2020-12-15 11:14:06 -0500

    DOCSP-13320 added subset classes section

--- source/examples/Schemas/SubsetClasses.cs
+++ source/examples/Schemas/SubsetClasses.cs
@@ -1,8 +1,10 @@
+// Declare your schema
 class LoneClass : RealmObject
 {
     public string Name { get; set;}
 }
 
+// In your main function, define your config
 var config = new RealmConfiguration("RealmWithOneClass.realm");
 config.ObjectClasses = new[] { typeof(LoneClass) };
 
