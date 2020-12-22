e35b1d570e88a6009e654328ccadaef89d128256
Katie Sadoff <katie.sadoff@mongodb.com>
2020-12-22 15:59:27 -0500

    DOCSP-13325 clarified primary key example

--- source/examples/Query/FindByPrimaryKey.cs
+++ source/examples/Query/FindByPrimaryKey.cs
@@ -1,7 +1,15 @@
+// Object to be stored in the Realm instance
 var myTask = new Task
 {
     Id = 1
 };
 
+realm.Write(() =>
+{
+    realm.Add(myTask);
+});
+
+// Other code...
+
 // Find specific object by primary key
-var obj = realm.Find<MyObject>(1);
+var obj = realm.Find<Task>(1);
