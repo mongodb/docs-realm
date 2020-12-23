c9f452ff783ecdee5514b56d86e1444c7095a311
Chris Bush <chris.bush@mongodb.com>
2020-12-23 16:07:53 -0500

    Remove old untested code snippet files

--- source/examples/Query/String.swift
+++
@@ -1,4 +0,0 @@
-// Use [c] for case-insensitivity.
-print("Projects that start with 'e': \(projects.filter("name BEGINSWITH[c] 'e'").count)");
-
-print("Projects that contain 'ie': \(projects.filter("name CONTAINS 'ie'").count)");
