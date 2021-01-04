1b8cede2bae8c5d0874bf4d253358aefa79cb148
Chris Bush <chris.bush@mongodb.com>
2021-01-04 11:13:58 -0500

    Add notifications

--- examples/ios/Examples/Notifications.swift
+++ examples/ios/Examples/Notifications.swift
@@ -1,14 +1,18 @@
 import RealmSwift
+import XCTest
 
+// Note: the following is not really tested but it makes for a convenient
+// full program example.
+// :code-block-start: register-an-object-change-listener
 // Define the dog class.
-class Dog: Object {
+class NotificationExample_Dog: Object {
     @objc dynamic var name = ""
 }
 
-var token: NotificationToken?
+var objectNotificationToken: NotificationToken?
 
-func example() {
-    let dog = Dog()
+func objectNotificationExample() {
+    let dog = NotificationExample_Dog()
     dog.name = "Max"
 
     // Open the default realm.
@@ -18,7 +22,7 @@ func example() {
     }
     // Observe object notifications. Keep a strong reference to the notification token
     // or the observation will stop. Invalidate the token when done observing.
-    token = dog.observe { change in
+    objectNotificationToken = dog.observe { change in
         switch change {
         case .change(let object, let properties):
             for property in properties {
@@ -35,4 +39,91 @@ func example() {
         dog.name = "Wolfie"
     }
 }
+// :code-block-end:
 
+// :code-block-start: register-a-collection-change-listener
+class CollectionNotificationExampleViewController: UITableViewController {
+    var notificationToken: NotificationToken? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let realm = try! Realm()
+        let results = realm.objects(NotificationExample_Dog.self)
+
+        // Observe collection notifications. Keep a strong
+        // reference to the notification token or the
+        // observation will stop.
+        notificationToken = results.observe { [weak self] (changes: RealmCollectionChange) in
+            guard let tableView = self?.tableView else { return }
+            switch changes {
+            case .initial:
+                // Results are now populated and can be accessed without blocking the UI
+                tableView.reloadData()
+            case .update(_, let deletions, let insertions, let modifications):
+                // Query results have changed, so apply them to the UITableView
+                tableView.beginUpdates()
+                // Always apply updates in the following order: deletions, insertions, then modifications.
+                // Handling insertions before deletions may result in unexpected behavior.
+                tableView.deleteRows(at: deletions.map({ IndexPath(row: $0, section: 0)}),
+                                     with: .automatic)
+                tableView.insertRows(at: insertions.map({ IndexPath(row: $0, section: 0) }),
+                                     with: .automatic)
+                tableView.reloadRows(at: modifications.map({ IndexPath(row: $0, section: 0) }),
+                                     with: .automatic)
+                tableView.endUpdates()
+            case .error(let error):
+                // An error occurred while opening the Realm file on the background worker thread
+                fatalError("\(error)")
+            }
+        }
+    }
+
+    deinit {
+        notificationToken?.invalidate()
+    }
+}
+// :code-block-end:
+
+class Notifications: XCTestCase {
+    func testSilentWrite() {
+        // :code-block-start: silent-write
+        let realm = try! Realm()
+
+        // Observe Realm Notifications
+        let token = realm.observe { notification, realm in
+            // ... handle update
+        }
+
+        // Later, pass the token in an array to the realm.write(withoutNotifying:)
+        // method to write without send a notification to that observer.
+        try! realm.write(withoutNotifying: [token]) {
+            // ... write to realm
+        }
+
+        // Finally
+        token.invalidate()
+        // :code-block-end:
+    }
+    
+    func testRealmNotification() {
+        class VC {
+            func updateUI() {}
+        }
+        let viewController = VC()
+        // :code-block-start: register-a-realm-change-listener
+        let realm = try! Realm()
+        
+        // Observe realm notifications. Keep a strong reference to the notification token
+        // or the observation will stop.
+        let token = realm.observe { notification, realm in
+            // `notification` is an enum specifying what kind of notification was emitted.
+            // See https://realm.io/docs/swift/latest/api/Classes/Realm.html#/s:10RealmSwift0A0C12NotificationO
+            // for details.
+            viewController.updateUI()
+        }
+
+        // Later, explicitly stop observing.
+        token.invalidate()
+        // :code-block-end:
+    }
+}
