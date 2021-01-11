586e22023bb6ed3f56e42b40a2cdf60d301b8260
Mohammad Chughtai <mohammad.hunan@Mohammads-MacBook-Pro.local>
2021-01-11 13:56:41 -0500

    clarify step 6

---
+++ source/examples/QuickStarts/electron/quickstart-setup-main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
