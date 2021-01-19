25f11c13fae72548e0ddb5678091f53722ba7304
Chris Bush <chrbsh@gmail.com>
2021-01-19 17:25:10 -0500

    Apply suggestions from code review
    Co-authored-by: Thomas Goyne <tgoyne@gmail.com>

--- examples/ios/Examples/Crud.m
+++ examples/ios/Examples/Crud.m
@@ -232,12 +232,12 @@
     // :code-block-start: upsert
     RLMRealm *realm = [RLMRealm defaultRealm];
     [realm transactionWithBlock:^{
-        CrudExampleObjc_Person *jones = [[CrudExampleObjc_Person alloc] initWithValue:@{@"ID": @1234, @"name": @"Jones"}];
+        CrudExampleObjc_Person *jones = [[CrudExampleObjc_Person alloc] initWithValue:@{@"_id": @1234, @"name": @"Jones"}];
         // Add a new person to the realm. Since nobody with ID 1234
         // has been added yet, this adds the instance to the realm.
         [realm addOrUpdateObject:jones];
         
-        CrudExampleObjc_Person *bowie = [[CrudExampleObjc_Person alloc] initWithValue:@{@"ID": @1234, @"name": @"Bowie"}];
+        CrudExampleObjc_Person *bowie = [[CrudExampleObjc_Person alloc] initWithValue:@{@"_id": @1234, @"name": @"Bowie"}];
         // Judging by the ID, it's the same person, just with a different name.
         // This overwrites the original entry (i.e. Jones -> Bowie).
         [realm addOrUpdateObject:bowie];
