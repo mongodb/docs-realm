daa3b4f110f3e58f95b1079cd099eaee1e49bb9b
Mohammad Chughtai <mohammad.hunan@Mohammads-MacBook-Pro.local>
2021-01-20 14:40:20 -0500

    made parameter to _changedDog to indicate that its not used

--- source/examples/Notifications/ObjectNotification.js
+++ source/examples/Notifications/ObjectNotification.js
@@ -10,9 +10,8 @@ async function example() {
   });
 
   // Observe object notifications.
-  dog.addListener((obj, changes) => {
-    // obj === dog
-    console.log(`object is deleted? ${changes.deleted}`);
+  dog.addListener((_changedDog, changes) => {
+    console.log(`dog is deleted? ${changes.deleted}`);
     console.log(`${changes.changedProperties.length} properties have been changed:`);
     changes.changedProperties.forEach((prop) => {
       console.log(` ${prop}`);
