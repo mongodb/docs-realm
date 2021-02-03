7e9091ec9913293a2af4c33d604e614b80fe26a1
mongocaleb <caleb.thompson@mongodb.com>
2021-02-03 12:46:10 -0800

    notifications

--- examples/dotnet/Examples/WorkWithRealm.cs
+++ examples/dotnet/Examples/WorkWithRealm.cs
@@ -7,6 +7,8 @@ using Realms;
 using Realms.Sync;
 using TaskStatus = dotnet.TaskStatus;
 using Task = dotnet.Task;
+using System.Collections.Specialized;
+using System.Diagnostics;
 
 namespace UnitTests
 {
@@ -35,11 +37,9 @@ namespace UnitTests
             string userEmail = "bob@bob.com";
 
             // :code-block-start: initialize-realm
-            // :hide-start:
-            /*:replace-with:
             var myRealmAppId = "<your_app_id>";
             var app = App.Create(myRealmAppId);
-            :code-block-end:*/
+            //:code-block-end:
             // :code-block-start: appConfig
             var appConfig = new AppConfiguration(myRealmAppId)
             {
@@ -136,7 +136,7 @@ namespace UnitTests
                 var aimee = await app.LogInAsync(Credentials.EmailPassword(
                     "aimee@example.com", "sekrit"));
                 Assert.IsTrue(aimee.Id == app.CurrentUser.Id, "aimee is current user");
- 
+
                 var elvis = await app.LogInAsync(Credentials.EmailPassword(
                     "elvis@example.com", "sekrit2"));
                 Assert.IsTrue(elvis.Id == app.CurrentUser.Id, "elvis is current user");
@@ -163,6 +163,94 @@ namespace UnitTests
             }
 
             return;
+        }
+
+        [Test]
+        public void Notifications()
+        {
+            myRealmAppId = "tuts-tijya";
+            var app = App.Create(myRealmAppId);
+            var realm = Realm.GetInstance("");
+
+            //:code-block-start:notifications
+            // Observe realm notifications.
+            realm.RealmChanged += (sender, eventArgs) =>
+            {
+                // sender is the realm that has changed.
+                // eventArgs is reserved for future use.
+                // ... update UI ...
+            };
+            //:code-block-end:
+
+            //:code-block-start:collection-notifications
+            // Observe collection notifications. Retain the token to keep observing.
+            var token = realm.All<Dog>()
+                .SubscribeForNotifications((sender, changes, error) =>
+            {
+                foreach (var i in changes.DeletedIndices)
+                {
+                    // ... handle deletions ...
+                }
+
+                foreach (var i in changes.InsertedIndices)
+                {
+                    // ... handle insertions ...
+                }
+
+                foreach (var i in changes.NewModifiedIndices)
+                {
+                    // ... handle modifications ...
+                }
+            });
+
+            // Later, when you no longer wish to receive notifications
+            token.Dispose();
+            //:code-block-end:
+
+            //:code-block-start:object-notifications
+            var theKing = realm.All<Person>()
+                .FirstOrDefault(p => p.Name == "Elvis Presley");
+
+            theKing.PropertyChanged += (sender, eventArgs) =>
+            {
+                Debug.WriteLine($"New value set for The King: " +
+                    $"{eventArgs.PropertyName}");
+            };
+        }
+        //:code-block-end:
+
+        class NotificationUnsub
+        {
+            Realm realm;
+
+
+            public NotificationUnsub()
+            {
+                realm = Realm.GetInstance("");
+            }
+            //:code-block-start:unsubscribe
+            private IQueryable<Task> tasks;
+
+            public void LoadUI()
+            {
+                tasks = realm.All<Task>();
+
+                // Subscribe for notifications - since tasks is IQueryable<Task>, we're
+                // using the AsRealmCollection extension method to cast it to IRealmCollection
+                tasks.AsRealmCollection().CollectionChanged += OnTasksChanged;
+            }
+
+            public void UnloadUI()
+            {
+                // Unsubscribe from notifications
+                tasks.AsRealmCollection().CollectionChanged -= OnTasksChanged;
+            }
+
+            private void OnTasksChanged(object sender, NotifyCollectionChangedEventArgs args)
+            {
+                // Do something with the notification information
+            }
+            // :code-block-end:
         }
     }
 }
---
+++ source/examples/generated/WorkWithRealm.codeblock.object-notifications.cs
@@ -0,0 +1,6 @@
+    var theKing = realm.All<Person>()
+        .FirstOrDefault(p => p.Name == "Elvis Presley");
+
+    theKing.PropertyChanged += (sender, eventArgs) =>
+    {
+        Debug.WriteLine($"New value set for The King: " +             $"{eventArgs.PropertyName}");     }; }
