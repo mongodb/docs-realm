28e34c01cb4c0438c8ee0f54d40c54e75e48e5d7
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2021-02-09 13:05:57 -0500

    added + bluehawked open and close realm snippet w passing test

---
+++ examples/node/Examples/open-and-close-a-local-realm.js
@@ -0,0 +1,26 @@
+import Realm from "realm";
+
+// const app = new Realm.App({ id: "example-testers-kvjdy" });
+
+const CarSchema = {
+  name: "Car",
+  properties: {
+    make: "string",
+    model: "string",
+    miles: "int",
+  },
+};
+
+describe("Open and Close a Local Realm", () => {
+  test("should open a synced realm", async () => {
+    // :code-block-start: open-local-realm-with-car-schema
+    // Open a local realm file with a particular path & predefined CarSchema
+    const realm = await Realm.open({
+      path: "myrealm",
+      schema: [CarSchema],
+    });
+    // :code-block-end:
+    // you can test that a realm has been open in general (but not if a realm has been open with a specific path or schema)
+    expect(realm).toStrictEqual(new Realm());
+  });
+});
