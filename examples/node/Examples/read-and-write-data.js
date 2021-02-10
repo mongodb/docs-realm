ae1397b4651e81d9e73f4781f4f3ccd58d25dfcb
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2021-02-10 13:25:01 -0500

    bluehawked and unit tested filter queries

--- examples/node/Examples/read-and-write-data.js
+++ examples/node/Examples/read-and-write-data.js
@@ -5,6 +5,8 @@ const TaskSchema = {
   properties: {
     _id: "int",
     name: "string",
+    priority: "int?",
+    progressMinutes: "int?",
   },
   primaryKey: "_id",
 };
@@ -74,5 +76,58 @@ describe("Read & Write Data", () => {
 
     // close the realm
     realm.close();
+  });
+  test("should return objects from filter queries", async () => {
+    // a realm is opened
+    const realm = await Realm.open({
+      path: "myrealm",
+      schema: [TaskSchema],
+    });
+    let task, task2, task3;
+    // write to a realm
+    realm.write(() => {
+      task = realm.create("Task", {
+        _id: 191230,
+        name: "go grocery shopping",
+        priority: 10,
+        progressMinutes: 50,
+      });
+      task2 = realm.create("Task", {
+        _id: 325212012,
+        name: "throw out the trash",
+        priority: 4,
+        progressMinutes: 0,
+      });
+      task3 = realm.create("Task", {
+        _id: 43259540,
+        name: "do the laundry",
+        priority: 2,
+        progressMinutes: 5,
+      });
+    });
+
+    // :code-block-start: read-and-write-filter-queries
+    // retrieve the set of Task objects
+    const tasks = realm.objects("Task");
+    // filter for tasks with a high priority
+    const highPriorityTasks = tasks.filtered("priority > 5");
+    // filter for tasks that have just-started or short-running progress
+    const lowProgressTasks = tasks.filtered(
+      "1 <= progressMinutes && progressMinutes < 10"
+    );
+    console.log(
+      `Number of high priority tasks: ${highPriorityTasks.length} \n`,
+      `Number of just-started or short-running tasks: ${lowProgressTasks.length}`
+    );
+    // :code-block-end:
+
+    expect(highPriorityTasks[0].name).toBe(task.name); // expect that 'go grocery shopping' should be the first (and only) high priority
+    expect(lowProgressTasks[0].name).toBe(task3.name); // expect that 'do the laundry' should be the first (and only) low progress task
+    // delete tasks
+    realm.write(() => {
+      realm.delete(task);
+      realm.delete(task2);
+      realm.delete(task3);
+    });
   });
 });
