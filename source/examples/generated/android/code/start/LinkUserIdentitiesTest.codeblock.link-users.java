459a1eac454ac57dbb5bfec540649d2a9466b915
nathan-contino-mongo <nathan.contino@mongodb.com>
2021-02-10 11:01:17 -0700

    Android testing logic improvements

--- source/examples/generated/android/code/start/LinkUserIdentitiesTest.codeblock.link-users.java
+++ source/examples/generated/android/code/start/LinkUserIdentitiesTest.codeblock.link-users.java
@@ -1,5 +1,5 @@
 user.linkCredentialsAsync(Credentials.emailPassword(email, password), result -> {
-    if (it.isSuccess()) {
+    if (result.isSuccess()) {
         Log.v("EXAMPLE", "Successfully linked existing user identity with email/password user: " + result.get());
     } else {
         Log.e("EXAMPLE", "Failed to link user identities with: " + result.getError());
