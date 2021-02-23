8849d776a3559824b35c10dcf22b1cab18ed5c86
Caleb Thompson <caleb.thompson@mongodb.com>
2021-02-23 12:35:54 -0800

    fix formatting. bluehawk code is borked

--- examples/dotnet/LocalOnly/Guitar.cs
+++ examples/dotnet/LocalOnly/Guitar.cs
@@ -9,18 +9,16 @@ namespace LocalOnly
         [PrimaryKey]
         public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
 
+        [Required]
         public string Make { get; set; }
 
+        [Required]
         public string Model { get; set; }
 
         public double Price { get; set; }
 
         public string Owner { get; set; }
 
-        public Guitar()
-        {
-
-        }
     }
     //:code-block-end:
 }
