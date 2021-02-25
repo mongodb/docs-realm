c50fb9f02c8823089ad735149051a995d2571539
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2021-02-25 16:14:04 -0500

    update tests

--- examples/node/Examples/quickstart-local.js
+++ examples/node/Examples/quickstart-local.js
@@ -45,17 +45,82 @@ describe("QuickStart Local", () => {
     expect(task2.name).toBe("go exercise");
 
     // :code-block-start: quickstart-local-find-sort-and-filter-objects
+    // query realm for all instances of the "Task" type.
+    const tasks = realm.objects("Task");
+
+    // filter for all tasks with a status of "Open"
+    const openTasks = tasks.filtered("status = 'Open'");
+
+    // Sort tasks by name in ascending order
+    const tasksByName = tasks.sorted("name");
     // :code-block-end:
-    // :code-block-start: quickstart-local-modify-an-object
-    // :code-block-end:
-    // :code-block-start: quickstart-local-delete-an-object
-    // :code-block-end:
+    expect(tasks.length).toBe(2);
+    expect(openTasks.length).toBe(2);
+    expect(tasksByName[0].name).toBe("go exercise");
+
+    let taskHasBeenModified = false;
+    let taskHasBeenDeleted = false;
     // :code-block-start: quickstart-local-watch-a-collection
+    // Define the collection notification listener
+    function listener(tasks, changes) {
+      // :hide-start:
+      if (changes.newModifications.length > 0) {
+        taskHasBeenModified = true;
+      }
+      if (changes.deletions.length > 0) {
+        taskHasBeenDeleted = true;
+      }
+      // :hide-end:
+      // Update UI in response to deleted objects
+      changes.deletions.forEach((index) => {
+        // Deleted objects cannot be accessed directly,
+        // but we can update a UI list, etc. knowing the index.
+      });
+      // Update UI in response to inserted objects
+      changes.insertions.forEach((index) => {
+        let insertedTasks = tasks[index];
+        // ...
+      });
+      // Update UI in response to modified objects
+      // `oldModifications` contains object indexes from before they were modified
+      changes.oldModifications.forEach((index) => {
+        let modifiedTask = tasks[index];
+        // ...
+      });
+      // Update UI in response to modified objects
+      // `newModifications` contains object indexes from after they were modified
+      changes.newModifications.forEach((index) => {
+        let modifiedTask = tasks[index];
+        // ...
+      });
+    }
+    // Observe collection notifications.
+    tasks.addListener(listener);
     // :code-block-end:
 
+    // :code-block-start: quickstart-local-modify-an-object
     realm.write(() => {
+      task1.status = "InProgress";
+    });
+    // :code-block-end:
+    expect(task1.status).toBe("InProgress");
+    // :code-block-start: quickstart-local-delete-an-object
+    realm.write(() => {
+      // Delete the task from the realm.
       realm.delete(task1);
-      realm.delete(task2);
+      // Discard the reference.
+      task1 = null;
+    });
+    // :code-block-end:
+
+    // wait 1 second until the collection listener has registered the modification and deletion events
+    setTimeout(() => {
+      expect(taskHasBeenModified).toBe(true);
+      expect(taskHasBeenDeleted).toBe(true);
+    }, 1000);
+
+    realm.write(() => {
+      realm.delete(task2); // task1 has already been deleted
     });
 
     realm.close();
