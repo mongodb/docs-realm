4149b676c7b3b17ae27faef0614f46b327c8b1c2
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2021-03-01 14:45:05 -0500

    (DOCSP-14354): sync changes js network functionality (#817)
    * added pause and resume example
    
    * fixed text
    
    * added prog notifications method
    
    * added check connection state
    
    * grammer fixes
    
    * more syntax fixes
    
    * removed unneeded spacing
    
    * updated phrasing to be less confusing
    
    Co-authored-by: Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>

---
+++ source/examples/generated/node/sync-changes-between-devices.codeblock.sync-changes-between-devices-check-upload-and-download-progress.js
@@ -0,0 +1,34 @@
+var config = {
+  schema: [DogSchema], // predefined schema
+  sync: {
+    user: app.currentUser,
+    partitionValue: "MyPartitionValue",
+  },
+};
+let realm = await Realm.open(config);
+const syncSession = realm.syncSession;
+syncSession.addProgressNotification(
+  "upload",
+  "reportIndefinitely",
+  (transferred, transferable) => {
+    console.log(`${transferred} bytes has been transferred`);
+    console.log(
+      `There are ${transferable} total transferable bytes, including the ones that have already been transferred`
+    );
+  }
+);
+// Upload something
+let dog;
+realm.write(() => {
+  dog = realm.create("Dog", {
+    name: "Fido",
+    age: 2,
+  });
+});
+// use dog
+
+// remember to unregister the progress notifications
+syncSession.removeProgressNotification((transferred, transferable) => {
+  console.log(`There was ${transferable} total transferable bytes`);
+  console.log(`${transferred} bytes were transferred`);
+});
