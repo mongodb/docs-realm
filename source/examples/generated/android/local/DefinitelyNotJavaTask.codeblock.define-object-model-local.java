dd5da04a7a48eab522a3c68fe1a7d8c08d93d3fa
nathan-contino-mongo <nathan.contino@mongodb.com>
2021-03-02 11:02:37 -0700

    task.java was loooong

--- source/examples/generated/android/local/DefinitelyNotJavaTask.codeblock.define-object-model-local.java
+++ source/examples/generated/android/local/DefinitelyNotJavaTask.codeblock.define-object-model-local.java
@@ -3,31 +3,13 @@ import io.realm.annotations.PrimaryKey;
 import io.realm.annotations.Required;
 
 public class Task extends RealmObject {
-    @PrimaryKey
-    private String name;
+    @PrimaryKey private String name;
+    @Required private String status = TaskStatus.Open.name();
 
-    @Required
-    private String status = TaskStatus.Open.name();
-
-    public void setStatus(TaskStatus status) {
-        this.status = status.name();
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Task(String _name) {
-        this.name = _name;
-    }
-
+    public void setStatus(TaskStatus status) { this.status = status.name(); }
+    public String getStatus() { return this.status; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Task(String _name) { this.name = _name; }
     public Task() {}
 }
