2b4f566a0aaba36bf6f723048884d4e656412b57
nathan-contino-mongo <nathan.contino@mongodb.com>
2021-03-02 09:35:01 -0700

    code-block fix and quick-start repoint

--- examples/android/local/app/src/main/java/com/mongodb/realm/examples/model/kotlin/FrogObjectExampleKt.kt
+++ examples/android/local/app/src/main/java/com/mongodb/realm/examples/model/kotlin/FrogObjectExampleKt.kt
@@ -8,12 +8,12 @@ package com.mongodb.realm.examples.model.kotlin
 import io.realm.RealmObject
 
 
-// providing default values for each constructor parameter fulfills the need for an empty constructor
+// :emphasize: // providing default values for each constructor parameter fulfills the need for an empty constructor
 class FrogObjectExampleKt(
     var name: String? = null,
     var age: Int = 0,
     var species: String? = null,
     var owner: String? = null
-) : RealmObject() // To add an object to your Realm Schema, extend RealmObject
+) : RealmObject() // :emphasize: // To add an object to your Realm Schema, extend RealmObject
 // :replace-end:
 // :code-block-end:
