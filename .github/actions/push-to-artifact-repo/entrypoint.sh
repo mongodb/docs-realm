e96ec556a9b1d495f354d55d0373f3cc4797bcf9
Chris Bush <chris.bush@mongodb.com>
2021-03-03 20:49:16 -0500

    to-repo -> toRepo

--- .github/actions/push-to-artifact-repo/entrypoint.sh
+++ .github/actions/push-to-artifact-repo/entrypoint.sh
@@ -5,7 +5,7 @@ npm i -g bluehawk bluehawk-plugin-git
 bluehawk \
   --plugin "`bluehawk-plugin-git`" \
   git copy \
-  --to-repo "$INPUT_TO_REPO" \
+  --to-repo "$INPUT_TOREPO" \
   --state "$INPUT_STATE" \
   --branch "$INPUT_STATE" \
   --delete-everything \
