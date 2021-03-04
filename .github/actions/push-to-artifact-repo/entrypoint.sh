453d1168c92b57533f0cf6e6cfdfb653a0a914f4
Chris Bush <chris.bush@mongodb.com>
2021-03-03 21:28:16 -0500

    Finalize

--- .github/actions/push-to-artifact-repo/entrypoint.sh
+++ .github/actions/push-to-artifact-repo/entrypoint.sh
@@ -5,11 +5,13 @@ npm i -g bluehawk bluehawk-plugin-git
 git config --global user.email "$INPUT_EMAIL"
 git config --global user.name "$INPUT_NAME"
 
-bluehawk \
-  --plugin "`bluehawk-plugin-git`" \
-  git copy \
-  --to-repo "$INPUT_TOREPO" \
-  --state "$INPUT_STATE" \
-  --branch "$INPUT_STATE" \
-  --delete-everything \
-  "$INPUT_SOURCE"
+for state in $INPUT_STATES; do
+  bluehawk \
+    --plugin "`bluehawk-plugin-git`" \
+    git copy \
+    --to-repo "$INPUT_TOREPO" \
+    --state $state \
+    --branch $state \
+    --delete-everything \
+    "$INPUT_SOURCE"
+done
