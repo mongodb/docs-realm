c6001db68ea43d89232b19ee2fa10ca4a1e28400
Dachary Carey <dachary.carey@mongodb.com>
2021-03-04 17:24:49 -0500

    Add RealmEnum to iOS documentation

--- examples/ios/Examples/QueryEngine.swift
+++ examples/ios/Examples/QueryEngine.swift
@@ -60,16 +60,16 @@ class QueryEngine: XCTestCase {
         // :code-block-start: comparison-operators
         let highPriorityTasks = tasks.filter("priority > 5")
         print("High priority tasks: \(highPriorityTasks.count)")
-        
+
         let longRunningTasks = tasks.filter("progressMinutes > 120")
         print("Long running tasks: \(longRunningTasks.count)")
-        
+
         let unassignedTasks = tasks.filter("assignee == nil")
         print("Unassigned tasks: \(unassignedTasks.count)")
-        
+
         let aliOrJamiesTasks = tasks.filter("assignee IN {'Ali', 'Jamie'}")
         print("Ali or Jamie's tasks: \(aliOrJamiesTasks.count)")
-        
+
         let progressBetween30and60 = tasks.filter("progressMinutes BETWEEN {30, 60}")
         print("Tasks with progress between 30 and 60 minutes: \(progressBetween30and60.count)")
         // :code-block-end:
@@ -83,10 +83,10 @@ class QueryEngine: XCTestCase {
         // Use [c] for case-insensitivity.
         let startWithE = projects.filter("name BEGINSWITH[c] 'e'")
         print("Projects that start with 'e': \(startWithE.count)")
-        
+
         let containIe = projects.filter("name CONTAINS 'ie'")
         print("Projects that contain 'ie': \(containIe.count)")
-        
+
         // [d] for diacritic insensitivty: contains 'e', 'E', 'é', etc.
         let containElike = projects.filter("name CONTAINS[cd] 'e'")
         print("Projects that contain 'e', 'E', 'é', etc.: \(containElike.count)")
@@ -98,13 +98,13 @@ class QueryEngine: XCTestCase {
 
         let allTasksLowerPriority = projects.filter("tasks.@max.priority < 5")
         print("Projects where all tasks are lower priority: \(allTasksLowerPriority.count)")
-        
+
         let allTasksHighPriority = projects.filter("tasks.@min.priority > 5")
         print("Projects where all tasks are high priority: \(allTasksHighPriority.count)")
-        
+
         let moreThan5Tasks = projects.filter("tasks.@count > 5")
         print("Projects with more than 5 tasks: \(moreThan5Tasks.count)")
-        
+
         let longRunningProjects = projects.filter("tasks.@sum.progressMinutes > 100")
         print("Long running projects: \(longRunningProjects.count)")
         // :code-block-end:
@@ -112,7 +112,7 @@ class QueryEngine: XCTestCase {
         // :code-block-start: set-operators
         let noCompleteTasks = projects.filter("NONE tasks.isComplete == true")
         print("Projects with no complete tasks: \(noCompleteTasks.count)")
-        
+
         let anyTopPriorityTasks = projects.filter("ANY tasks.priority == 10")
         print("Projects with any top priority tasks: \(anyTopPriorityTasks.count)")
         // :code-block-end:
