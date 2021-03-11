ecf90c66274c2e5570270bc1fe5de622ad308afc
nate contino <nathan.contino@mongodb.com>
2021-03-11 08:46:36 -0700

    (DOCSP-14621): Create objects from JSON, update strings and byte arrays (Android) (#878)

--- examples/android/local/app/src/androidTest/java/com/mongodb/realm/examples/java/WritesTest.java
+++ examples/android/local/app/src/androidTest/java/com/mongodb/realm/examples/java/WritesTest.java
@@ -1,19 +1,67 @@
 package com.mongodb.realm.examples.java;
 
 import android.util.Log;
-
 import com.mongodb.realm.examples.Expectation;
 import com.mongodb.realm.examples.RealmTest;
 import com.mongodb.realm.examples.model.kotlin.Frog;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import io.realm.OrderedRealmCollectionSnapshot;
+import io.realm.RealmResults;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
-import io.realm.RealmResults;
 
 public class WritesTest extends RealmTest {
+    @Test
+    public void testCreateObjectFromJSON() {
+        Expectation expectation = new Expectation();
+        activity.runOnUiThread(() -> {
+            RealmConfiguration config = new RealmConfiguration.Builder()
+                    .allowQueriesOnUiThread(true)
+                    .allowWritesOnUiThread(true)
+                    .inMemory()
+                    .name("create-object-from-json")
+                    .build();
+            Realm realm = Realm.getInstance(config);
+            try {
+                // :code-block-start: create-an-object-json
+                // Insert from a string
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        realm.createObjectFromJson(Frog.class,
+                                "{ name: \"Doctor Cucumber\", age: 1, species: \"bullfrog\", owner: \"Wirt\" }");
+                    }
+                });
+
+                // Insert multiple items using an InputStream
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        try {
+                            InputStream inputStream = new FileInputStream(
+                                    new File("path_to_file"));
+                            realm.createAllFromJson(Frog.class, inputStream);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+                // :code-block-end:
+            } catch (Exception e) { // this should throw when "path_to_file" doesn't resolve to a real file
+                expectation.fulfill();
+            }
+            realm.close();
+        }
+        expectation.await();
+    }
+                              
     @Test
     public void testIterate() {
         Expectation expectation = new Expectation();
