bb6ee1dc642c2a9f8707336c8495494eb52c501e
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2021-03-11 11:01:35 -0500

    updated wording

---
+++ source/examples/generated/node/sync-changes-between-devices.codeblock.sync-changes-between-devices-sync-changes-in-the-background-create-OpenRealmBehaviorObject.js
@@ -0,0 +1,3 @@
+const OpenRealmBehaviorConfiguration = {
+  type: "openImmediately",
+};
--- source/examples/generated/node/sync-changes-between-devices.codeblock.sync-changes-between-devices-sync-changes-in-the-background-create-config.js
+++ source/examples/generated/node/sync-changes-between-devices.codeblock.sync-changes-between-devices-sync-changes-in-the-background-create-config.js
@@ -3,11 +3,10 @@ const config = {
   sync: {
     user: app.currentUser,
     partitionValue: "MyPartitionValue",
+    // The behavior to use when this is the first time opening a realm.
+    newRealmFileBehavior: OpenRealmBehaviorConfiguration,
     // The behavior to use when a realm file already exists locally,
     // i.e. you have previously opened the realm.
-    existingRealmFileBehavior: {
-      type: "downloadBeforeOpen",
-    },
+    existingRealmFileBehavior: OpenRealmBehaviorConfiguration,
   },
 };
-const realm = await Realm.open(config);
