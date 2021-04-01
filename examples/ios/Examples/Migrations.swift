13ea7d60ceb0a23862133a1d7b17edcf2cd03620
Chris Bush <chris.bush@mongodb.com>
2021-04-01 15:41:53 -0400

    (DOCSP-14652): Update migrations examples

--- examples/ios/Examples/Migrations.swift
+++ examples/ios/Examples/Migrations.swift
@@ -1,11 +1,16 @@
 // :replace-start: {
 //   "terms": {
+//     "MigrationExampleV0_": "",
 //     "MigrationExampleV1_": "",
 //     "MigrationExampleV2_": ""
 //   }
 // }
 import XCTest
 import RealmSwift
+
+class MigrationExampleV0_Person: Object {
+    @objc dynamic var yearsSinceBirth = 0
+}
 
 // :code-block-start: model-v1
 // In the first version of the app, the Person model
@@ -65,5 +70,25 @@ class Migrations: XCTestCase {
         // Quash unused variable warning
         XCTAssert(realm.isEmpty)
     }
+
+    func testRenameProperty() {
+        // :code-block-start: rename-property
+        let config = Realm.Configuration(
+            // :hide-start:
+            // Prevent schema version from affecting other unit tests
+            inMemoryIdentifier: "LocalMigrationExample",
+            // :hide-end:
+            schemaVersion: 1,
+            migrationBlock: { migration, oldSchemaVersion in
+                // We haven’t migrated anything yet, so oldSchemaVersion == 0
+                if oldSchemaVersion < 1 {
+                    // The renaming operation should be done outside of calls to `enumerateObjects(ofType: _:)`.
+                    migration.renameProperty(onType: MigrationExampleV0_Person.className(), from: "yearsSinceBirth", to: "age")
+                }
+            })
+        // :code-block-end: rename-property
+        print(config)
+    }
+
 }
 // :replace-end:
