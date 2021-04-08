d378627f50f1a7c45091b16c9fb9aacc718f2dc5
Dachary Carey <dachary.carey@mongodb.com>
2021-04-08 11:00:17 -0400

    Update example to use two code snippets, refine prose

--- examples/ios/Examples/MutableSetExample.swift
+++ examples/ios/Examples/MutableSetExample.swift
@@ -7,31 +7,14 @@
 import XCTest
 import RealmSwift
 
-// class MutableSetExamples_Dog: Object {
-//    @objc dynamic var name = ""
-//    @objc dynamic var breed: String?
-// }
-//
-// class MutableSetExamples_Person: Object {
-//    @objc dynamic var id = 0
-//    @objc dynamic var name = ""
-//
-//    // To-many relationship - a person can have many dogs
-//    let dogs = List<MutableSetExamples_Dog>()
-//
-//    override static func primaryKey() -> String? {
-//        return "id"
-//    }
-// }
-
-// :code-block-start: set-collections
+// :code-block-start: set-collections-model
 class MutableSetExamples_Dog: Object {
     @objc dynamic var name = ""
     @objc dynamic var currentCity = ""
     let citiesVisited = MutableSet<String>()
 }
+// :code-block-end:
 
-// :hide-start:
 class MutableSetExample: XCTestCase {
 
     override func tearDown() {
@@ -40,9 +23,9 @@ class MutableSetExample: XCTestCase {
             realm.deleteAll()
         }
     }
-// :hide-end:
-    func testMutableSet() {
 
+    func testMutableSet() {
+        // :code-block-start: set-collections-example
         let realm = try! Realm()
 
         // Record a dog's name and current city
@@ -57,79 +40,25 @@ class MutableSetExample: XCTestCase {
             dog.citiesVisited.insert(dog.currentCity)
         }
 
-        // Later, retreive the dog
-        let dogs = realm.objects(MutableSetExamples_Dog.self)
-        let specificDog = dogs.filter("name == 'Maui'").first!
- 
-        // Update his current city, and add it to the set of cities visited
+        // Later... update the dog's current city, and add it to the set of cities visited
         try! realm.write {
-            specificDog.currentCity = "Toronto"
-            specificDog.citiesVisited.insert(specificDog.currentCity)
+            dog.currentCity = "Toronto"
+            dog.citiesVisited.insert(dog.currentCity)
         }
         
         // Send him back to New York
         try! realm.write {
-            specificDog.currentCity = "New York"
-            specificDog.citiesVisited.insert(specificDog.currentCity)
+            dog.currentCity = "New York"
+            dog.citiesVisited.insert(dog.currentCity)
         }
         
         // Get some information about the cities he has visited
         // This tells us he has visited New York and Toronto.
-        print(specificDog.citiesVisited)
+        print(dog.citiesVisited)
         // Because 'citiesVisited' is a MutableSet, you only see
         // New York listed once, even though he has been there twice
-        XCTAssert(specificDog.citiesVisited.count == 2)
+        XCTAssert(dog.citiesVisited.count == 2)
+        // :code-block-end:
     }
-    // :code-block-end:
 }
-
 // :replace-end:
-
-// Create some dogs of specific breeds. Two of the dogs are the same breed.
-//        let dog1 = MutableSetExamples_Dog()
-//        dog1.name = "Alpha"
-//        dog1.breed = "Beagle"
-//
-//        let dog2 = MutableSetExamples_Dog()
-//        dog2.name = "Beta"
-//        dog2.breed = "Labrador Retriever"
-//
-//        let dog3 = MutableSetExamples_Dog()
-//        dog3.name = "Charlie"
-//        dog3.breed = "Beagle"
-//
-//        // Create a relationship between the dogs and a specific person
-//        let person = MutableSetExamples_Person()
-//        person.id = 12345
-//        person.dogs.append(objectsIn: [dog1, dog2, dog3])
-//
-//        try! realm.write {
-//            realm.add(person)
-//        }
-//
-//        // Later, query the specific person
-//        let specificPerson = realm.object(ofType: MutableSetExamples_Person.self, forPrimaryKey: 12345)
-//
-//        // Create a Realm MutableSet of type String to store the dog breeds
-//        let specificPersonsDogBreeds = MutableSet<String>()
-//
-//        // Iterate through the person's dogs, and add each breed to the set
-//        for dog in specificPerson!.dogs {
-//            specificPersonsDogBreeds.insert(dog.breed!)
-//        }
-//
-//        // Get some details about the person's dogs.
-//        // In this example, the person should have 3 dogs
-//        print("# dogs: \(specificPerson!.dogs.count)")
-//        XCTAssert(specificPerson!.dogs.count == 3)
-//
-//        // But the number of breeds in the set should only be 2
-//        // Printing them yields Labrador Retriever and Beagle
-//        print("Dog breeds: \(specificPersonsDogBreeds)")
-//        XCTAssert(specificPersonsDogBreeds.count == 2)
-//
-//        // Check whether the set contains a specific value, and do something
-//        // In this example, we'll see "At least one of this person's dogs is a Beagle"
-//        if specificPersonsDogBreeds.contains("Beagle") {
-//            print("At least one of this person's dogs is a Beagle")
-//        }
