5d6cdd158a75e8d914bc7dfce75fcedc164119c4
Chris Bush <chrbsh@gmail.com>
2021-04-08 15:19:10 -0400

    (DOCSP-14636): Update threading with thread-safe reference docs (#970)
    * (DOCSP-14636): Update threading with thread-safe reference docs
    
    * Add realm sharing
    
    * (DOCSP-12993): Import extension tip from cookbook
    
    * Resolve within transaction
    
    * Update source/sdk/ios/advanced-guides/threading.txt
    
    Co-authored-by: nate contino <nathan.contino@10gen.com>
    
    Co-authored-by: nate contino <nathan.contino@10gen.com>

--- examples/ios/Examples/Threading.swift
+++ examples/ios/Examples/Threading.swift
@@ -1,7 +1,57 @@
+// :replace-start: {
+//   "terms": {
+//     "ThreadingExamples_": "",
+//     "fileprivate ": ""
+//   }
+// }
 import XCTest
 import RealmSwift
 
+class ThreadingExamples_Person: Object {
+    @objc dynamic var name = ""
+
+    convenience init(name: String) {
+        self.init()
+        self.name = name
+    }
+}
+
+class ThreadingExamples_Email: Object {
+    @objc dynamic var read = false
+}
+
+// :code-block-start: write-async-extension
+fileprivate extension Realm {
+    func writeAsync<T: ThreadConfined>(_ passedObject: T, errorHandler: @escaping ((_ error: Swift.Error) -> Void) = { _ in return }, block: @escaping ((Realm, T?) -> Void)) {
+        let objectReference = ThreadSafeReference(to: passedObject)
+        let configuration = self.configuration
+        DispatchQueue(label: "background").async {
+            autoreleasepool {
+                do {
+                    let realm = try Realm(configuration: configuration)
+                    try realm.write {
+                        // Resolve within the transaction to ensure you get the latest changes from other threads
+                        let object = realm.resolve(objectReference)
+                        block(realm, object)
+                    }
+                } catch {
+                    errorHandler(error)
+                }
+            }
+        }
+    }
+}
+// :code-block-end:
+
 class Threading: XCTestCase {
+    override func setUp() {
+        Realm.Configuration.defaultConfiguration = Realm.Configuration(inMemoryIdentifier: self.name)
+    }
+
+    override func tearDown() {
+        Realm.Configuration.defaultConfiguration = Realm.Configuration()
+    }
+
     func testFreeze() {
         // :code-block-start: freeze
         let realm = try! Realm()
@@ -40,4 +90,55 @@ class Threading: XCTestCase {
         assert(frozenTask.realm!.isFrozen)
         // :code-block-end:
     }
+
+    func testPassAcrossThreads() {
+        let expectation = XCTestExpectation(description: "it completes")
+        // :code-block-start: pass-across-threads
+        let person = ThreadingExamples_Person(name: "Jane")
+        let realm = try! Realm()
+
+        try! realm.write {
+            realm.add(person)
+        }
+
+        // Create thread-safe reference to person
+        let personRef = ThreadSafeReference(to: person)
+
+        // Pass the reference to a background thread
+        DispatchQueue(label: "background").async {
+            autoreleasepool {
+                let realm = try! Realm()
+                try! realm.write {
+                    // Resolve within the transaction to ensure you get the latest changes from other threads
+                    guard let person = realm.resolve(personRef) else {
+                        return // person was deleted
+                    }
+                    person.name = "Jane Doe"
+                }
+                expectation.fulfill() // :remove:
+            }
+        }
+        // :code-block-end:
+        wait(for: [expectation], timeout: 10)
+    }
+
+    func testWriteAsyncExtension() {
+        let expectation = XCTestExpectation(description: "it completes")
+        // :code-block-start: use-write-async-extension
+        let realm = try! Realm()
+        let readEmails = realm.objects(ThreadingExamples_Email.self).filter("read == true")
+        realm.writeAsync(readEmails) { (realm, readEmails) in
+            guard let readEmails = readEmails else {
+                // Already deleted
+                expectation.fulfill() // :remove:
+                return
+            }
+            realm.delete(readEmails)
+            expectation.fulfill() // :remove:
+        }
+        // :code-block-end:
+        wait(for: [expectation], timeout: 10)
+    }
 }
+
+// :replace-end:
