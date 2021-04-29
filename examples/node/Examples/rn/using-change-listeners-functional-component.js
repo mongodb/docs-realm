81e103de3f7e211375603d569a46f85aa2457c83
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2021-04-29 11:46:26 -0400

    added emphasize to non-generated file

--- examples/node/Examples/rn/using-change-listeners-functional-component.js
+++ examples/node/Examples/rn/using-change-listeners-functional-component.js
@@ -4,8 +4,9 @@ import Realm from 'realm';
 // :code-block-start: using-change-listeners-functional-component
 const TaskList = () => {
   const [tasks, setTasks] = useState([]);
-
+  // :emphasize-start:
   useEffect(() => {
+  // :emphasize-end:
     Realm.open({
       schema: [TaskSchema], // predefined schema
     }).then(realm => {
@@ -14,10 +15,12 @@ const TaskList = () => {
       // set state to the initial value of your realm objects
       setTasks([...tasks]);
 
+      // :emphasize-start:
       tasks.addListener(() => {
         // update state of tasks to the updated value
         setTasks([...tasks]);
       });
+      // :emphasize-end:
 
       realm.write(() => {
         // the following tasks will trigger the change listener and update the UI
@@ -30,10 +33,12 @@ const TaskList = () => {
       });
 
       // cleanup function
+      // :emphasize-start:
       return () => {
         const tasks = realm.objects('Task');
         // Remember to remove the listener when you're done!
         tasks.removeAllListeners();
+      // :emphasize-end:
         // Call the close() method when done with a realm instance to avoid memory leaks.
         realm.close();
       };
