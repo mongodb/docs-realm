f2bd0047b9ad2a63363a412ceb82afb8ab62bddc
Dachary Carey <dachary.carey@mongodb.com>
2021-05-04 17:36:19 -0400

    Expand AnyRealmValue example to include additional types, such as none and object

--- source/examples/generated/code/start/AnyRealmValue.codeblock.mixed-data-type.swift
+++ source/examples/generated/code/start/AnyRealmValue.codeblock.mixed-data-type.swift
@@ -3,16 +3,24 @@ let myDog = Dog()
 myDog.name = "Rex"
 // Because we declared age as a `RealmProperty`, we must mutate its `value` property
 myDog.age.value = .int(5)
+// This dog has no companion.
+// You can set the field's type to "none", which represents `nil`
+myDog.companion.value = .none
 
 // Create another Dog object whose age is a float
 let myOtherDog = Dog()
 myOtherDog.name = "Spot"
 myOtherDog.age.value = .float(2.5)
+// This dog's companion is a cat named Fluffy, represented by a string here
+myOtherDog.companion.value = .string("Fluffy the Cat")
 
 // Create a third Dog object whose age is a string
 let myThirdDog = Dog()
 myThirdDog.name = "Fido"
-myOtherDog.age.value = .string("3 and 3/4")
+myThirdDog.age.value = .string("3 months")
+// This dog's companion is a Person, which is an object that has a
+// string name, an optional string address, and an int age
+myThirdDog.companion.value = .object(Person(value: ["name": "Sylvia", "age": 12]))
 
 // Get the default realm. You only need to do this once per thread.
 let realm = try! Realm()
@@ -34,4 +42,12 @@ if case let .int(age) = myDog.age.value {
 // before trying to use it avoids an exception
 if case let .int(age) = myOtherDog.age.value {
     print(age)  // Doesn't print anything
+}
+
+// Retreiving an object stored as an ``AnyRealmProperty`` gives you
+// the complete object, containing all of the object's properties
+if case let .object(companion) = myThirdDog.companion.value {
+    print(companion)
+    // Prints all the details of the Person object:
+    // {name = Sylvia; address = null; age = 12;}
 }
