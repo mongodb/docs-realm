c22f3b6743255c2e6d408dc5c963b4f416c7e1c6
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2021-05-11 16:16:18 -0400

    added uuid examples

--- examples/node/Examples/data-types.js
+++ examples/node/Examples/data-types.js
@@ -51,7 +51,11 @@ describe("Node.js Data Types", () => {
     realm.write(() => {
       realm.create("Profile", {
         name: "John Doe.",
-        _id: new UUID(),
+        _id: new UUID(), // create a _id with a randomly generated UUID
+      });
+      realm.create("Profile", {
+        name: "Tim Doe.",
+        _id: new UUID("882dd631-bc6e-4e0e-a9e8-f07b685fec8c"), // create a _id with a specific UUId value
       });
     });
     // :code-block-end:
