a51bb7cb2e410bc75063bf174d3c96d04a41240d
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2021-05-10 23:47:51 -0400

    removed unneeded file

--- source/examples/generated/node/data-types.codeblock.set-additional-fields.js
+++
@@ -1,4 +0,0 @@
-// use the `put()` method to set a new field, yearBuilt, to the yakimaCity home
-yakimaCity.home.put({ yearBuilt: 1993 });
-// alternatively, use dot notation to add a new field, price, to the yakimaCity home
-yakimaCity.home.price = 512400;
