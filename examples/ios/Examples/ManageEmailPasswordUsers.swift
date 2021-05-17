e3136e0a38ab17e15872e432d047023c0d736dfb
Dachary Carey <dachary.carey@mongodb.com>
2021-05-17 13:17:15 -0400

    Add comments to code example, prevent objc test from clobbering swift test with the same name

--- examples/ios/Examples/ManageEmailPasswordUsers.swift
+++ examples/ios/Examples/ManageEmailPasswordUsers.swift
@@ -106,20 +106,26 @@ class ManageEmailPasswordUsers: XCTestCase {
     }
 
     func testPasswordResetFunc() {
-        var expectation = XCTestExpectation()
+        let expectation = XCTestExpectation()
         // :code-block-start: password-reset-function
         let app = App(id: YOUR_REALM_APP_ID)
         let client = app.emailPasswordAuth
 
         let email = "forgot.my.password@example.com"
         let newPassword = "mynewpassword12345"
+        // The password reset function takes any number of
+        // arguments. You might ask the user to provide answers to
+        // security questions, for example, to verify the user
+        // should be able to complete the password reset.
         let args: [AnyBSON] = []
 
+        // This SDK call maps to the custom password reset
+        // function that you define in the backend
         client.callResetPasswordFunction(email: email, password: newPassword, args: args) { (error) in
             guard error == nil else {
                 print("Password reset failed: \(error!.localizedDescription)")
                 // :hide-start:
-                XCTAssertEqual(error!.localizedDescription, "failed to reset password for user")
+                XCTAssertEqual(error!.localizedDescription, "user not found")
                 expectation.fulfill()
                 // :hide-end:
                 return
--- source/examples/generated/code/start/ManageEmailPasswordUsers.codeblock.password-reset-function.swift
+++ source/examples/generated/code/start/ManageEmailPasswordUsers.codeblock.password-reset-function.swift
@@ -3,8 +3,14 @@ let client = app.emailPasswordAuth
 
 let email = "forgot.my.password@example.com"
 let newPassword = "mynewpassword12345"
+// The password reset function takes any number of
+// arguments. You might ask the user to provide answers to
+// security questions, for example, to verify the user
+// should be able to complete the password reset.
 let args: [AnyBSON] = []
 
+// This SDK call maps to the custom password reset
+// function that you define in the backend
 client.callResetPasswordFunction(email: email, password: newPassword, args: args) { (error) in
     guard error == nil else {
         print("Password reset failed: \(error!.localizedDescription)")
