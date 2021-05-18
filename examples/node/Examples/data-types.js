5f43200dd279c5cad51ee5a5c131c83796201217
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2021-05-18 14:02:12 -0400

    (DOCSP-14577): UUID (#1067)
    * bump realmjs to 10.5.0-beta-1
    
    * removed uuid as it's own page and added a subsection on it
    
    * added uuid bluehawked snippet + readded uuid to toc
    
    * added uuid examples
    
    * removed uuid from field types as a paragraph
    
    * update wording
    
    * update wording
    
    * fix passive voice
    
    * add specific uuid example
    
    * removed innacurate collections are homogenous (per mixed)

--- examples/node/Examples/data-types.js
+++ examples/node/Examples/data-types.js
@@ -194,6 +194,7 @@ describe("Node.js Data Types", () => {
     const Blaise = realm.objects("Dog").filtered(`name = 'Blaise'`)[0];
     const Euclid = realm.objects("Dog").filtered(`name = 'Euclid'`)[0];
     const Pythagoras = realm.objects("Dog").filtered(`name = 'Pythagoras'`)[0];
+    // delete the objects to keep the tests idempotent
     realm.write(() => {
       realm.delete(Euler);
       realm.delete(Blaise);
@@ -295,6 +296,46 @@ describe("Node.js Data Types", () => {
     // delete the object specifically created in this test to keep tests idempotent
     realm.write(() => {
       realm.delete(harryPotter);
+    });
+    // close the realm
+    realm.close();
+  });
+  test("should work with UUID", async () => {
+    // :code-block-start: work-with-uuid
+    const { UUID } = Realm.BSON;
+    const ProfileSchema = {
+      name: "Profile",
+      primaryKey: "_id",
+      properties: {
+        _id: "uuid",
+        name: "string",
+      },
+    };
+    const realm = await Realm.open({
+      schema: [ProfileSchema],
+    });
+    realm.write(() => {
+      realm.create("Profile", {
+        name: "John Doe.",
+        _id: new UUID(), // create a _id with a randomly generated UUID
+      });
+      realm.create("Profile", {
+        name: "Tim Doe.",
+        _id: new UUID("882dd631-bc6e-4e0e-a9e8-f07b685fec8c"), // create a _id with a specific UUID value
+      });
+    });
+    // :code-block-end:
+
+    const johnDoeProfile = realm
+      .objects("Profile")
+      .filtered("name = 'John Doe.'")[0];
+
+    // test if johnDoeProfile's _id is a valid UUID field
+    expect(UUID.isValid(johnDoeProfile._id)).toBe(true);
+
+    // delete the objects to keep the tests idempotent
+    realm.write(() => {
+      realm.delete(johnDoeProfile);
     });
     // close the realm
     realm.close();
