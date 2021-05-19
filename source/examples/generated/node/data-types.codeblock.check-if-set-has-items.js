5d8338d57e820d6cfb85a1fa6b444d737bf8c9fd
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2021-05-19 09:04:46 -0400

    fix grammar + wording + changed Set to Realm.Set in js snippets

--- source/examples/generated/node/data-types.codeblock.check-if-set-has-items.js
+++ source/examples/generated/node/data-types.codeblock.check-if-set-has-items.js
@@ -1,4 +1,4 @@
-// check if the characterTwo has completed level 3 by calling the `set.has()` method
+// check if the characterTwo has completed level 3 by calling the `Realm.Set.has()` method
 const characterTwoHasCompletedLevelThree = characterTwo.levelsCompleted.has(3);
 console.log(
   `Is level three completed by the characterTwo: ${characterTwoHasCompletedLevelThree}`
