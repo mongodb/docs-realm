c72bbfcc5eff595b00db10bd2cf85a68ef374165
nathan-contino-mongo <nathan.contino@mongodb.com>
2021-05-20 10:58:03 -0600

    Android Tutorial: Failing Upwards

--- source/tutorial/generated/code/final/ProjectActivity.codeblock.fetch-synced-user-safely.kt
+++ source/tutorial/generated/code/final/ProjectActivity.codeblock.fetch-synced-user-safely.kt
@@ -1,3 +1,2 @@
-val syncedUsers : RealmResults<User> = realm.where<User>().sort("_id").findAll()
+val syncedUsers : RealmResults<User> = realm.where<User>().sort("id").findAll()
 val syncedUser : User? = syncedUsers.getOrNull(0) // since there might be no user objects in the results, default to "null"
-Log.v(TAG(), "Synced users length: ${syncedUsers.size}")
