8a5fa13b40903ec58df41fb5457ad20f9ed12205
Chris Bush <chris.bush@mongodb.com>
2021-06-01 14:21:30 -0400

    (DOCSP-14564): Document Map (Dictionary) type

---
+++ source/examples/generated/code/start/MapExamples.codeblock.map.swift
@@ -0,0 +1,35 @@
+let realm = try! Realm()
+// Record a dog's name and current city
+let dog = Dog()
+dog.name = "Wolfie"
+dog.currentCity = "New York"
+// Store the data in a realm
+try! realm.write {
+    realm.add(dog)
+    // Set values
+    dog.favoriteParksByCity["New York"] = "Domino Park"
+    dog.favoriteParksByCity["Chicago"] = "Wiggly Field"
+    // Another way to set values
+    dog.favoriteParksByCity.setValue("Bush Park", forKey: "Ottawa")
+
+    // Overwrite a value
+    dog.favoriteParksByCity["New York"] = "Washington Square Park"
+}
+
+// Check if an entry exists
+if dog.favoriteParksByCity.keys.contains("Chicago") {
+    print("\(dog.name)' has a favorite park in Chicago")
+}
+
+// Iterate over entries
+for element in dog.favoriteParksByCity {
+    print("\(dog.name)'s favorite park in \(element.key) is \(element.value)")
+}
+
+// Delete an entry
+try! realm.write {
+    // Use removeObject(for:)
+    dog.favoriteParksByCity.removeObject(for: "New York")
+    // Or assign `nil` to delete
+    dog.favoriteParksByCity["New York"] = nil
+}
