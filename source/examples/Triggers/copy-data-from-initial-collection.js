2eb7c2e3c5b86add611b3901bc60ec02f444d197
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2021-06-16 14:31:22 -0400

    added updated trigger fns

--- source/examples/Triggers/copy-data-from-initial-collection.js
+++ source/examples/Triggers/copy-data-from-initial-collection.js
@@ -1,9 +1,23 @@
-
 exports = async function(changeEvent) {
-    const db = context.services.get("<Your Mongodb Atlas Service Name>").db("<Your Database Name>"); // replace with your Atlas Service Name and your database name
-    const taskCollection = db.collection("Task");
-    const taskV2Collection = db.collection("TaskV2");
-    const docId = changeEvent.documentKey._id; // _id of changed or inserted document
-    const changedItem = await taskCollection.findOne({_id: docId}); // find the changed or inserted documented
-    await taskV2Collection.insertOne(changedItem); // insert the changed or inserted document into its partnet collection
-};
+    const db = context.services.get("mongodb-atlas").db("synced-schema-changes-database");
+    const collection = db.collection("Task");
+    
+      // if the Task object has been deleted, delete the adjacent object in the TaskV2 collection
+    if(changeEvent.operationType === "delete") {
+      const tasksV2Collection = db.collection("TaskV2");
+      return tasksV2Collection.deleteOne({ _id: changeEvent.documentKey._id.toString()})
+    }
+    
+    //  if the Task object has been created, modified, or replaced, do the same to the adjacent object in the TaskV2 collection
+    const pipeline = [
+      { $match: { _id: changeEvent.documentKey._id } },
+      {
+        $addFields: {
+          _id: { $toString: "$_id" },
+        },
+      },
+      { $merge: "TaskV2" }
+    ]
+    return collection.aggregate(pipeline);
+  };
+  
--- source/examples/Triggers/copy-data-from-second-version-collection.js
+++ source/examples/Triggers/copy-data-from-second-version-collection.js
@@ -1,8 +1,23 @@
 exports = async function(changeEvent) {
-    const db = context.services.get("<Your Mongodb Atlas Service Name>").db("<Your Database Name>"); // replace with your Atlas Service Name and your database name
-    const taskV2Collection = db.collection("TaskV2");
-    const taskCollection = db.collection("Task");
-    const docId = changeEvent.documentKey._id; // _id of changed or inserted document
-    const changedItem = await taskV2Collection.findOne({_id: docId}); // find the changed or inserted documented
-    await taskCollection.insertOne(changedItem); // insert the changed or inserted document into its partnet collection
- };
+    const db = context.services.get("mongodb-atlas").db("synced-schema-changes-database");
+    const collection = db.collection("TaskV2");
+    
+      // if the TaskV2 object has been deleted, delete the adjacent object in the Task collection
+    if(changeEvent.operationType === "delete") {
+      const taskCollection = db.collection("Task");
+      return taskCollection.deleteOne({ _id: parseInt(changeEvent.documentKey._id)})
+    }
+    
+    //  if the TaskV2 object has been created, modified, or replaced, do the same to the adjacent object in the Task collection
+    const pipeline = [
+      { $match: { _id: String(changeEvent.documentKey._id) } },
+      {
+        $addFields: {
+          _id: { $toInt: "$_id" },
+        },
+      },
+      { $merge: "Task" }
+    ]
+    return collection.aggregate(pipeline);
+  };
+  
