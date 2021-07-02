45d8f92b6261e68baac74a2a723597f7d5628f2c
nathan-contino-mongo <nathan.contino@mongodb.com>
2021-07-02 11:49:20 -0600

    nabil comments

--- examples/kmm/androidApp/build.gradle.kts
+++ examples/kmm/androidApp/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    compileOnly("io.realm.kotlin:library:0.3.1")
 }
 
 android {
