db6773e57491c00f07263b81f83f02794e0a7979
Diego Freniche <diego@freniche.com>
2021-07-05 18:29:39 +0200

    Added Swift file to get Swift standard library

---
+++ tutorial/rn/ios/File.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  rn
+//
+//  Created by Diego Freniche Brito on 5/7/21.
+//
+
+// We need to add a .swift file to the project to embed the swift standard libs.
+// https://github.com/facebookarchive/react-native-fbsdk/issues/755#issuecomment-634101710
+
+import Foundation
