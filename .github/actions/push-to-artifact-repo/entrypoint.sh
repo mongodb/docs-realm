29f667f3b6be8705cc0b38f40552124e1eb97152
Chris Bush <chris.bush@mongodb.com>
2021-07-06 12:03:27 -0400

    Update arty-fact to include link to commit

--- .github/actions/push-to-artifact-repo/entrypoint.sh
+++ .github/actions/push-to-artifact-repo/entrypoint.sh
@@ -5,6 +5,12 @@ npm i -g bluehawk bluehawk-plugin-git
 git config --global user.email "$INPUT_EMAIL"
 git config --global user.name "$INPUT_NAME"
 
+if [ -z "$INPUT_COMMITMESSAGE" ]; then
+  COMMITMESSAGE="Update"
+else
+  COMMITMESSAGE="$INPUT_COMMITMESSAGE"
+fi
+
 for state in $INPUT_STATES; do
   bluehawk \
     --plugin "`bluehawk-plugin-git`" \
@@ -13,5 +19,6 @@ for state in $INPUT_STATES; do
     --state $state \
     --branch $state \
     --delete-everything \
+    --commit-message "$COMMITMESSAGE" \
     "$INPUT_SOURCE"
 done
