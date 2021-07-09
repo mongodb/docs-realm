866e9f78fddbcd21383adb6c1b97a5d467a4a4eb
Chris Bush <chrbsh@gmail.com>
2021-07-08 20:20:49 -0400

    Remove yarn.lock and update web tutorial for realm-web 1+ (#1214)

--- tutorial/web/src/components/EditPermissionsModal.js
+++ tutorial/web/src/components/EditPermissionsModal.js
@@ -17,7 +17,7 @@ function useTeamMembers() {
   const [newUserEmailError, setNewUserEmailError] = React.useState(null);
   const app = useRealmApp();
   // :state-start: final
-  const { addTeamMember, removeTeamMember, getMyTeamMembers } = app.functions;
+  const { addTeamMember, removeTeamMember, getMyTeamMembers } = app.currentUser.functions;
   // :state-end: :state-uncomment-start: start
   // // TODO: Import the Realm functions: addTeamMember, removeTeamMember, and getMyTeamMembers
   // :state-uncomment-end:
