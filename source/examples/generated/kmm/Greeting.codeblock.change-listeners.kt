1a3a4a64d57da2ad95c853c79237c29eb81bf790
nate contino <nathan.contino@mongodb.com>
2021-07-14 11:00:15 -0600

    Christian/Nabil feedback on Kotlin Multiplatform 0.4.0 docs update (#1231)

--- source/examples/generated/kmm/Greeting.codeblock.change-listeners.kt
+++ source/examples/generated/kmm/Greeting.codeblock.change-listeners.kt
@@ -1,14 +1,17 @@
 val config = RealmConfiguration(schema = setOf(Task::class))
 val realm = Realm(config)
+
 // fetch objects from a realm as Flowables
-val tasks = realm.objects(Task::class).observe()
-tasks.map { task -> print("task: $task") }
+CoroutineScope(Dispatchers.Main).launch {
+    val flow: Flow<RealmResults<Task>> = realm.objects(Task::class).observe()
+    flow.collect { task ->
+        println("Task: $task")
+    }
+}
 
 // write an object to the realm in a coroutine
-runBlocking {
-    launch(Dispatchers.Unconfined) {
-        realm.write {
-            this.copyToRealm(Task().apply { name = "my task"; status = "Open"})
-        }
+CoroutineScope(Dispatchers.Main).launch {
+    realm.write {
+        this.copyToRealm(Task().apply { name = "my task"; status = "Open"})
     }
 }
