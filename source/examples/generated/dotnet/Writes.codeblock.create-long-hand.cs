389a87499610d329f7e1ae9d19677f975c425ba2
Caleb Thompson <caleb.thompson@mongodb.com>
2021-07-28 11:59:13 -0700

    bluehawk

--- source/examples/generated/dotnet/Writes.codeblock.create-long-hand.cs
+++ source/examples/generated/dotnet/Writes.codeblock.create-long-hand.cs
@@ -1,8 +1,8 @@
 // Open a thread-safe transaction.
 var transaction = realm.BeginWrite();
-// Add the instance to the realm.
-realm.Add(myDog);
 
+// Perform a write op...
+realm.Add(myDog);
 try
 {
     // Do other work that needs to be included in
