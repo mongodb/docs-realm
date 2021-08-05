5fa6eee291bad7318058f8eb6b46f7b535fb1ba3
Dachary <dachary.carey@mongodb.com>
2021-08-05 14:48:21 -0400

    Incorporate feedback for the iOS tutorial (#1271)

--- tutorial/swift-ios/Task Tracker/ManageTeamViewController.swift
+++ tutorial/swift-ios/Task Tracker/ManageTeamViewController.swift
@@ -2,7 +2,6 @@
 //  ManageTeamViewController.swift
 //  Task Tracker
 //
-//  Created by MongoDB on 2020-07-30.
 //  Copyright © 2020-2021 MongoDB, Inc. All rights reserved.
 //
 // :code-block-start: complete
