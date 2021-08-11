81a853114759cc6f95a32189faba27dc639983a5
Caleb Thompson <caleb.thompson@mongodb.com>
2021-08-11 10:00:24 -0700

    uncomment test

--- examples/dotnet/Examples/ClientResetExamples.cs
+++ examples/dotnet/Examples/ClientResetExamples.cs
@@ -16,60 +16,60 @@ namespace Examples
         const string myRealmAppId = Config.appid;
 
 
-        /* [Test]
-         public async Task resetsTheClient()
-         {
-             app = App.Create(myRealmAppId);
+        [Test]
+        public async Task resetsTheClient()
+        {
+            app = App.Create(myRealmAppId);
 
-             user = app.LogInAsync(Credentials.EmailPassword("foo@foo.com", "foobar")).Result;
+            user = app.LogInAsync(Credentials.EmailPassword("foo@foo.com", "foobar")).Result;
 
-             config = new SyncConfiguration("myPart", user);
-             //:hide-start:
-             config.ObjectClasses = new[]
-             {
+            config = new SyncConfiguration("myPart", user);
+            //:hide-start:
+            config.ObjectClasses = new[]
+            {
                  typeof(dotnet.User)
              };
-             //:hide-end:
-             var realm = await Realm.GetInstanceAsync(config);
+            //:hide-end:
+            var realm = await Realm.GetInstanceAsync(config);
 
-             // :code-block-start: handle
-             Session.Error += (sender, err) =>
-             {
-                 if (err.Exception is ClientResetException clientResetEx)
-                 {
-                     var session = (Session)sender;
-                     Console.WriteLine("Client Reset requested for " +
-                         session.Path + "due to " + clientResetEx.Message);
+            // :code-block-start: handle
+            Session.Error += (sender, err) =>
+            {
+                if (err.Exception is ClientResetException clientResetEx)
+                {
+                    var session = (Session)sender;
+                    Console.WriteLine("Client Reset requested for " +
+                        session.Path + "due to " + clientResetEx.Message);
 
                      // Prompt user to perform client reset immediately. If they don't do it,
                      // they won't receive any data from the server until they restart the app
                      // and all changes they make will be discarded when the app restarts.
                      var didUserConfirmReset = true;
-                     if (didUserConfirmReset)
-                     {
+                    if (didUserConfirmReset)
+                    {
                          // Close the Realm before doing the reset as it'll need
                          // to be deleted and all objects obtained from it will be
                          // invalidated.
                          realm.Dispose();
-                         var didReset = clientResetEx.InitiateClientReset();
-                         if (didReset)
-                         {
+                        var didReset = clientResetEx.InitiateClientReset();
+                        if (didReset)
+                        {
                              // Navigate the user back to the main page or reopen the
                              // the Realm and reinitialize the current page.
                          }
-                         else
-                         {
+                        else
+                        {
                              // Reset failed - notify user that they'll need to restart the app.
                          }
                          // :hide-start:
                          Assert.IsTrue(didReset);
                          // :hide-end:
                      }
-                 }
-             };
-             // :code-block-end:
-             TestingExtensions.SimulateError(realm.GetSession(),
-                 ErrorCode.DivergingHistories, "diverging histories!", false);
-         }*/
+                }
+            };
+            // :code-block-end:
+            TestingExtensions.SimulateError(realm.GetSession(),
+                ErrorCode.DivergingHistories, "diverging histories!", false);
+        }
     }
 }
