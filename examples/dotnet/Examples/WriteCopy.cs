9084d4ce2dfaa52c2df9a419dc92ea81164d6c99
Caleb Thompson <caleb.thompson@mongodb.com>
2021-08-30 10:37:43 -0700

    review 1 updates

--- examples/dotnet/Examples/WriteCopy.cs
+++ examples/dotnet/Examples/WriteCopy.cs
@@ -7,7 +7,6 @@ namespace Examples
 {
     public class WriteCopy
     {
-        byte[] encryptionKey = new byte[64];
         [Test]
         public void TestWriteCopy()
         {
@@ -16,30 +15,16 @@ namespace Examples
             var realm = Realm.GetInstance("myRealm.realm");
 
             // Create a RealmConfiguration for the *copy*
-            var config = new RealmConfiguration("myRealm_copy.realm")
-            {
-                // optionally encrypt it
-                EncryptionKey = encryptionKey,
-            };
+            var config = new RealmConfiguration("myRealm_copy.realm");
+            // Make sure the file doesn't already exist
+            Realm.DeleteRealm(config);
 
             // Copy the realm
             realm.WriteCopy(config);
+
+            // Want to know where the copy is?
+            var locationOfCopy = config.DatabasePath;
             // :code-block-end:
         }
-
-        [OneTimeTearDown]
-        public void Cleanup()
-        {
-            var config = new RealmConfiguration("myRealm_copy.realm");
-            Realm.DeleteRealm(config);
-        }
     }
-
 }
-
-
-/* // Check if we already have a key stored in the platform's secure storage.
-            // If we don't, generate a new one:
-            var encryptionKey = new byte[64];
-            using var rng = new RNGCryptoServiceProvider();
-            rng.GetBytes(encryptionKey);*/
--- source/examples/generated/dotnet/WriteCopy.codeblock.copy_a_realm.cs
+++ source/examples/generated/dotnet/WriteCopy.codeblock.copy_a_realm.cs
@@ -1,12 +1,13 @@
 // open an existing realm
-var realm = Realm.GetInstance("/users/caleb/myRealm.realm");
+var realm = Realm.GetInstance("myRealm.realm");
 
 // Create a RealmConfiguration for the *copy*
-var config = new RealmConfiguration("/users/caleb/myRealm_copy.realm")
-{
-    // optionally encrypt it
-    EncryptionKey = encryptionKey,
-};
+var config = new RealmConfiguration("myRealm_copy.realm");
+// Make sure the file doesn't already exist
+Realm.DeleteRealm(config);
 
 // Copy the realm
 realm.WriteCopy(config);
+
+// Want to know where the copy is?
+var locationOfCopy = config.DatabasePath;
