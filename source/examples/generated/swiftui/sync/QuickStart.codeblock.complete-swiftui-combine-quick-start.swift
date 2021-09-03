32596e7f9813849d4cc58259ad5cc8bcfe4378f4
Dachary Carey <dachary.carey@mongodb.com>
2021-09-03 15:47:06 -0400

    Update SwiftUI quickstart and text to fix logout bug

--- source/examples/generated/swiftui/sync/QuickStart.codeblock.complete-swiftui-combine-quick-start.swift
+++ source/examples/generated/swiftui/sync/QuickStart.codeblock.complete-swiftui-combine-quick-start.swift
@@ -89,25 +89,40 @@ struct LocalOnlyContentView: View {
     }
 }
 
-// The main content view if using Sync.
+// If using Sync, this view observes the app object.
 struct SyncContentView: View {
     // Observe the Realm app object in order to react to login state changes.
     @ObservedObject var app: RealmSwift.App
-    
-    // Observe a realm that may be opened after login.
-    @State var realm: Realm?
 
     var body: AnyView {
         // If there is no user logged in, show the login view.
         guard let user = app.currentUser else {
             return AnyView(LoginView(app: app))
         }
+        
+        // If there is a user logged in, show the RealmContentView
+        return AnyView(RealmContentView(user: user, leadingBarButton: AnyView(LogoutButton(app: app))))
+    }
+}
+
+// The view for opening a realm and setting up a group
+struct RealmContentView: View {
+    // Get the user. You need this user from SyncContentView
+    // in order to open the synced realm.
+    var user: User
+
+    var leadingBarButton: AnyView?
+    
+    // Observe a realm that may be opened after login.
+    @State var realm: Realm?
+    
+    var body: AnyView {
         // If logged in but the realm is not open yet, then show a progress spinner
         // while opening the realm. Realm.asyncOpen() downloads the remote changes before
         // the realm opens, which might take a moment.
         guard let realm = realm else {
             return AnyView(ProgressView() // Show the activity indicator while the realm loads
-                .onReceive(Realm.asyncOpen(configuration: user.configuration(partitionValue: user.id)).assertNoFailure()) { realm in
+                            .onReceive(Realm.asyncOpen(configuration: user.configuration(partitionValue: user.id)).assertNoFailure()) { realm in
                     // Preload one group if it does not exist. This app only ever allows
                     // one group per user partition, but you could expand it to allow many groups.
                     if realm.objects(Group.self).count == 0 {
@@ -122,7 +137,7 @@ struct SyncContentView: View {
         // If logged in and the realm has been opened, then go to the items
         // screen for the only group in the realm.
         return AnyView(ItemsView(group: realm.objects(Group.self).first!,
-                                 leadingBarButton: AnyView(LogoutButton(app: app))))
+                                 leadingBarButton: AnyView(LogoutButton(app: app!))))
         // Pass the app to descendents via this environment object.
     }
 }
---
+++ source/examples/generated/swiftui/sync/QuickStart.codeblock.realm-content-view.swift
@@ -0,0 +1,36 @@
+// The view for opening a realm and setting up a group
+struct RealmContentView: View {
+    // Get the user. You need this user from SyncContentView
+    // in order to open the synced realm.
+    var user: User
+
+    var leadingBarButton: AnyView?
+    
+    // Observe a realm that may be opened after login.
+    @State var realm: Realm?
+    
+    var body: AnyView {
+        // If logged in but the realm is not open yet, then show a progress spinner
+        // while opening the realm. Realm.asyncOpen() downloads the remote changes before
+        // the realm opens, which might take a moment.
+        guard let realm = realm else {
+            return AnyView(ProgressView() // Show the activity indicator while the realm loads
+                            .onReceive(Realm.asyncOpen(configuration: user.configuration(partitionValue: user.id)).assertNoFailure()) { realm in
+                    // Preload one group if it does not exist. This app only ever allows
+                    // one group per user partition, but you could expand it to allow many groups.
+                    if realm.objects(Group.self).count == 0 {
+                        try! realm.write {
+                            realm.add(Group())
+                        }
+                    }
+                    // Assign the realm to the state property to trigger a view refresh.
+                    self.realm = realm
+                })
+        }
+        // If logged in and the realm has been opened, then go to the items
+        // screen for the only group in the realm.
+        return AnyView(ItemsView(group: realm.objects(Group.self).first!,
+                                 leadingBarButton: AnyView(LogoutButton(app: app!))))
+        // Pass the app to descendents via this environment object.
+    }
+}
