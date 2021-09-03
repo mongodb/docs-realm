1d2c03cf114f3f00b710d936f792396249fbfffc
Dachary Carey <dachary.carey@mongodb.com>
2021-09-03 17:14:18 -0400

    Eliminate returns and casts in RealmContentView

--- source/examples/generated/swiftui/sync/QuickStart.codeblock.complete-swiftui-combine-quick-start.swift
+++ source/examples/generated/swiftui/sync/QuickStart.codeblock.complete-swiftui-combine-quick-start.swift
@@ -110,29 +110,32 @@ struct RealmContentView: View {
     // Observe a realm that may be opened after login.
     @State var realm: Realm?
     
-    var body: AnyView {
-        // If logged in but the realm is not open yet, then show a progress spinner
-        // while opening the realm. Realm.asyncOpen() downloads the remote changes before
-        // the realm opens, which might take a moment.
-        guard let realm = realm else {
-            return AnyView(ProgressView() // Show the activity indicator while the realm loads
-                            .onReceive(Realm.asyncOpen(configuration: app!.currentUser!.configuration(partitionValue: app!.currentUser!.id)).assertNoFailure()) { realm in
-                    // Preload one group if it does not exist. This app only ever allows
-                    // one group per user partition, but you could expand it to allow many groups.
-                    if realm.objects(Group.self).count == 0 {
-                        try! realm.write {
-                            realm.add(Group())
-                        }
-                    }
-                    // Assign the realm to the state property to trigger a view refresh.
-                    self.realm = realm
-                })
+    var body: some View {
+        
+        // If there is an existing realm, use it.
+        if let realm = realm {
+            // If logged in and the realm has been opened, then go to the items
+            // screen for the only group in the realm.
+            ItemsView(group: realm.objects(Group.self).first!,
+                      leadingBarButton: AnyView(LogoutButton(app: app!)))
+            // Pass the app to descendents via this environment object.
+        } else {
+            // If logged in but the realm is not open yet, then show a progress spinner
+            // while opening the realm. Realm.asyncOpen() downloads the remote changes before
+            // the realm opens, which might take a moment.
+            ProgressView() // Show the activity indicator while the realm loads
+                .onReceive(Realm.asyncOpen(configuration: app!.currentUser!.configuration(partitionValue: app!.currentUser!.id)).assertNoFailure()) { realm in
+            // Preload one group if it does not exist. This app only ever allows
+            // one group per user partition, but you could expand it to allow many groups.
+            if realm.objects(Group.self).count == 0 {
+                try! realm.write {
+                    realm.add(Group())
+                }
+            }
+            // Assign the realm to the state property to trigger a view refresh.
+            self.realm = realm
+            }
         }
-        // If logged in and the realm has been opened, then go to the items
-        // screen for the only group in the realm.
-        return AnyView(ItemsView(group: realm.objects(Group.self).first!,
-                                 leadingBarButton: AnyView(LogoutButton(app: app!))))
-        // Pass the app to descendents via this environment object.
     }
 }
 
