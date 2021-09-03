23652ef693d53266afa244894004df0ad35a8454
Dachary <dachary.carey@mongodb.com>
2021-09-03 17:24:35 -0400

    (DOCSP-18501): Update SwiftUI quickstart to fix logout bug (#1327)

--- source/examples/generated/swiftui/sync/QuickStart.codeblock.complete-swiftui-combine-quick-start.swift
+++ source/examples/generated/swiftui/sync/QuickStart.codeblock.complete-swiftui-combine-quick-start.swift
@@ -89,41 +89,53 @@ struct LocalOnlyContentView: View {
     }
 }
 
-// The main content view if using Sync.
+// If using Sync, this view observes the app object.
 struct SyncContentView: View {
     // Observe the Realm app object in order to react to login state changes.
     @ObservedObject var app: RealmSwift.App
+
+    var body: some View {
+        // If there is no user logged in, show the login view.
+        if app.currentUser == nil {
+            LoginView(app: app)
+        } else {
+            RealmContentView()
+        }
+    }
+}
+
+// The view for opening a realm and setting up a group
+struct RealmContentView: View {
     
     // Observe a realm that may be opened after login.
     @State var realm: Realm?
-
-    var body: AnyView {
-        // If there is no user logged in, show the login view.
-        guard let user = app.currentUser else {
-            return AnyView(LoginView(app: app))
+    
+    var body: some View {
+        
+        // If there is an existing realm, use it.
+        if let realm = realm {
+            // If logged in and the realm has been opened, then go to the items
+            // screen for the only group in the realm.
+            ItemsView(group: realm.objects(Group.self).first!,
+                      leadingBarButton: AnyView(LogoutButton(app: app!)))
+            // Pass the app to descendents via this environment object.
+        } else {
+            // If logged in but the realm is not open yet, then show a progress spinner
+            // while opening the realm. Realm.asyncOpen() downloads the remote changes before
+            // the realm opens, which might take a moment.
+            ProgressView() // Show the activity indicator while the realm loads
+                .onReceive(Realm.asyncOpen(configuration: app!.currentUser!.configuration(partitionValue: app!.currentUser!.id)).assertNoFailure()) { realm in
+            // Preload one group if it does not exist. This app only ever allows
+            // one group per user partition, but you could expand it to allow many groups.
+            if realm.objects(Group.self).count == 0 {
+                try! realm.write {
+                    realm.add(Group())
+                }
+            }
+            // Assign the realm to the state property to trigger a view refresh.
+            self.realm = realm
+            }
         }
-        // If logged in but the realm is not open yet, then show a progress spinner
-        // while opening the realm. Realm.asyncOpen() downloads the remote changes before
-        // the realm opens, which might take a moment.
-        guard let realm = realm else {
-            return AnyView(ProgressView() // Show the activity indicator while the realm loads
-                .onReceive(Realm.asyncOpen(configuration: user.configuration(partitionValue: user.id)).assertNoFailure()) { realm in
-                    // Preload one group if it does not exist. This app only ever allows
-                    // one group per user partition, but you could expand it to allow many groups.
-                    if realm.objects(Group.self).count == 0 {
-                        try! realm.write {
-                            realm.add(Group())
-                        }
-                    }
-                    // Assign the realm to the state property to trigger a view refresh.
-                    self.realm = realm
-                })
-        }
-        // If logged in and the realm has been opened, then go to the items
-        // screen for the only group in the realm.
-        return AnyView(ItemsView(group: realm.objects(Group.self).first!,
-                                 leadingBarButton: AnyView(LogoutButton(app: app))))
-        // Pass the app to descendents via this environment object.
     }
 }
 
