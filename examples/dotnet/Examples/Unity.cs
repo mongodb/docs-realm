689bba0094b544507a37b57f6868cefa81bf012b
MongoCaleb <32645888+MongoCaleb@users.noreply.github.com>
2021-09-10 14:12:20 -0700

    Add page about copying & bundling a realm in .NET (#1310)

---
+++ examples/dotnet/Examples/Unity.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using Realms;
+using Realms.Sync;
+using System.IO;
+using System.Threading.Tasks;
+
+public class Cube
+{
+    private async void ReadCopy()
+    {
+        var app = App.Create("foo");
+        var user = await app.LogInAsync(Credentials.Anonymous());
+        // :code-block-start: read_a_realm_unity
+        // After copying the above created file to the project folder,
+        // we can access it in Application.dataPath:
+        //:uncomment-start:
+        //var realmPath = Path.Combine(Application.dataPath, "bundled.realm");
+        // // And then we open it like any other realm:
+        //var realm = await Realm.GetInstanceAsync(realmPath);
+        //:uncomment-end:
+        // :code-block-end:
+    }
+}
