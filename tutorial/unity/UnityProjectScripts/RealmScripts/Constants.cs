e851ed67502eb052f251365921698aa2e2d2a7e8
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2021-09-16 08:27:44 -0400

    removed Gameplay + Mechanics since they are having issues when importing them to the unity project since theres already classes with those names

--- tutorial/unity/UnityProjectScripts/RealmScripts/Constants.cs
+++ tutorial/unity/UnityProjectScripts/RealmScripts/Constants.cs
@@ -1,16 +1,16 @@
-// :code-block-start: create-constants-sync
-// :state-start: sync
 sealed class Constants
 {
     public sealed class Realm
     {
+        // :code-block-start: create-constants-sync
+        // :state-start: sync
         // :remove-start:
         public const string AppId = "unity-tutorial-3-kgzvn";
         // :remove-end: 
         // :state-uncomment-start: sync 
         // public const string AppId = "<your-realm-app-ID-here>";
         // :state-uncomment-end:
+        // :state-end: sync
+        // :code-block-end:
     }
 }
-// :state-end: sync
-// :code-block-end:
