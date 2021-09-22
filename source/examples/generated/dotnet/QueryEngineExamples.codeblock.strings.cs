b69dbdf7ec31bf02830c2a03b84685e2897d34f1
Caleb Thompson <caleb.thompson@mongodb.com>
2021-09-22 14:50:42 -0700

    putting back the Where methods

--- source/examples/generated/dotnet/QueryEngineExamples.codeblock.strings.cs
+++ source/examples/generated/dotnet/QueryEngineExamples.codeblock.strings.cs
@@ -5,17 +5,17 @@
 // Last(), or LastOrDefault().
 
 // Get all tasks where the Assignee's name starts with "E" or "e"
-var tasksStartWitE = tasks.Last(t => t.Assignee.StartsWith("E",
+var tasksStartWitE = tasks.Where(t => t.Assignee.StartsWith("E",
     StringComparison.OrdinalIgnoreCase));
 
 // Get all tasks where the Assignee's name ends wth "is"
 // (lower case only)
-var endsWith = tasks.SingleOrDefault(t =>
+var endsWith = tasks.Where(t =>
     t.Assignee.EndsWith("is", StringComparison.Ordinal));
 
 // Get all tasks where the Assignee's name contains the
 // letters "ami" in any casing
-var tasksContains = tasks.FirstOrDefault(t => t.Assignee.Contains("ami",
+var tasksContains = tasks.Where(t => t.Assignee.Contains("ami",
      StringComparison.OrdinalIgnoreCase));
 
 // Get all tasks that have no assignee
