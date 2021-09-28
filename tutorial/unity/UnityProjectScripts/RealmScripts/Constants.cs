63e0fe9f930b4d0c3bb81273743262543581ec53
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2021-09-28 08:58:14 -0400

    added replace start

--- tutorial/unity/UnityProjectScripts/RealmScripts/Constants.cs
+++ tutorial/unity/UnityProjectScripts/RealmScripts/Constants.cs
@@ -3,12 +3,11 @@ sealed class Constants
     public sealed class Realm
     {
         // :code-block-start: create-constants-sync
-        // :remove-start:
+        // :replace-start: {
+        //   "terms": { "unity-tutorial-3-kgzvn": "<your-realm-app-ID-here>" }
+        // }
         public const string AppId = "unity-tutorial-3-kgzvn";
-        // :remove-end: 
-        // :uncomment-start: 
-        // public const string AppId = "<your-realm-app-ID-here>";
-        // :uncomment-end:
+        // :replace-end:
         // :code-block-end:
     }
 }
