9324bb50e7a4b0b12813a5451af4433e3bf823b3
Dachary <dachary.carey@mongodb.com>
2021-09-30 11:34:10 -0400

    (DOCSP-17883): [iOS] Async-await doc updates and code examples for 10.16.0 release (#1374)
    * Draft async-await updates and code examples for 10.16.0 release
    * Update docs with release version and API links

--- source/examples/generated/code/start/ManageEmailPasswordUsers.codeblock.retry-confirmation-function.swift
+++ source/examples/generated/code/start/ManageEmailPasswordUsers.codeblock.retry-confirmation-function.swift
@@ -3,12 +3,11 @@ let client = app.emailPasswordAuth
 let email = "skroob@example.com"
 // If Realm is set to run a custom confirmation function,
 // we can trigger that function to run again here.
-client.retryCustomConfirmation(email) { (error) in
-    guard error == nil else {
-        print("Failed to retry custom confirmation: \(error!.localizedDescription)")
-        return
-    }
+do {
+    try await client.retryCustomConfirmation(email)
     // The custom confirmation function has been
     // triggered to run again for the user.
     print("Custom confirmation retriggered")
+} catch {
+    print("Failed to retry custom confirmation: \(error.localizedDescription)")
 }
