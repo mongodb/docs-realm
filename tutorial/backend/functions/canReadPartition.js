e8042c33013975fda4aeb34693dc101e079dc5bf
Chris Bush <chrbsh@gmail.com>
2021-10-11 14:56:30 -0400

    (DOCSP-13881): Remove custom user data in Realm permissions functions (#1407)
    - User data is potentially stale here
    - Instead, fetch latest from database

--- tutorial/backend/functions/canReadPartition.js
+++ tutorial/backend/functions/canReadPartition.js
@@ -1,16 +1,12 @@
 exports = async function(partitionValue) {
+  const cluster = context.services.get("mongodb-atlas");
+  const userCollection = cluster.db("tracker").collection("User");
   try {
-    const callingUser = context.user;
-
-    // The user custom data contains a canReadPartitions array that is managed
-    // by a system function.
-    const {canReadPartitions} = callingUser.custom_data;
-    
+    const user = await userCollection.findOne({ _id: context.user.id });
     // If the user's canReadPartitions array contains the partition, they may read the partition
-    return canReadPartitions && canReadPartitions.includes(partitionValue);
-
+    return user.canReadPartitions && user.canReadPartitions.includes(partitionValue);
   } catch (error) {
-    console.error(error);
-    return false;
+    console.error(`Couldn't find user ${context.user.id}: ${error}`);
+    return false
   }
-};
+}
--- tutorial/backend/functions/canWritePartition.js
+++ tutorial/backend/functions/canWritePartition.js
@@ -1,16 +1,12 @@
 exports = async function(partitionValue) {
+  const cluster = context.services.get("mongodb-atlas");
+  const userCollection = cluster.db("tracker").collection("User");
   try {
-    const callingUser = context.user;
-
-    // The user custom data contains a canWritePartitions array that is managed
-    // by a system function.
-    const {canWritePartitions} = callingUser.custom_data;
-    
+    const user = await userCollection.findOne({ _id: context.user.id });
     // If the user's canWritePartitions array contains the partition, they may write to it
-    return canWritePartitions && canWritePartitions.includes(partitionValue);
-
+    return user.canWritePartitions && user.canWritePartitions.includes(partitionValue);
   } catch (error) {
-    console.error(error);
-    return false;
+    console.error(`Couldn't find user ${context.user.id}: ${error}`);
+    return false
   }
 };
