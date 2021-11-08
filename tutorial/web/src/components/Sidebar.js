2212af0ead94349a863e367e478aee8ee7496c6f
mongodben <ben.p@mongodb.com>
2021-11-08 15:58:11 -0500

    refactor useProjects watcher to react to change
    Use the `watch()` method of the mongodb api to listen to changes to project list and update them in the UI.

--- tutorial/web/src/components/Sidebar.js
+++ tutorial/web/src/components/Sidebar.js
@@ -12,6 +12,7 @@ export default function Sidebar({
   setIsEditingPermissions,
 }) {
   const projects = useProjects();
+  console.log('projects are', projects)
   const app = useRealmApp();
 
   return (
--- tutorial/web/src/graphql/useProjects.js
+++ tutorial/web/src/graphql/useProjects.js
@@ -1,14 +1,51 @@
 import { useRealmApp } from "../RealmApp";
+import React from 'react';
 
 // :code-block-start: useProjects
+// :state-start: final
+function setProjectsFromChange(change, setProjects){
+  const { fullDocument: { memberOf } } = change;
+  setProjects(memberOf);
+}
+
 export default function useProjects() {
   const app = useRealmApp();
+  const [projects, setProjects] = React.useState(app.currentUser.customData.memberOf);
   if (!app.currentUser) {
     throw new Error("Cannot list projects if there is no logged in user.");
   }
-  // :state-start: final
-  const projects = app.currentUser.customData.memberOf;
-  // :state-end: :state-uncomment-start: start
+  const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+  const users = mongodb.db("tracker").collection("User");
+
+  React.useEffect( () => { 
+    (async () => {
+      for await (const change of users.watch()) {
+        switch (change.operationType) {
+          case "insert": {
+            setProjectsFromChange(change, setProjects);
+            break;
+          }
+          case "update": {
+            setProjectsFromChange(change, setProjects);
+            break;
+          }
+          case "replace": {
+            setProjectsFromChange(change, setProjects);
+            break;
+          }
+          case "delete": {
+            setProjectsFromChange(change, setProjects);
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      }
+    })()
+  });
+  // :state-end: 
+  // :state-uncomment-start: start
   // // TODO: Retrieve the current user's projects and assign it to `projects`.
   // const projects;
   // :state-uncomment-end:
