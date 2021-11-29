43035a40495ecd9c956e8a15d6af44c605d45a6e
mongodben <ben.p@mongodb.com>
2021-11-29 16:51:43 -0500

    update code snip

--- source/tutorial/generated/node/projects.codeblock.getProjects.js
+++ source/tutorial/generated/node/projects.codeblock.getProjects.js
@@ -1,7 +1,15 @@
 async function getProjects() {
-  const realm = await index.getRealm(`user=${users.getAuthedUser().id}`);
-  const currentUser = users.getAuthedUser().id;
-  const user = realm.objectForPrimaryKey("User", currentUser);
-  const projects = user.memberOf;
-  return projects;
+  const user = users.getAuthedUser();
+  try {
+    const { memberOf: projects } = await user.refreshCustomData();
+
+    // Make sure that the user object has been created
+    if (!projects) {
+      output.error("The user object hasn't been created yet. Try again soon.");
+      throw new Error("No projects for user");
+    }
+    return projects;
+  } catch (err) {
+    output.error("There was a problem accessing custom user data");
+  }
 }
