9b3e8517e6960f2cd367a4cd09844bec9e395ec6
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2022-01-07 08:11:54 -0500

    removed unneeded vars in update method

--- examples/node/Examples/flexible-sync.js
+++ examples/node/Examples/flexible-sync.js
@@ -50,13 +50,12 @@ describe("Flexible Sync Tests", () => {
     // :code-block-end:
 
     // :code-block-start: subscribe-to-queryable-fields
-    let sub1, sub2, sub3;
     subscriptions.update((mutableSubscriptionsInstance) => {
-      sub1 = mutableSubscriptionsInstance.add(longRunningTasks, {
+      mutableSubscriptionsInstance.add(longRunningTasks, {
         name: "longRunningTasksSubscription",
       });
-      sub2 = mutableSubscriptionsInstance.add(bensTasks);
-      sub3 = mutableSubscriptionsInstance.add(realm.objects("Team"), {
+      mutableSubscriptionsInstance.add(bensTasks);
+      mutableSubscriptionsInstance.add(realm.objects("Team"), {
         name: "teamsSubscription",
         throwOnUpdate: true,
       });
--- source/examples/generated/node/flexible-sync.codeblock.subscribe-to-queryable-fields.js
+++ source/examples/generated/node/flexible-sync.codeblock.subscribe-to-queryable-fields.js
@@ -1,10 +1,9 @@
-let sub1, sub2, sub3;
 subscriptions.update((mutableSubscriptionsInstance) => {
-  sub1 = mutableSubscriptionsInstance.add(longRunningTasks, {
+  mutableSubscriptionsInstance.add(longRunningTasks, {
     name: "longRunningTasksSubscription",
   });
-  sub2 = mutableSubscriptionsInstance.add(bensTasks);
-  sub3 = mutableSubscriptionsInstance.add(realm.objects("Team"), {
+  mutableSubscriptionsInstance.add(bensTasks);
+  mutableSubscriptionsInstance.add(realm.objects("Team"), {
     name: "teamsSubscription",
     throwOnUpdate: true,
   });
