1906aecd6378f42109d4e84f00a32106fcf3587c
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2022-01-10 11:29:40 -0500

    (DOCSP-19410): Flexible Sync - .NET Usage Examples

---
+++ examples/dotnet/Examples/FlexibleSyncExamples.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Examples
+{
+    public class FlexibleSyncExamples
+    {
+        // [Test]
+        public FlexibleSyncExamples()
+        {
+            // :code-block-start: open-a-flexible-synced-realm
+            //var config = new FlexibleSyncConfiguration(app.CurrentUser);
+            //var realm = Realm.GetInstance(config);
+            // :code-block-end: 
+        }
+    }
+}
