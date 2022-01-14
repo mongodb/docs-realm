833f29dab55b1759712729f91d221ffcb61dc0db
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-01-14 14:18:47 -0500

    init project w flutter 2.0.0 template

--- examples/flutter/test/widget_test.dart
+++ examples/flutter/test/widget_test.dart
@@ -1,30 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:dart_flutter/main.dart';
+import 'package:provider_shopper/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('smoke test', (tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Navigating through login page.
+    await tester.tap(find.text('ENTER'));
+    await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Check that shopping cart is empty at start.
+    await tester.tap(find.byIcon(Icons.shopping_cart));
+    await tester.pumpAndSettle();
+    expect(find.text('\$0'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Buy an item.
+    await tester.pageBack();
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('ADD').first);
+
+    // Check that the shopping cart is not empty anymore.
+    await tester.tap(find.byIcon(Icons.shopping_cart));
+    await tester.pumpAndSettle();
+    expect(find.text('\$0'), findsNothing);
   });
 }
