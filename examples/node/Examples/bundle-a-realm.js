3fea10c7e0977fef11f2c9d2b74e8c201d0bf09f
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-01-27 16:12:57 -0500

    add sync considerations

--- examples/node/Examples/bundle-a-realm.js
+++ examples/node/Examples/bundle-a-realm.js
@@ -1,4 +1,4 @@
-import Realm from "realm";
+import Realm, { ObjectId } from "realm";
 import path from "path";
 
 describe("Bundle a Realm", () => {
@@ -42,5 +42,56 @@ describe("Bundle a Realm", () => {
     // clean up
     Realm.deleteFile(originalConfig);
     Realm.deleteFile(copyConfig);
+  });
+
+  test.skip("Must fully sync Realm before bundling", async () => {
+    const Car = {
+      name: "Car",
+      properties: {
+        make: "string",
+        model: "string",
+        miles: "int",
+        owner: "string",
+      },
+    };
+
+    const app = new Realm.App({ id: "demo_app-cicfi" });
+
+    let credentials = Realm.Credentials.anonymous();
+    await app.logIn(credentials);
+
+    // :snippet-start: fully-sync-before-copy
+    const config = {
+      sync: {
+        user: app.currentUser,
+        partitionValue: app.currentUser.id,
+      },
+      schema: [Car],
+    };
+    const realm = await Realm.open(config);
+
+    // create many changes
+    realm.write(() => {
+      for (let i = 0; i < 25; i++) {
+        realm.create("Car", {
+          make: "Toyota",
+          model: "Prius",
+          miles: i,
+          owner: app.currentUser.id,
+        });
+      }
+    });
+
+    // ensure synchronize all changes before copy
+    await realm.syncSession.uploadAllLocalChanges();
+    await realm.syncSession.downloadAllServerChanges();
+
+    // changes are synchronized -- we can copy the realm
+    realm.writeCopyTo(__dirname + "syncedCopy.realm");
+    // :snippet-end:
+
+    // log out the user that created the realm
+    await app.currentUser.logOut();
+    realm.close();
   });
 });
