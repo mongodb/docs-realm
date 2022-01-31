411c8a1af733f8bfd3feff746f033fdde7cbafd8
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2022-01-31 09:26:26 -0500

    Update examples/dotnet/Examples/FlexibleSyncExamples.cs
    Co-authored-by: Nikola Irinchev <irinchev@me.com>

--- examples/dotnet/Examples/FlexibleSyncExamples.cs
+++ examples/dotnet/Examples/FlexibleSyncExamples.cs
@@ -31,8 +31,7 @@ namespace Examples
             {
                 // subscribe to all long running tasks, and give the subscription the name 'longRunningTasksSubscription'
                 var longRunningTasksQuery = realm.All<MyTask>().Where(t => t.Status == "completed" && t.ProgressMinutes > 120 ); 
-                var longRunningTasksSubscriptionOptions = new SubscriptionOptions() { Name = "longRunningTasksSubscription" };
-                var longRunningTasksSubscription = subscriptions.Add(longRunningTasksQuery, longRunningTasksSubscriptionOptions);
+               subscriptions.Add(longRunningTasksQuery, new() { Name = "longRunningTasks" });
 
                 // subscribe to all of Ben's Task objects
                 var bensTasks = subscriptions.Add(realm.All<MyTask>().Where(t => t.Owner == "Ben"));
