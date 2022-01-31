a7ac06908af03bba1814593b4a4006129f4c8606
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2022-01-31 09:33:24 -0500

    Update examples/dotnet/Examples/FlexibleSyncExamples.cs
    Co-authored-by: Nikola Irinchev <irinchev@me.com>

--- examples/dotnet/Examples/FlexibleSyncExamples.cs
+++ examples/dotnet/Examples/FlexibleSyncExamples.cs
@@ -53,8 +53,7 @@ namespace Examples
             realm.Subscriptions.Update(() =>
             {
                 var updatedLongRunningTasksQuery = realm.All<MyTask>().Where(t => t.Status == "completed" && t.ProgressMinutes > 130);
-                var longRunningTasksSubscriptionOptions = new SubscriptionOptions() { Name = "longRunningTasksSubscription" };
-                var longRunningTasksSubscription = subscriptions.Add(updatedLongRunningTasksQuery, longRunningTasksSubscriptionOptions);
+                subscriptions.Add(updatedLongRunningTasksQuery, new() { Name = "longRunningTasks" });
             });
             // :code-block-end:
 
