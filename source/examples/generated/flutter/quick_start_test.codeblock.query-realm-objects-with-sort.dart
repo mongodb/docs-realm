d230b1bfb43dc54c2fe72e8aa915ea58e394e16b
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-02-01 12:26:37 -0500

    implement dart team feedback

--- source/examples/generated/flutter/quick_start_test.codeblock.query-realm-objects-with-sort.dart
+++ source/examples/generated/flutter/quick_start_test.codeblock.query-realm-objects-with-sort.dart
@@ -1,7 +1,7 @@
 realm.write(() {
-  realm.add(Car('BMW', 'Z4', miles: 42));
-  realm.add(Car('Audi', 'A8', miles: 99));
-  realm.add(Car('Mercedes', 'G-Wagon', miles: 2));
+  realm.add(Car('BMW', model: 'Z4', miles: 42));
+  realm.add(Car('Audi', model: 'A8', miles: 99));
+  realm.add(Car('Mercedes', model: 'G-Wagon', miles: 2));
 });
 final sortedCars = realm.query<Car>('TRUEPREDICATE SORT(model ASC)');
 for (var car in sortedCars) {
