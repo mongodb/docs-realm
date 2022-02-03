c190253ec2594053af130269d43e7c52de6a5ef3
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2022-02-03 10:50:10 -0500

    update syntax to 8.0

--- source/examples/generated/dotnet-flexible-sync/FlexibleSyncExamples.codeblock.update-a-subscription.cs
+++ source/examples/generated/dotnet-flexible-sync/FlexibleSyncExamples.codeblock.update-a-subscription.cs
@@ -1,5 +1,5 @@
 realm.Subscriptions.Update(() =>
 {
     var updatedLongRunningTasksQuery = realm.All<Task>().Where(t => t.Status == "completed" && t.ProgressMinutes > 130);
-    realm.Subscriptions.Add(updatedLongRunningTasksQuery, new() { Name = "longRunningTasks" });
+    realm.Subscriptions.Add(updatedLongRunningTasksQuery, new SubscriptionOptions() { Name = "longRunningTasks" });
 });
