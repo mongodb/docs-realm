606463022e9932166205945af7f0bc108cd1fd06
Dachary Carey <dachary.carey@mongodb.com>
2022-02-11 17:57:22 -0500

    Actually bundle a realm

--- examples/ios/Examples/BundleRealms.swift
+++ examples/ios/Examples/BundleRealms.swift
@@ -81,16 +81,6 @@ class BundleRealms: XCTestCase {
         var config = user.configuration(partitionValue: "Partition You Want to Bundle")
         config.objectTypes = [QsTask.self]
 
-        // Specify the path for the app user's realm
-        // We're using FileManager here for tested code examples,
-        // but you'll use NSBundle.main.path(forResource: ofType)
-        // if you bundle the realm with your app via Xcode.
-        guard let seedFileDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
-        let completeSeedPath = seedFileDir.appendingPathComponent("seed.realm")
-
-        // Set the path as a seed file for the app user's realm
-        config.seedFilePath = completeSeedPath
-
         // Open the synced realm, downloading any changes before opening it
         let realm = try await Realm(configuration: config, downloadBeforeOpen: .always)
         print("Successfully opened the bundled realm")
@@ -114,6 +104,10 @@ class BundleRealms: XCTestCase {
         daenerysTasks = tasks.filter("owner == 'Daenerys'")
         XCTAssertEqual(daenerysTasks.count, 2)
         // :code-block-end:
+        // Delete the task we just added to avoid messing with XCTAsserts
+        try realm.write {
+            realm.delete(task)
+        }
     }
 }
 // :replace-end:
--- source/examples/generated/code/start/BundleRealms.codeblock.open-bundled-synced-realm.swift
+++ source/examples/generated/code/start/BundleRealms.codeblock.open-bundled-synced-realm.swift
@@ -8,16 +8,6 @@ let user = try await app.login(credentials: Credentials.anonymous)
 var config = user.configuration(partitionValue: "Partition You Want to Bundle")
 config.objectTypes = [Task.self]
 
-// Specify the path for the app user's realm
-// We're using FileManager here for tested code examples,
-// but you'll use NSBundle.main.path(forResource: ofType)
-// if you bundle the realm with your app via Xcode.
-guard let seedFileDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
-let completeSeedPath = seedFileDir.appendingPathComponent("seed.realm")
-
-// Set the path as a seed file for the app user's realm
-config.seedFilePath = completeSeedPath
-
 // Open the synced realm, downloading any changes before opening it
 let realm = try await Realm(configuration: config, downloadBeforeOpen: .always)
 print("Successfully opened the bundled realm")
