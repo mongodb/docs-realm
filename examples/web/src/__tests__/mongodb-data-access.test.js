92150416debf153f50e4a3a11bb43aedb910d8bd
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-02-28 17:08:28 -0500

    create empty tests for mongodb data access

---
+++ examples/web/src/__tests__/mongodb-data-access.test.js
@@ -0,0 +1,66 @@
+import * as Realm from "realm-web";
+import { APP_ID } from "../realm.config.json";
+
+describe("Set up MongoDB Client", () => {
+  test("instantiate handle", () => {
+    return;
+  });
+});
+describe("CRUD operations", () => {
+  describe("Create documents", () => {
+    test("Insert single document", () => {
+      return;
+    });
+    test("Insert multiple documents", () => {
+      return;
+    });
+  });
+  describe("Read documents", () => {
+    test("Find single document", async () => {
+      return;
+    });
+
+    test("Find multiple documents", async () => {
+      return;
+    });
+    test("Count documents in collection", async () => {
+      return;
+    });
+  });
+  describe("Update documents", () => {
+    test("Update single document", async () => {
+      return;
+    });
+
+    test("Update multiple documents", async () => {
+      return;
+    });
+    test("Upsert documents", async () => {
+      return;
+    });
+  });
+  describe("Delete documents", () => {
+    test("Delete single document", async () => {
+      return;
+    });
+    test("Delete multiple documents", async () => {
+      return;
+    });
+  });
+});
+describe("Watch for changes", () => {
+  test("Watch for changes in a collection", async () => {
+    return;
+  });
+  test("Watch for changes in a collection with a filter", async () => {
+    return;
+  });
+});
+
+describe("Aggregate documents", () => {
+  test("Basic aggregation", async () => {
+    return;
+  });
+});
+
+// TODO: refactor docs to make aggregation stages examples testable
