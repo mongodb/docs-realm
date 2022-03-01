9838b58237a7714ff1944acf5dc065ffc787bc4e
nathan-contino-mongo <nathan.contino@mongodb.com>
2022-03-01 12:34:57 -0700

    migration trimming, kotlin

--- examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/MigrateFromJavaToKotlinSDKTest.kt
+++ examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/MigrateFromJavaToKotlinSDKTest.kt
@@ -381,21 +381,32 @@ class MigrateFromJavaToKotlinSDKTest: RealmTest() {
                 .build()
 
             // :code-block-start: migrations
-            // A Realm migration that performs automatic schema migration
-            // and allows additional custom migration of data.
-            RealmConfiguration.Builder(schema = setOf(Sample::class))
+            // A Realm migration that performs
+            // automatic schema migration
+            // and allows additional custom
+            // migration of data.
+            RealmConfiguration.Builder(
+                schema = setOf(Sample::class))
                 .migration(AutomaticSchemaMigration {
-                        context: AutomaticSchemaMigration.MigrationContext ->
-                    val oldRealm: DynamicRealm =
+                        context:
+                            AutomaticSchemaMigration.MigrationContext ->
+                    val oldRealm:
+                            DynamicRealm =
                         context.oldRealm
-                    val newRealm: DynamicMutableRealm =
+                    val newRealm:
+                            DynamicMutableRealm =
                         context.newRealm
-                    // dynamic realm gives access to realm data
-                    // through a generic string based API
+                    // dynamic realm gives access
+                    // to realm data
+                    // through a generic string
+                    // based API
                     context.enumerate("Sample") {
-                            oldObject: DynamicRealmObject,
-                            newObject: DynamicMutableRealmObject? ->
-                        newObject?.set("longField", 42L)
+                            oldObject:
+                                DynamicRealmObject,
+                            newObject:
+                                DynamicMutableRealmObject? ->
+                        newObject?.set("longField",
+                                        42L)
                     }
                 })
                 .build()
--- source/examples/generated/kotlin/MigrateFromJavaToKotlinSDKTest.codeblock.migrations.kt
+++ source/examples/generated/kotlin/MigrateFromJavaToKotlinSDKTest.codeblock.migrations.kt
@@ -1,18 +1,29 @@
-// A Realm migration that performs automatic schema migration
-// and allows additional custom migration of data.
-RealmConfiguration.Builder(schema = setOf(Sample::class))
+// A Realm migration that performs
+// automatic schema migration
+// and allows additional custom
+// migration of data.
+RealmConfiguration.Builder(
+    schema = setOf(Sample::class))
     .migration(AutomaticSchemaMigration {
-            context: AutomaticSchemaMigration.MigrationContext ->
-        val oldRealm: DynamicRealm =
+            context:
+                AutomaticSchemaMigration.MigrationContext ->
+        val oldRealm:
+                DynamicRealm =
             context.oldRealm
-        val newRealm: DynamicMutableRealm =
+        val newRealm:
+                DynamicMutableRealm =
             context.newRealm
-        // dynamic realm gives access to realm data
-        // through a generic string based API
+        // dynamic realm gives access
+        // to realm data
+        // through a generic string
+        // based API
         context.enumerate("Sample") {
-                oldObject: DynamicRealmObject,
-                newObject: DynamicMutableRealmObject? ->
-            newObject?.set("longField", 42L)
+                oldObject:
+                    DynamicRealmObject,
+                newObject:
+                    DynamicMutableRealmObject? ->
+            newObject?.set("longField",
+                            42L)
         }
     })
     .build()
