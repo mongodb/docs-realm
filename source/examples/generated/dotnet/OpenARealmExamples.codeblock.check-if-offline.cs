4d415a9f3390430e56aa4ff147eec306c6d5beb1
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2022-03-10 13:17:13 -0500

    replace SyncConfig with PartitionSync config in bluehawked snippets

--- source/examples/generated/dotnet/OpenARealmExamples.codeblock.check-if-offline.cs
+++ source/examples/generated/dotnet/OpenARealmExamples.codeblock.check-if-offline.cs
@@ -3,13 +3,13 @@ if (app.CurrentUser == null)
     // App must be online for user to authenticate
     user = await app.LogInAsync(
         Credentials.EmailPassword("caleb@mongodb.com", "shhhItsASektrit!"));
-    config = new SyncConfiguration("_part", user);
+    config = new PartitionSyncConfiguration("_part", user);
     realm = await Realm.GetInstanceAsync(config);
 }
 else
 {
     // This works whether online or offline
     user = app.CurrentUser;
-    config = new SyncConfiguration("_part", user);
+    config = new PartitionSyncConfiguration("_part", user);
     realm = Realm.GetInstance(config);
 }
