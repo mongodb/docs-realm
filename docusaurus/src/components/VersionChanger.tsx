910880afeedf61194c54633dc0691f7f3f5b8880
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-03-14 11:29:05 -0400

    add basic version changing

--- docusaurus/src/components/VersionChanger.tsx
+++ docusaurus/src/components/VersionChanger.tsx
@@ -1,6 +1,8 @@
 import { useHistory } from "react-router-dom";
 import React, { useState, useEffect, Fragment, memo } from "react";
 import { useAllPluginInstancesData } from "@docusaurus/useGlobalData";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { versions } from "process";
 
 const CONTEXTS = [
   {
@@ -19,8 +21,31 @@ const CONTEXTS = [
 
 const getContext = (id) => CONTEXTS.find((context) => context.id === id);
 
-export const getCurrentPageInfo = () => {
-  return window.location.pathname;
+export const getCurrentSiteInstance = (history) => {
+  // const history = useHistory();
+  const {
+    location: { pathname },
+  } = history;
+  const splitPath = pathname.split("/").slice(1);
+  if (!splitPath.includes("sdk")) {
+    return {
+      sdkInstance: "default",
+      sdkPage: null,
+    };
+  } else {
+    const sdkWordIdx = splitPath.findIndex((value) => value === "sdk");
+    const sdkInstance = splitPath[sdkWordIdx + 1];
+    const sdkPage = splitPath.slice(sdkWordIdx + 2).join("/");
+    console.log(sdkPage);
+    const sdkVersion = isNaN(splitPath[sdkWordIdx + 2][0])
+      ? "current"
+      : splitPath[sdkWordIdx + 2];
+    return {
+      sdkInstance,
+      sdkPage,
+      sdkVersion,
+    };
+  }
 };
 
 const pathExists = (path, data) => {
@@ -28,15 +53,59 @@ const pathExists = (path, data) => {
 };
 
 function SDKSelector({ sdkSitesMetaData, activeSDK }) {
+  const history = useHistory();
+  function changeHandler(e) {
+    const sdkName = e.target.value;
+    const sdkMetadata = sdkSitesMetaData[sdkName];
+    const sdkBasePage = sdkMetadata.path + "intro"; //TODO: refactor into smthn better
+    history.push(sdkBasePage);
+  }
   delete sdkSitesMetaData.default;
   return (
-    <select>
-      {Object.keys(sdkSitesMetaData).map((sdkId) => (
-        <option key={sdkId} value={sdkId}>
-          {getContext(sdkId).name}
-        </option>
-      ))}
-    </select>
+    <div>
+      <select onChange={changeHandler}>
+        {Object.keys(sdkSitesMetaData).map((sdkId) => (
+          <option key={sdkId} value={sdkId} selected={sdkId === activeSDK}>
+            {getContext(sdkId).name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+function ActiveSDKVersionSelector({
+  activeSdk,
+  activeSdkVersions,
+  currentSdkVersion,
+  sdkSitesMetaData,
+}) {
+  const history = useHistory();
+  function changeHandler2(e) {
+    const sdkVersion = e.target.value;
+    console.log({ sdkVersion });
+    const sdkMetadata = sdkSitesMetaData[activeSdk];
+    console.log({ sdkMetadata });
+    const sdkVersionBasePage = sdkMetadata.path + `${sdkVersion}/` + "intro"; //TODO: refactor into smthn better
+    console.log({ sdkVersionBasePage });
+    history.push(sdkVersionBasePage);
+  }
+  return (
+    <div>
+      <select onChange={changeHandler2}>
+        {activeSdkVersions.map((versionsInfo, i) => {
+          return (
+            <option
+              key={versionsInfo.name}
+              value={versionsInfo.name}
+              selected={currentSdkVersion === versionsInfo.name}
+            >
+              {versionsInfo.label}
+            </option>
+          );
+        })}
+      </select>
+    </div>
   );
 }
 
@@ -49,9 +118,9 @@ const ContextSwitcher = ({ className }) => {
   console.log(history.location);
 
   useEffect(() => {
-    const [doc] = getCurrentPageInfo();
+    const { sdkPage } = getCurrentSiteInstance(history);
 
-    const currContext = getContext(doc);
+    const currContext = getContext(sdkPage);
     if (currContext && currContext.id !== context.id) {
       setContext(currContext);
     }
@@ -60,7 +129,7 @@ const ContextSwitcher = ({ className }) => {
   const handleChange = (newValue) => {
     setContext(newValue);
 
-    const [, ...docPath] = getCurrentPageInfo();
+    const { sdkInstance, sdkPage } = getCurrentSiteInstance(history);
 
     const newDoc = newValue.id;
 
@@ -83,13 +152,20 @@ const ContextSwitcher = ({ className }) => {
   };
   const currentPath = history.location.pathname;
   if (currentPath.includes("/sdk/")) {
-    const sdkName = currentPath.split("/").find((segment) => {
-      CONTEXTS.map(({ id }) => id).includes(segment);
-    });
+    const { sdkInstance: sdkName } = getCurrentSiteInstance(history);
+    // @ts-ignore
+    const versions = data[sdkName].versions;
+    console.log(versions);
     return (
-      <>
+      <div style={{ paddingTop: 60 }}>
         <SDKSelector sdkSitesMetaData={data} activeSDK={sdkName} />
-      </>
+        <ActiveSDKVersionSelector
+          activeSdk={sdkName}
+          activeSdkVersions={versions}
+          sdkSitesMetaData={data}
+          currentSdkVersion={getCurrentSiteInstance(history).sdkVersion}
+        />
+      </div>
     );
   } else return <></>;
 };
