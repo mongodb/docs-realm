1d77b42a81a3da3d8bf442098e0682b3fc921c5d
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-03-22 16:30:25 -0400

    (DOCSP-21502): Query RealmList of RealmObjects - Flutter SDK (#1771)
    * add query RealmObject list docs
    
    * fix link
    
    * implement CT suggestions

---
+++ source/examples/generated/flutter/read_write_data_test.codeblock.query-realm-list.dart
@@ -0,0 +1,11 @@
+final config = Configuration([Person.schema, Team.schema]);
+final realm = Realm(config);
+final heroes = Team('Millenium Falcon Crew', crew: [
+  Person('Luke'),
+  Person('Leia'),
+  Person('Han'),
+  Person('Chewbacca')
+]);
+realm.write(() => realm.add(heroes));
+
+final lukeAndLeia = (heroes.crew).query(r'name BEGINSWITH $0', ['L']);
