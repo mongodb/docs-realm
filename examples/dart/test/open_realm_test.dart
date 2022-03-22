5f0d9aee683e73e7ccc316e504bca06f5185a801
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-03-22 14:24:29 -0400

    (DOCSP-21503): Expand Open Realm docs for Flutter SDK (#1769)
    * draft changes
    
    * Apply suggestions from CB code review
    
    Co-authored-by: Chris Bush <chris.bush@10gen.com>
    
    Co-authored-by: Chris Bush <chris.bush@10gen.com>

--- examples/dart/test/open_realm_test.dart
+++ examples/dart/test/open_realm_test.dart
@@ -15,5 +15,68 @@ void main() {
       // :snippet-end:
       expect(realm.isClosed, true);
     });
+
+    group('In-memory realm', () {
+      test('Configuration inMemory - no files after closing realm', () {
+        // :snippet-start: in-memory-realm
+        var config = Configuration([Car.schema], inMemory: true);
+        var realm = Realm(config);
+        // :snippet-end:
+        realm.write(() => realm.add(Car('Tesla')));
+        realm.close();
+        expect(Realm.existsSync(config.path), false);
+      });
+
+      test('Configuration inMemory can not be readOnly', () {
+        Configuration config = Configuration([Car.schema], inMemory: true);
+        var realm = Realm(config);
+
+        config.isReadOnly = true;
+        expect(
+            () => Realm(config),
+            throwsA(RealmException(
+                "Realm at path '${config.path}' already opened with different read permissions")));
+        realm.close();
+      });
+    });
+
+    group('Read-only realm', () {
+      test('Configuration readOnly - reading is possible', () {
+        Configuration initConfig = Configuration([Car.schema]);
+        var realm = Realm(initConfig);
+        realm.write(() => realm.add(Car("Mustang")));
+        realm.close();
+
+        // :snippet-start: read-only-realm
+        var config = Configuration([Car.schema], readOnly: true);
+        realm = Realm(config);
+        // :snippet-end:
+        var cars = realm.all<Car>();
+        expect(cars.isNotEmpty, true);
+        realm.close();
+      });
+
+      test('Configuration readOnly - writing on read-only Realms throws', () {
+        Configuration config = Configuration([Car.schema]);
+        var realm = Realm(config);
+        realm.close();
+
+        config = Configuration([Car.schema], readOnly: true);
+        realm = Realm(config);
+        expect(
+            () => realm.write(() {}),
+            throwsA(RealmException(
+                "Can't perform transactions on read-only Realms.")));
+        realm.close();
+      });
+    });
+    test('Configuration - FIFO files fallback path', () {
+      // :snippet-start: fifo-file
+      var config =
+          Configuration([Car.schema], fifoFilesFallbackPath: "./fifo_folder");
+      var realm = Realm(config);
+      // :snippet-end:
+      realm.close();
+    });
   });
 }
