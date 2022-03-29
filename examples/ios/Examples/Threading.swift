2c946fecfa98f226cc55528cc9de06f149f93ea6
Dachary Carey <dachary.carey@mongodb.com>
2022-03-29 14:28:09 -0400

    Remove how-not-to-do-it code from example

--- examples/ios/Examples/Threading.swift
+++ examples/ios/Examples/Threading.swift
@@ -259,14 +259,6 @@ class Threading: XCTestCase {
             let realm = try! Realm(configuration: .defaultConfiguration, queue: serialQueue)
             // Do something with Realm on the non-main thread
         }
-        // This does not work. Realm does not support concurrent
-        // queues, and 'global()' is a concurrent queue.
-        // DispatchQueue.global().async {
-        //     autoreleasepool{
-        //          try! Realm()
-        //          // Do something with Realm on the non-main thread
-        //     }
-        // }
         // :code-block-end:
     }
 }
--- source/examples/generated/code/start/Threading.codeblock.use-realm-with-serial-queue.swift
+++ source/examples/generated/code/start/Threading.codeblock.use-realm-with-serial-queue.swift
@@ -5,11 +5,3 @@ serialQueue.async {
     let realm = try! Realm(configuration: .defaultConfiguration, queue: serialQueue)
     // Do something with Realm on the non-main thread
 }
-// This does not work. Realm does not support concurrent
-// queues, and 'global()' is a concurrent queue.
-// DispatchQueue.global().async {
-//     autoreleasepool{
-//          try! Realm()
-//          // Do something with Realm on the non-main thread
-//     }
-// }
