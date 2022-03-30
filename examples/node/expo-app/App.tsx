f7429f72110bd90e8660bdbd39644facc35785fc
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2022-03-30 10:52:01 -0400

    update per pr comments

--- examples/node/expo-app/App.tsx
+++ examples/node/expo-app/App.tsx
@@ -128,7 +128,7 @@ const styles = StyleSheet.create({
 
 // :code-block-start: wrap-app-within-realm-provider
 function AppWrapper() {
-  if (!RealmProvider) {
+  if (!app.currentUser) {
     return null;
   }
   return (
