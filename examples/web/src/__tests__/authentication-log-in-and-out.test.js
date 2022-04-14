302224964a13b9d98266440b2bb66428fcaad260
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-04-14 16:32:56 -0400

    update code example comment

--- examples/web/src/__tests__/authentication-log-in-and-out.test.js
+++ examples/web/src/__tests__/authentication-log-in-and-out.test.js
@@ -139,7 +139,7 @@ describe("Log in user", () => {
     // :snippet-start: custom-jwt-firebase
     // ... log user into Firebase & initialize Realm app
 
-    const token = await getIdToken(firebaseUser); // using modular Firebase Web v9 SDK
+    const token = await getIdToken(firebaseUser); // using modular Firebase Web v9 SDK method auth.getIdToken() - https://firebase.google.com/docs/reference/js/auth#getidtoken
     const credentials = Realm.Credentials.jwt(token);
     const realmUser = await realmApp.logIn(credentials);
     // :snippet-end:
--- source/examples/generated/web/authentication-log-in-and-out.test.codeblock.custom-jwt-firebase.js
+++ source/examples/generated/web/authentication-log-in-and-out.test.codeblock.custom-jwt-firebase.js
@@ -1,5 +1,5 @@
 // ... log user into Firebase & initialize Realm app
 
-const token = await getIdToken(firebaseUser); // using modular Firebase Web v9 SDK
+const token = await getIdToken(firebaseUser); // using modular Firebase Web v9 SDK method auth.getIdToken() - https://firebase.google.com/docs/reference/js/auth#getidtoken
 const credentials = Realm.Credentials.jwt(token);
 const realmUser = await realmApp.logIn(credentials);
