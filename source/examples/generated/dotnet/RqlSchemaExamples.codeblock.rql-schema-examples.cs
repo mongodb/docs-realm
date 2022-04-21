0ae38ba18cadd49f9dd7a7a8e9b063450c4c3fc7
Caleb Thompson <caleb.thompson@mongodb.com>
2022-04-21 11:09:39 -0700

    update to fix borked unit tests

--- source/examples/generated/dotnet/RqlSchemaExamples.codeblock.rql-schema-examples.cs
+++ source/examples/generated/dotnet/RqlSchemaExamples.codeblock.rql-schema-examples.cs
@@ -1,39 +1,39 @@
 public class Task : RealmObject
 {
-  [PrimaryKey]
-  [MapTo("id")]
-  public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+    [PrimaryKey]
+    [MapTo("_id")]
+    public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
 
-  [MapTo("name")]
-  [Required]
-  public string Name { get; set; }
+    [MapTo("name")]
+    [Required]
+    public string Name { get; set; }
 
-  [MapTo("isComplete")]
-  public bool IsComplete { get; set; } = false;
+    [MapTo("isComplete")]
+    public bool IsComplete { get; set; } = false;
 
-  [MapTo("assignee")]
-  public string Assignee {get; set;}
+    [MapTo("assignee")]
+    public string Assignee { get; set; }
 
-  [MapTo("priority")]
-  public int Priority {get; set;} = 0;
-  
-  [MapTo("progressMinutes")]
-  public int ProgressMinutes {get; set;} = 0;
+    [MapTo("priority")]
+    public int Priority { get; set; } = 0;
+
+    [MapTo("progressMinutes")]
+    public int ProgressMinutes { get; set; } = 0;
 }
 
 public class Project : RealmObject
 {
-  [PrimaryKey]
-  [MapTo("id")]
-  public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+    [PrimaryKey]
+    [MapTo("_id")]
+    public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
 
-  [MapTo("name")]
-  [Required]
-  public string Name { get; set; }
+    [MapTo("name")]
+    [Required]
+    public string Name { get; set; }
 
-  [MapTo("tasks")]
-  public Task[] Tasks {get; set;}
+    [MapTo("tasks")]
+    public IList<Task> Tasks { get; }
 
-  [MapTo("quota")]
-  public int Quota {get; set;}
+    [MapTo("quota")]
+    public int Quota { get; set; }
 }
