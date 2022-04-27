7d654e6f63f23fb99d83b0274e7f66c6aa4c83f6
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2022-04-27 11:09:02 -0400

    remove unneeded comments

--- examples/node/Examples/read-and-write-data.js
+++ examples/node/Examples/read-and-write-data.js
@@ -408,21 +408,11 @@ describe("Read & Write Data", () => {
       const person = realm.create("Person", { name: "Ali" });
       // Find dogs younger than 2.
       const puppies = realm.objects("Dog").filtered("age < 2");
-
-      // const results = realm.objects("whatever").filtered("whatever == 2");
-      // for (const obj of results) {
-      //   obj.whatever = 3;
-      // }
       // Loop through to update.
       for (const puppy of puppies) {
         // Give all puppies to Ali.
         puppy.owner = person;
       }
-      // Loop through to update.
-      // puppies.forEach((puppy) => {
-      //   // Give all puppies to Ali.
-      //   puppy.owner = person;
-      // });
     });
     // :code-block-end:
 
--- source/examples/generated/node/read-and-write-data.codeblock.read-and-write-bulk-update.js
+++ source/examples/generated/node/read-and-write-data.codeblock.read-and-write-bulk-update.js
@@ -3,19 +3,9 @@ realm.write(() => {
   const person = realm.create("Person", { name: "Ali" });
   // Find dogs younger than 2.
   const puppies = realm.objects("Dog").filtered("age < 2");
-
-  // const results = realm.objects("whatever").filtered("whatever == 2");
-  // for (const obj of results) {
-  //   obj.whatever = 3;
-  // }
   // Loop through to update.
   for (const puppy of puppies) {
     // Give all puppies to Ali.
     puppy.owner = person;
   }
-  // Loop through to update.
-  // puppies.forEach((puppy) => {
-  //   // Give all puppies to Ali.
-  //   puppy.owner = person;
-  // });
 });
