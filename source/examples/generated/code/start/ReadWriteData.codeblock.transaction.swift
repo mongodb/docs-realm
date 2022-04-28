760c62480198896425ea2dd483f986dfcd7e5058
Dachary Carey <dachary.carey@mongodb.com>
2022-04-28 14:41:49 -0400

    Oops, add inadvertently deleted comments from unrelated example

--- source/examples/generated/code/start/ReadWriteData.codeblock.transaction.swift
+++ source/examples/generated/code/start/ReadWriteData.codeblock.transaction.swift
@@ -5,7 +5,11 @@ let realm = try! Realm()
 do {
     // Open a thread-safe transaction.
     try realm.write {
-
+        // Make any writes within this code block.
+        // Realm automatically cancels the transaction
+        // if this code throws an exception. Otherwise,
+        // Realm automatically commits the transaction
+        // after the end of this code block.
     }
 } catch let error as NSError {
     // Failed to write to realm.
