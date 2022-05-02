4df9e2795b70e1526768c82e422abf9508c0e552
Caleb Thompson <caleb.thompson@mongodb.com>
2022-05-02 09:29:59 -0700

    update text to match new methods

--- examples/dotnet/Examples/ClientResetExamples.cs
+++ examples/dotnet/Examples/ClientResetExamples.cs
@@ -23,63 +23,6 @@ namespace Examples
         Realm fsRealm;
         RealmUser fsUser;
 
-
-        // this is the old way. No longer applies, but keeping the
-        // code for now, just in case...
-
-        /*[Test]
-         public async Task ResetsTheClient()
-         {
-             app = App.Create(myRealmAppId);
-
-             user = app.LogInAsync(Credentials.EmailPassword("foo@foo.com", "foobar")).Result;
-
-             config = new PartitionSyncConfiguration("myPart", user);
-             //:hide-start:
-             config.Schema = new[] { typeof(User) };
-             //:hide-end:
-             realm = await Realm.GetInstanceAsync(config);
-
-             // :code-block-start: handle
-             Session.Error += (sender, err) =>
-             {
-                 if (err.Exception is ClientResetException clientResetEx)
-                 {
-                     var session = (Session)sender;
-                     Console.WriteLine("Client Reset requested for " +
-                         session.Path + "due to " + clientResetEx.Message);
-
-                     // Prompt user to perform client reset immediately. If they don't do it,
-                     // they won't receive any data from the server until they restart the app
-                     // and all changes they make will be discarded when the app restarts.
-                     var didUserConfirmReset = true;
-                     if (didUserConfirmReset)
-                     {
-                         // Close the Realm before doing the reset as it'll need
-                         // to be deleted and all objects obtained from it will be
-                         // invalidated.
-                         realm.Dispose();
-                         var didReset = clientResetEx.InitiateClientReset();
-                         if (didReset)
-                         {
-                             // Navigate the user back to the main page or reopen the
-                             // the Realm and reinitialize the current page.
-                         }
-                         else
-                         {
-                             // Reset failed - notify user that they'll need to restart the app.
-                         }
-                         // :hide-start:
-                         Assert.IsTrue(didReset);
-                         // :hide-end:
-                     }
-                 }
-             };
-             // :code-block-end:
-             TestingExtensions.SimulateError(realm.SyncSession,
-                 ErrorCode.DivergingHistories, "diverging histories!", false);
-         }*/
-
         [Test]
         public async Task HandleAutoClientReset()
         // :code-block-start: DiscardLocalResetHandler
@@ -89,12 +32,12 @@ namespace Examples
         {
             // :hide-start:
             app = App.Create(myRealmAppId);
-            user = app.LogInAsync(Credentials.EmailPassword("foo@foo.com", "foobar")).Result;
+            user = app.LogInAsync(Credentials.EmailPassword("valerie@mongodb.com", "astral")).Result;
             // :hide-end:
             var config = new PartitionSyncConfiguration("myPartition", user);
             config.ClientResetHandler = new DiscardLocalResetHandler()
             {
-                OnBeforeReset = HandleBeforeResetCallbak,
+                OnBeforeReset = HandleBeforeResetCallback,
                 OnAfterReset = HandleAfterResetCallback,
                 ManualResetFallback = HandleManualResetCallback
             };
@@ -105,7 +48,7 @@ namespace Examples
             var realm = await Realm.GetInstanceAsync(config);
         }
 
-        private void HandleBeforeResetCallbak(Realm beforeFrozen)
+        private void HandleBeforeResetCallback(Realm beforeFrozen)
         {
             // Notify the user that a reset is going to happen.
             // This method may also be useful if you want to make a backup
@@ -165,7 +108,7 @@ namespace Examples
         {
             fsApp = App.Create(myRealmAppId);
             fsUser = fsApp.LogInAsync(
-                Credentials.EmailPassword("foo@foo.com", "foobar")).Result;
+                Credentials.EmailPassword("valerie@mongodb.com", "astral")).Result;
 
             var fsConfig = new FlexibleSyncConfiguration(fsUser);
             fsConfig.ClientResetHandler =
