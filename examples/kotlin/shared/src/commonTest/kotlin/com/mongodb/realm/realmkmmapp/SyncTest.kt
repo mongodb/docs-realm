71a8ab5b402ad9e74f905f60c85e4c90bd2e249e
nathan-contino-mongo <nathan.contino@mongodb.com>
2022-05-06 09:12:00 -0400

    remove explicit realm naming from open/configure examples -- no longer needed?

--- examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/SyncTest.kt
+++ examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/SyncTest.kt
@@ -38,9 +38,7 @@ class SyncTest: RealmTest() {
         val app = App.create(YOUR_APP_ID)
         runBlocking {
             val user = app.login(Credentials.anonymous())
-            val config = SyncConfiguration.Builder(user, PARTITION, setOf(/*realm object models here*/))
-                // specify name so realm doesn't just use the "default.realm" file for this user
-                .name(PARTITION)
+            val config = SyncConfiguration.Builder(user,PARTITION, setOf(/*realm object models here*/))
                 .maxNumberOfActiveVersions(10)
                 .build()
             val realm = Realm.open(config)
--- source/examples/generated/kotlin/syncTest.codeblock.configure-a-synced-realm.kt
+++ source/examples/generated/kotlin/syncTest.codeblock.configure-a-synced-realm.kt
@@ -1,9 +1,7 @@
 val app = App.create(YOUR_APP_ID)
 runBlocking {
     val user = app.login(Credentials.anonymous())
-    val config = SyncConfiguration.Builder(user, PARTITION, setOf(/*realm object models here*/))
-        // specify name so realm doesn't just use the "default.realm" file for this user
-        .name(PARTITION)
+    val config = SyncConfiguration.Builder(user,PARTITION, setOf(/*realm object models here*/))
         .maxNumberOfActiveVersions(10)
         .build()
     val realm = Realm.open(config)
