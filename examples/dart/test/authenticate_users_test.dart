f7413121eaedef345f96a1f68d996f11e8e96eda
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-05-19 11:23:47 -0400

    multiple users examples

--- examples/dart/test/authenticate_users_test.dart
+++ examples/dart/test/authenticate_users_test.dart
@@ -1,3 +1,5 @@
+// @Skip('currently failing (see issue 1234)')
+
 import 'package:test/test.dart';
 import 'package:realm_dart/realm.dart';
 
@@ -5,8 +7,19 @@ const APP_ID = "example-testers-kvjdy";
 
 void main() {
   late App app;
-  setUpAll(() {
+  setUpAll(() async {
     app = App(AppConfiguration(APP_ID));
+    EmailPasswordAuthProvider authProvider = EmailPasswordAuthProvider(app);
+    try {
+      await authProvider.registerUser("lisa@example.com", "myStr0ngPassw0rd");
+    } catch (err) {
+      print(err);
+    }
+    try {
+      await authProvider.registerUser("bart@example.com", "myStr0ngPassw0rd");
+    } catch (err) {
+      print(err);
+    }
   });
   tearDown(() async {
     if (app.currentUser != null) {
@@ -31,7 +44,7 @@ void main() {
       // :snippet-end:
       expect(app.currentUser != null, true);
       expect(emailPwCredentials.provider, AuthProviderType.emailPassword);
-    }, skip: 'must be manually validated');
+    });
   });
   test("Log out user", () async {
     Credentials anonCredentials = Credentials.anonymous();
@@ -48,5 +61,45 @@ void main() {
     User? user = app.currentUser;
     // :snippet-end:
     expect(app.currentUser?.id.isNotEmpty, true);
+  });
+  group('Work with multiple users', () {
+    late User lisa;
+    late User bart;
+    setUpAll(() async {
+      // var timer = Timer(Duration(seconds: 1), () => print('done'));
+      // timer.cancel();
+      await app.logIn(Credentials.anonymous());
+      lisa = await app.logIn(
+          Credentials.emailPassword("lisa@example.com", "myStr0ngPassw0rd"));
+      bart = await app.logIn(
+          Credentials.emailPassword("bart@example.com", "myStr0ngPassw0rd"));
+    });
+    tearDownAll(() async {
+      for (User user in app.users) {
+        await user.logOut();
+      }
+    });
+
+    test('List all users on the device', () async {
+      // :snippet-start: list-all-users
+      Iterable<User> users = app.users;
+      // :snippet-end:
+      expect(users.length, 3);
+    });
+    test('Change the active user', () async {
+      User otherUser = lisa;
+      // :snippet-start: change-active-user
+      app.switchUser(otherUser);
+      // :snippet-end:
+    });
+    test('Remove a user from the device', () async {
+      late var user;
+      if (app.currentUser != null) {
+        user = app.currentUser;
+        // :snippet-start: remove-user
+        await app.removeUser(user);
+        // :snippet-end:
+      }
+    });
   });
 }
---
+++ source/examples/generated/flutter/authenticate_users_test.codeblock.list-all-users.dart
@@ -0,0 +1,1 @@
+Iterable<User> users = app.users;
