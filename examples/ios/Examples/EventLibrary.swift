3d91aa369199ac21570ad5eadf81a727743c51fe
Dachary Carey <dachary.carey@mongodb.com>
2022-05-23 15:23:49 -0400

    Update code examples

--- examples/ios/Examples/EventLibrary.swift
+++ examples/ios/Examples/EventLibrary.swift
@@ -21,7 +21,7 @@ class EventLibrary_Person: Object, CustomEventRepresentable {
         self.name = name
         self.employeeId = employeeId
     }
-    
+
     // To conform to `CustomEventRepresentable`, your object
     // must implement a `customEventRepresentation` func that
     // defines your customized event serialization
@@ -39,19 +39,37 @@ class EventLibrary: XCTestCase {
     var collection: MongoCollection!
     var start: Date!
 
-    override func setUp() {
-        app.login(credentials: Credentials.anonymous) { [self] (result) in
+//    override func setUp() {
+//        app.login(credentials: Credentials.anonymous) { [self] (result) in
+//            switch result {
+//            case .failure(let error):
+//                print("Login failed: \(error.localizedDescription)")
+//            case .success(user):
+//                print("Successfully logged in as user \(user)")
+//                let mongoClient = user.mongoClient("mongodb1")
+//                let database = mongoClient.database(named: "test_data")
+//                self.collection = database.collection(withName: "AuditEvent")
+//                _ = self.collection.deleteManyDocuments(filter: [:])
+//            case .success(_):
+//                print("This case should never be hit")
+//            }
+//        }
+//    }
+
+    func deleteTestData() {
+        app.login(credentials: Credentials.anonymous) { (result) in
             switch result {
             case .failure(let error):
-                print("Login failed: \(error.localizedDescription)")
-            case .success(user):
-                print("Successfully logged in as user \(user)")
-                let mongoClient = user.mongoClient("mongodb1")
-                let database = mongoClient.database(named: "test_data")
-                self.collection = database.collection(withName: "AuditEvent")
-                _ = self.collection.deleteManyDocuments(filter: [:])
-            case .success(_):
-                print("This case should never be hit")
+                fatalError("Login failed: \(error.localizedDescription)")
+            case .success(let user):
+                // Continue
+                print("Successfully logged in to app")
+                var config = user.configuration(partitionValue: "Some partition value")
+                let realm = try! Realm(configuration: config)
+                try! realm.write {
+                    let eventPeople = realm.objects(EventLibrary_Person.self)
+                    realm.delete(eventPeople)
+                }
             }
         }
     }
@@ -61,71 +79,153 @@ class EventLibrary: XCTestCase {
     }
 
     func testDefaultEventInitilization() {
-        // :code-block-start: default-event-configuration
-        var config = user.configuration(partitionValue: "Some partition value")
-        config.eventConfiguration = EventConfiguration()
-        // :code-block-end:
-    }
-    
-    func testEventInitilizationWithParams() async {
-        // :code-block-start: event-configuration-with-params
-        let eventSyncUser = await login()
-        var config = user.configuration(partitionValue: "Some partition value")
-        config.eventConfiguration = EventConfiguration(metadata: ["username": "Jason Bourne"], syncUser: eventSyncUser, partitionPrefix: "event-")
-        // :code-block-end:
-        
-        func login() async -> User? {
-            do {
-                let app = App(id: YOUR_REALM_APP_ID)
-                // Authenticate with the instance of the app that points
-                // to your backend. Here, we're using anonymous login.
-                let user = try await app.login(credentials: Credentials.anonymous)
-                print("Successfully logged in user: \(user)")
-                return user
-            } catch {
-                print("Failed to log in user: \(error.localizedDescription)")
+        app.login(credentials: Credentials.anonymous) { (result) in
+            switch result {
+            case .failure(let error):
+                fatalError("Login failed: \(error.localizedDescription)")
+            case .success(let user):
+                // Continue
+                print("Successfully logged in to app")
+                initializeDefaultEventConfiguration()
             }
-            return nil
+        }
+        func initializeDefaultEventConfiguration() {
+            let user = app.currentUser!
+            // :code-block-start: default-event-configuration
+            var config = user.configuration(partitionValue: "Some partition value")
+            config.eventConfiguration = EventConfiguration()
+            // :code-block-end:
         }
     }
-    
+
+    func testEventInitilizationWithParams() {
+        app.login(credentials: Credentials.anonymous) { (result) in
+            switch result {
+            case .failure(let error):
+                fatalError("Login failed: \(error.localizedDescription)")
+            case .success(let user):
+                // Continue
+                print("Successfully logged in to app")
+//                initializeEventConfigurationWithParameters()
+            }
+        }
+
+        func initializeEventConfigurationWithParameters() async throws {
+            // :code-block-start: event-configuration-with-params
+            let eventSyncUser = try await app.login(credentials: Credentials.anonymous)
+            var config = user.configuration(partitionValue: "Some partition value")
+            config.eventConfiguration = EventConfiguration(metadata: ["username": "Jason Bourne"], syncUser: eventSyncUser, partitionPrefix: "event-")
+            // :code-block-end:
+        }
+    }
+
     func testInvokeEventRealm() {
-        var config = user.configuration(partitionValue: "Some partition value")
-        config.eventConfiguration = EventConfiguration()
-        // :code-block-start: invoke-event-realm
-        let realm = try! Realm(configuration: config)
-        let events = realm.events!
-        // :code-block-end:
-    }
-    
-    func recordReadAndWriteEvents() {
-        var config = user.configuration(partitionValue: "Some partition value")
-        config.eventConfiguration = EventConfiguration()
-        let realm = try! Realm(configuration: config)
-        let events = realm.events!
-        // :code-block-start: record-read-and-write-events
-        // Read event
-        events.beginScope(activity: "read object")
-        var person = realm.objects(Person.self).first!
-        events.endScope()
-        events.beginScope(activity: "mutate object")
-        // Write event
-        try! realm.write {
-            // Change name from "Anthony" to "Tony"
-            person.name = "Tony"
+        app.login(credentials: Credentials.anonymous) { (result) in
+            switch result {
+            case .failure(let error):
+                fatalError("Login failed: \(error.localizedDescription)")
+            case .success(let user):
+                // Continue
+                print("Successfully logged in to app")
+                invokeEventRealm()
+            }
         }
-        events.endScope()
-        // :code-block-end:
+
+        func invokeEventRealm() {
+            let user = app.currentUser!
+            var config = user.configuration(partitionValue: "Some partition value")
+            config.eventConfiguration = EventConfiguration()
+            config.objectTypes = [EventLibrary_Person.self]
+            // :code-block-start: invoke-event-realm
+            let realm = try! Realm(configuration: config)
+            let events = realm.events!
+            // :code-block-end:
+        }
     }
-    
-    func recordCustomEvents() {
-        var config = user.configuration(partitionValue: "Some partition value")
-        config.eventConfiguration = EventConfiguration()
-        let realm = try! Realm(configuration: config)
-        let events = realm.events!
-        // :code-block-start: record-custom-events
-        events.recordEvent(activity: "event", eventType: "press Home button")
-        // :code-block-end:
+
+    func testRecordReadAndWriteEvents() {
+        let expectation = XCTestExpectation(description: "Populate a read and write event")
+        // createTestData()
+        app.login(credentials: Credentials.anonymous) { (result) in
+            switch result {
+            case .failure(let error):
+                fatalError("Login failed: \(error.localizedDescription)")
+            case .success(let user):
+                print("Successfully logged in to app")
+                let user = app.currentUser!
+                var config = user.configuration(partitionValue: "Some partition value")
+                config.eventConfiguration = EventConfiguration()
+                config.objectTypes = [EventLibrary_Person.self]
+                Realm.asyncOpen(configuration: config) { (result) in
+                    switch result {
+                    case .failure(let error):
+                        print("Failed to open realm: \(error.localizedDescription)")
+                    case .success(let realm):
+                        // Create a person to use in this func
+                        try! realm.write {
+                            realm.create(EventLibrary_Person.self, value: ["name": "Anthony", "employeeId": 1])
+                        }
+                        print("Successfully created a person")
+                        recordReadAndWriteEvents(realm)
+                    }
+                }
+            }
+        }
+
+        func recordReadAndWriteEvents(_ realm: Realm) {
+            let events = realm.events!
+            // :code-block-start: record-read-and-write-events
+            // Read event
+            events.beginScope(activity: "read object")
+            print("I am in read object scope")
+            let person = realm.objects(EventLibrary_Person.self).first!
+            print("Successfully queried for person: \(person)")
+            events.endScope()
+            print("Read object scope has ended")
+            events.beginScope(activity: "mutate object")
+            print("I am in write object scope")
+            // Write event
+            try! realm.write {
+                // Change name from "Anthony" to "Tony"
+                person.name = "Tony"
+            }
+            print("Successfully changed the person's name to: \(person.name)")
+            events.endScope()
+            print("Write object scope has ended")
+            // :code-block-end:
+            deleteTestData()
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 10)
+    }
+
+    func testRecordCustomEvents() {
+        let expectation = XCTestExpectation(description: "Record a custom event")
+        app.login(credentials: Credentials.anonymous) { (result) in
+            switch result {
+            case .failure(let error):
+                fatalError("Login failed: \(error.localizedDescription)")
+            case .success(let user):
+                print("Successfully logged in to app")
+                let user = app.currentUser!
+                var config = user.configuration(partitionValue: "Some partition value")
+                config.eventConfiguration = EventConfiguration()
+                config.objectTypes = [EventLibrary_Person.self]
+                Realm.asyncOpen(configuration: config) { (result) in
+                    switch result {
+                    case .failure(let error):
+                        print("Failed to open realm: \(error.localizedDescription)")
+                    case .success(let realm):
+                        let events = realm.events!
+                        // :code-block-start: record-custom-events
+                        events.recordEvent(activity: "event", eventType: "custom event")
+                        // :code-block-end:
+                        expectation.fulfill()
+                    }
+                }
+            }
+        }
+        wait(for: [expectation], timeout: 10)
     }
 }
 
