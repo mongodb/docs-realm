fa8f1ca890f7c592f8b8818db676c6618e14eeda
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-06-03 16:13:18 -0400

    Misc Flutter fixes (#1962)
    * make pages folders
    
    * Fix type mistakes because of renaming (#1961)
    
    a -> an
    
    * add flutter app services as toc landing page
    
    * fix broken links
    
    * implement additional desi feedback
    
    * Fix some snippets (#1964)
    
    * add on this page nav
    
    * update dart examples to beta release from prerelease
    
    Co-authored-by: Desislava Stefanova <95419820+desistefanova@users.noreply.github.com>

--- source/examples/generated/flutter/manage_sync_session_test.snippet.add-subscription.dart
+++ source/examples/generated/flutter/manage_sync_session_test.snippet.add-subscription.dart
@@ -1,5 +1,5 @@
 final planeQuery = realm.all<Plane>();
-final longTrainQuery = realm.all<Train>().query("numCars >= 4");
+final longTrainQuery = realm.query<Train>("numCars >= 4");
 realm.subscriptions.update((MutableSubscriptionSet mutableSubscriptions) {
   mutableSubscriptions.add(planeQuery, name: "all-planes");
   mutableSubscriptions.add(longTrainQuery,
