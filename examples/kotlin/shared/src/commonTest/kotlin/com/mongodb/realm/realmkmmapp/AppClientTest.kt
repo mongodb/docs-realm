37daf92ff645b57e0d4364a37d5e6d1e0dc0eb95
nathan-contino-mongo <nathan.contino@mongodb.com>
2022-06-22 17:08:44 -0400

    polish

--- examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/AppClientTest.kt
+++ examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/AppClientTest.kt
@@ -29,23 +29,39 @@ class AppClientTest: RealmTest() {
 
     @Test
     fun testErrorHandlingTest() {
+        var email = getRandom()
+        var password = getRandom()
         runBlocking {
             // :snippet-start: handle-errors
             val app = App.create(YOUR_APP_ID)
             runCatching {
-                app.login(Credentials.anonymous())
+                app.login(Credentials.emailPassword(email, password))
             }.onSuccess {
                 Log.v("Successfully logged in")
+                // transition to another activity, load a fragment, to display logged in information here
             }.onFailure { ex: Throwable ->
                 when (ex) {
                     is InvalidCredentialsException -> {
-                        Log.v("Invalid username or password. Please try again.")
+                        Log.v("Failed to login due to invalid credentials: ${ex.message}")
+                        // :uncomment-start:
+                        // Toast.makeText(baseContext,
+                        //     "Invalid username or password. Please try again.", Toast.LENGTH_LONG).show()
+                        // :uncomment-end:
                     }
                     is ConnectionException -> {
-                        Log.e(ex.toString())
+                        Log.e("Failed to login due to a connection error: ${ex.message}")
+                        // :uncomment-start:
+                        // Toast.makeText(baseContext,
+                        //     "Login failed due to a connection error. Check your network connection and try again.", Toast.LENGTH_LONG).show()
+                        // :uncomment-end:
                     }
                     else -> {
-                        Log.e(ex.toString())
+                        Log.e("Failed to login: ${ex.message}")
+                        // generic error message for niche and unknown fail cases
+                        // :uncomment-start:
+                        // Toast.makeText(baseContext,
+                        //     "Login failed. Please try again.", Toast.LENGTH_LONG).show()
+                        // :uncomment-end:
                     }
                 }
             }
