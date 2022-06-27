59fdaf684ce157d1d2c6207577653e2d2f20f7ca
Dachary <dachary.carey@mongodb.com>
2022-06-27 18:17:52 -0400

    (DOCSP-23033): Add guidance around using Realm with SwiftUI Previews (#1983)
    * Add guidance around using Realm with SwiftUI Previews
    * Incorporate feedback

---
+++ source/examples/generated/swiftui/examples/SwiftUIFlexSyncExampleApp.snippet.preview-detail-view.swift
@@ -0,0 +1,7 @@
+struct ItemDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ItemDetailsView(item: Item.item2)
+        }
+    }
+}
---
+++ source/examples/generated/swiftui/examples/SwiftUIFlexSyncExampleApp.snippet.preview-extend-model-class-with-realm.swift
@@ -0,0 +1,25 @@
+extension ItemGroup {
+    static let itemGroup = ItemGroup(value: ["ownerId": "previewRealm"])
+    
+    static var previewRealm: Realm {
+        var realm: Realm
+        let identifier = "previewRealm"
+        let config = Realm.Configuration(inMemoryIdentifier: identifier)
+        do {
+            realm = try Realm(configuration: config)
+            // Check to see whether the in-memory realm already contains an ItemGroup. If it does, we'll just return the existing realm. If it doesn't, we'll add an ItemGroup and append the Items.
+            let realmObjects = realm.objects(ItemGroup.self)
+            if realmObjects.count == 1 {
+                return realm
+            } else {
+                try realm.write {
+                    realm.add(itemGroup)
+                    itemGroup.items.append(objectsIn: [Item.item1, Item.item2, Item.item3])
+                }
+                return realm
+            }
+        } catch let error {
+            fatalError("Can't bootstrap item data: \(error.localizedDescription)")
+        }
+    }
+}
