153af7802d0d02a31bebeafefe4e01b29470741d
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-08-01 12:50:07 -0400

    finish complete draft of page

--- source/examples/generated/flutter/quick_start_sync_test.snippet.quick-start-sync-complete.dart
+++ source/examples/generated/flutter/quick_start_sync_test.snippet.quick-start-sync-complete.dart
@@ -1,26 +1,17 @@
-// :snippet-start: quick-start-sync-complete
 import 'sync_schemas.dart';
 import 'package:realm_dart/realm.dart';
 
 void main() async {
-  const YOUR_APP_ID_HERE = 'flutter-flexible-luccm'; // :remove:
   // Add your App Services App ID
   const APP_ID = YOUR_APP_ID_HERE;
-  // :snippet-start: init-app
   App app = App(AppConfiguration(APP_ID));
-  // :snippet-end:
-  // :snippet-start: log-in
   User loggedInUser = await app.logIn(Credentials.anonymous());
-  // :snippet-end:
   print('Logged in anonymously with user id: ${loggedInUser.id}');
-  // :snippet-start: open-sync-realm
   Configuration config =
       Configuration.flexibleSync(loggedInUser, [Todo.schema]);
   Realm realm = Realm(
     config,
   );
-  // :snippet-end:
-  // :snippet-start: add-sync-subscription
 // Check if the subscription already exists before adding
   final userTodoSub = realm.subscriptions.findByName('getUserTodos');
   if (userTodoSub == null) {
@@ -29,44 +20,30 @@ void main() async {
       mutableSubscriptions.add(realm.all<Todo>(), name: 'getUserTodos');
     });
   }
-  // :snippet-end:
-  // :snippet-start: create-object
   Todo buyMilk = realm.write(() {
     return realm.add<Todo>(Todo(ObjectId(), 'Buy milk', loggedInUser.id));
   });
-  // :snippet-end:
   print('Created task: ${buyMilk.summary}');
-  // :snippet-start: get-all-objects
   RealmResults<Todo> allTodos = realm.all<Todo>();
-  // :snippet-end:
-  // :snippet-start: filter-results
   RealmResults<Todo> incompleteTodos = realm.query<Todo>("isComplete == false");
-  // :snippet-end:
   print("'Buy milk' status _before_ update: ${buyMilk.isComplete.toString()}");
   print("Deleting 'Buy Milk'");
-  // :snippet-start: update-object
   realm.write(() {
     buyMilk.isComplete = true;
   });
-  // :snippet-end:
   print("'Buy milk' status _after update: ${buyMilk.isComplete.toString()}");
-  // :snippet-start: delete-one
   realm.write(() {
     realm.delete(buyMilk);
   });
-  // :snippet-end:
-  // :snippet-start: delete-many
   realm.write(() {
     realm.deleteMany(allTodos);
   });
-  // :snippet-end:
   late Todo drinkCoffee;
   late Todo takeNap;
   realm.write(() {
     drinkCoffee = realm.add(Todo(ObjectId(), 'Drink coffee', loggedInUser.id));
     takeNap = realm.add(Todo(ObjectId(), 'Take nap', loggedInUser.id));
   });
-  // :snippet-start: watch-changes-collection
   // Listen for changes on whole collection
   final todos = realm.all<Todo>();
   final todosSubscription = todos.changes.listen((changes) {
@@ -86,7 +63,6 @@ void main() async {
   final completedTodosSubscription = completedTodos.changes.listen((changes) {
     // ... all the same data as above
   });
-  // :snippet-end:
   realm.write(() {
     drinkCoffee.isComplete = true;
     takeNap.isComplete = true;
@@ -98,7 +74,6 @@ void main() async {
   buyMilk = realm.write(() {
     return realm.add<Todo>(Todo(ObjectId(), 'Buy milk', loggedInUser.id));
   });
-  // :snippet-start: watch-changes-object
   final buyMilkSubscription = buyMilk.changes.listen((changes) {
     changes.isDeleted; // if the object has been deleted
     changes.object; // the RealmObject being listened to, `buyMilk`
@@ -108,7 +83,6 @@ void main() async {
   realm.write(() {
     buyMilk.isComplete = true;
   });
-  // :snippet-end:
   // Give time for async subscription change listeners to fire
   await Future.delayed(Duration(seconds: 1));
   await buyMilkSubscription.cancel();
@@ -119,4 +93,3 @@ void main() async {
   print('Bye bye :)');
   Realm.shutdown();
 }
-// :snippet-end:
