fbee90cfbd112bb50d4556d0b01d6f7b704e8479
Nathan Contino <ncontino@protonmail.com>
2022-08-16 11:17:04 -0400

    Improved Gradle documentation and dependency management across Kotlin and Java SDK example projects (#2089)
    * Improved Gradle documentation and dependency management across Kotlin and Java SDK example projects
    
    * bumped kotlin SDK version
    
    * Delete local.properties
    
    * Delete local.properties

--- examples/kotlin/settings.gradle.kts
+++ examples/kotlin/settings.gradle.kts
@@ -6,6 +6,21 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            version("realm", "1.0.2")
+            version("kotlinx-coroutines", "1.6.0-native-mt")
+            library("realm-plugin", "io.realm.kotlin", "gradle-plugin").versionRef("realm")
+            library("realm", "io.realm.kotlin", "library-base").versionRef("realm")
+            library("realm-sync", "io.realm.kotlin", "library-sync").versionRef("realm")
+            library("kotlinx-coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core").versionRef("kotlinx-coroutines")
+            library("kotlinx-coroutines-android", "org.jetbrains.kotlinx", "kotlinx-coroutines-android").versionRef("kotlinx-coroutines")
+            library("kotlinx-coroutines-test", "org.jetbrains.kotlinx", "kotlinx-coroutines-test").versionRef("kotlinx-coroutines")
+        }
+    }
+}
+
 rootProject.name = "RealmKMMApp"
 include(":androidRealmKMMApp")
 include(":shared")
