9c68643bcf81a539e11fad7770dbbff8408b4e8d
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-08-17 14:05:38 -0400

    add current realm version and delete user code

--- examples/dart/test/authenticate_users_test.dart
+++ examples/dart/test/authenticate_users_test.dart
@@ -232,14 +232,14 @@ void main() {
   });
 
   test('Delete user', () async {
-    await authProvider.registerUser("lisa@example.com", "myStr0ngPassw0rd");
-    final credentials =
-        Credentials.emailPassword("lisa@example.com", "myStr0ngPassw0rd");
-    await app.logIn(credentials);
-    // :snippet-start: delete-user
-    final currentUser = app.currentUser!;
-    // await App.deleteUser(currentUser);
-    // :snippet-end:
-    expect(app.currentUser, null);
-  }, skip: "skip til 0.3.2+beta working");
+    // await authProvider.registerUser("lisa@example.com", "myStr0ngPassw0rd");
+    // final credentials =
+    //     Credentials.emailPassword("lisa@example.com", "myStr0ngPassw0rd");
+    // await app.logIn(credentials);
+    // // :snippet-start: delete-user
+    // final currentUser = app.currentUser!;
+    // await app.deleteUser(currentUser);
+    // // :snippet-end:
+    expect(true, false);
+  });
 }
