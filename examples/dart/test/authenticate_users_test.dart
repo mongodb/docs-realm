bb5f2d895f42ca5d4e459930f9177ba64288b7f6
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-08-18 17:18:43 -0400

    clarify code

--- examples/dart/test/authenticate_users_test.dart
+++ examples/dart/test/authenticate_users_test.dart
@@ -93,7 +93,9 @@ void main() {
       Map<String, String> credentials = {
         "username": "iloverealm",
       };
+      // payload must be a JSON-encoded string
       String payload = jsonEncode(credentials);
+
       Credentials customCredentials = Credentials.function(payload);
       User currentUser = await app.logIn(customCredentials);
       // :snippet-end:
