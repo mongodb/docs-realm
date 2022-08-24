e15b139d2805257c7b9e0575faf3ca671e67fa84
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-08-24 17:26:07 -0400

    latest draft

---
+++ source/examples/tutorial/flutter/create_item.modified.dart
@@ -0,0 +1,44 @@
+// ... other imports
+import 'package:flutter_todo/viewmodels/item_viewmodel.dart';
+
+// ... CreateItem widget
+
+// _CreateItemFormWrapper widget
+
+class CreateItemForm extends StatefulWidget {
+  const CreateItemForm({Key? key}) : super(key: key);
+
+  @override
+  _CreateItemFormState createState() => _CreateItemFormState();
+}
+
+class _CreateItemFormState extends State<CreateItemForm> {
+  int _priority = PriorityLevel.low;
+  final _formKey = GlobalKey<FormState>();
+  var taskEditingController = TextEditingController();
+
+  void _setPriority(int priority) {
+    setState(() {
+      _priority = priority;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextTheme myTextTheme = Theme.of(context).textTheme;
+    final currentUser = Provider.of<AppServices>(context).currentUser;
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          // ... other widgets
+          SelectPriority(_priority, _setPriority),
+          // .. other widgets
+        ],
+      ),
+    );
+  }
+}
