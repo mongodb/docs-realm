f196ed88fa2754baf0d6b4ca67727baf74897219
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-09-07 13:18:10 -0400

    update clientside rendering

--- source/examples/generated/web/index.snippet.log-in-index-js.js
+++ source/examples/generated/web/index.snippet.log-in-index-js.js
@@ -5,9 +5,8 @@ import Link from "next/link";
 export default function Home() {
   const app = Realm.App.getApp(process.env.NEXT_PUBLIC_APP_ID);
 
+  // only runs on the client
   useEffect(() => {
-    // For initial server-side render
-    if (!app) return;
     // If no logged in user, log in
     if (!app.currentUser) {
       const anonymousUser = Realm.Credentials.anonymous();
