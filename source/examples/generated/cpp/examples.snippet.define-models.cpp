1ad342fd39ad880d0f5960095221c5734e23c151
Chris Bush <chris.bush@mongodb.com>
2022-09-09 11:36:38 -0400

    Add examples

--- source/examples/generated/cpp/examples.snippet.define-models.cpp
+++ source/examples/generated/cpp/examples.snippet.define-models.cpp
@@ -1,11 +1,3 @@
-#include <catch2/catch_test_macros.hpp>
-#include <string>
-
-// :snippet-start: includes
-#include <cpprealm/sdk.hpp>
-// :snippet-end:
-
-// :snippet-start: define-models
 // Define your models like regular structs.
 struct Dog : realm::object {
     realm::persisted<std::string> name;
@@ -32,21 +24,3 @@ struct Person : realm::object {
         realm::property<"dog", &Person::dog>
     >;
 };
-// :snippet-end:
-
-TEST_CASE("first test case", "[test]") {
-    // :snippet-start: usage
-    // Use Realm objects like regular objects.
-    auto dog = Dog { .name = "Rex", .age = 1 };
-    
-    std::cout << "dog: " << dog << "\n";
-
-    // Get the default Realm with compile time schema checking.
-    auto realm = realm::open<Person, Dog>();
-
-    // Persist your data in a write transaction
-    realm.write([&realm, &dog] {
-        realm.add(dog);
-    });
-    // :snippet-end:
-}
