cdc3cbc97b90a98d10b9b5dee9590ffae672de29
Dachary Carey <dachary.carey@mongodb.com>
2022-09-19 16:31:51 -0400

    Add .toUtc to example, wording improvements

--- examples/dart/test/data_types_test.dart
+++ examples/dart/test/data_types_test.dart
@@ -76,8 +76,8 @@ main() {
     final config = Configuration.local([Vehicle.schema]);
     final realm = Realm(config);
 
+    // Create a Realm object with date in UTC, or convert with .toUtc() before storing
     Vehicle subaruOutback = realm.write<Vehicle>(() {
-      // Create a Realm object with date in UTC, or convert with .toUtc() before storing
       return realm.add(Vehicle('Subie', DateTime.utc(2022, 9, 18, 12, 30, 0)));
     });
     // :remove-start:
@@ -85,14 +85,19 @@ main() {
         subaruOutback.dateLastServiced.toString(), '2022-09-18 12:30:00.000Z');
     // :remove-end:
 
+    final fordFusion = Vehicle('Fuse', DateTime(2022, 9, 18, 8, 30, 0).toUtc());
+    realm.write(() {
+      realm.add(fordFusion);
+    });
+
     // When you query the object, the `DateTime` returned is UTC
     final queriedSubaruOutback =
         realm.all<Vehicle>().query('nickname == "Subie"').first;
     expect(queriedSubaruOutback.nickname, 'Subie'); // :remove:
 
     // :remove-start:
-    final queriedDate = queriedSubaruOutback.dateLastServiced.toString();
-    expect(queriedDate, '2022-09-18 12:30:00.000Z');
+    final queriedSubieDate = queriedSubaruOutback.dateLastServiced.toString();
+    expect(queriedSubieDate, '2022-09-18 12:30:00.000Z');
     // :remove-end:
     // If your app needs it, convert it to Local() or the desired time zone
     final localizedSubieDateLastServiced =
--- source/examples/generated/flutter/data_types_test.snippet.datetime-use.dart
+++ source/examples/generated/flutter/data_types_test.snippet.datetime-use.dart
@@ -1,9 +1,14 @@
 final config = Configuration.local([Vehicle.schema]);
 final realm = Realm(config);
 
+// Create a Realm object with date in UTC, or convert with .toUtc() before storing
 Vehicle subaruOutback = realm.write<Vehicle>(() {
-  // Create a Realm object with date in UTC, or convert with .toUtc() before storing
   return realm.add(Vehicle('Subie', DateTime.utc(2022, 9, 18, 12, 30, 0)));
+});
+
+final fordFusion = Vehicle('Fuse', DateTime(2022, 9, 18, 8, 30, 0).toUtc());
+realm.write(() {
+  realm.add(fordFusion);
 });
 
 // When you query the object, the `DateTime` returned is UTC
