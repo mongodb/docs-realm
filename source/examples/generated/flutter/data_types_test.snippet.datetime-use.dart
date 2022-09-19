bbac28da896fabe95d40523f59c3f901996a1786
Dachary Carey <dachary.carey@mongodb.com>
2022-09-19 17:05:52 -0400

    Rerun Bluehawk to generate updated snippet

--- source/examples/generated/flutter/data_types_test.snippet.datetime-use.dart
+++ source/examples/generated/flutter/data_types_test.snippet.datetime-use.dart
@@ -1,6 +1,3 @@
-final config = Configuration.local([Vehicle.schema]);
-final realm = Realm(config);
-
 // Create a Realm object with date in UTC, or convert with .toUtc() before storing
 Vehicle subaruOutback = realm.write<Vehicle>(() {
   return realm.add(Vehicle('Subie', DateTime.utc(2022, 9, 18, 12, 30, 0)));
@@ -13,10 +10,8 @@ realm.write(() {
 
 // When you query the object, the `DateTime` returned is UTC
 final queriedSubaruOutback =
-    realm.all<Vehicle>().query('nickname == "Subie"').first;
+    realm.all<Vehicle>().query('nickname == "Subie"')[0];
 
 // If your app needs it, convert it to Local() or the desired time zone
 final localizedSubieDateLastServiced =
     queriedSubaruOutback.dateLastServiced.toLocal();
-
-realm.close();
