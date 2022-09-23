7d3ea213b8dc3822c0767645293acce0bbade867
lindseymoore <lindsey.moore@mongodb.com>
2022-09-23 12:32:37 -0400

    Revert "(DOCSP-23333): Asymmetric Sync docs for Swift SDK (#2171)"
    This reverts commit ba6f1f966781754163d5fd89f14465e2212b752d.

--- source/examples/generated/code/start/AsymmetricSync.snippet.asymmetric-model.swift
+++
@@ -1,7 +0,0 @@
-class WeatherSensor: AsymmetricObject {
-    @Persisted(primaryKey: true) var _id: ObjectId
-    @Persisted var deviceId: String
-    @Persisted var temperatureInFahrenheit: Float
-    @Persisted var barometricPressureInHg: Float
-    @Persisted var windSpeedInMph: Int
-}
--- source/examples/generated/code/start/AsymmetricSync.snippet.create-asymmetric-object.swift
+++
@@ -1,9 +0,0 @@
-try! asymmetricRealm.write {
-    asymmetricRealm.create(WeatherSensor.self,
-                           value: [ "_id": ObjectId.generate(),
-                                    "deviceId": "WX1278UIT",
-                                    "temperatureInFahrenheit": 66.7,
-                                    "barometricPressureInHg": 29.65,
-                                    "windSpeedInMph": 2
-                                    ])
-}
