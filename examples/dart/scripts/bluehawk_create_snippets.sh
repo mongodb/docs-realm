c2922372faab6af8cac93f1129e0046f484cfb20
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-10-03 14:10:45 -0400

    create snips

--- examples/dart/scripts/bluehawk_create_snippets.sh
+++ examples/dart/scripts/bluehawk_create_snippets.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+DIR_OUT=$(git rev-parse --show-toplevel)/source/examples/generated/flutter
+
+# remove existing examples in the directory
+rm -rf $DIR_OUT/^(?!.*(/tutorial/))
+echo "Deleted old snippets"
+
 # standard bluehawking
 bluehawk snip $(git rev-parse --show-toplevel)/examples/dart/test \
--o $(git rev-parse --show-toplevel)/source/examples/generated/flutter
+-o $DIR_OUT
