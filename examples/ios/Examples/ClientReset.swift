b1be3a64cf3606e049a1fba019ccaf74ea113cb4
Dachary Carey <dachary.carey@mongodb.com>
2022-10-07 11:08:32 -0400

    Swift client reset with recovery docs

--- examples/ios/Examples/ClientReset.swift
+++ examples/ios/Examples/ClientReset.swift
@@ -6,6 +6,8 @@
 import XCTest
 import RealmSwift
 
+let APP_ID = "swift-flexible-vkljj"
+
 class ClientReset_Dog: Object {
     @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted var name = ""
@@ -24,8 +26,8 @@ class ClientReset: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testSpecifyClientResetMode() async {
-        // :snippet-start: client-reset-discard-changes-with-blocks
+    func testSpecifyClientResetModeWithBeforeAfterBlock() async {
+        // :snippet-start: client-reset-with-blocks
         let beforeClientResetBlock: (Realm) -> Void = { before in
             // This block could be used to back-up a realm file, send reporting, etc.
         }
@@ -37,13 +39,79 @@ class ClientReset: XCTestCase {
         do {
             let app = App(id: YOUR_APP_SERVICES_APP_ID)
             let user = try await app.login(credentials: Credentials.anonymous)
+            var configuration = user.flexibleSyncConfiguration(clientResetMode: .recoverOrDiscardUnsyncedChanges(beforeReset: beforeClientResetBlock, afterReset: afterClientResetBlock))
+        } catch {
+            print("Error logging in user: \(error.localizedDescription)")
+        }
+        // :snippet-end:
+    }
+    
+    func testSpecifyRecoverUnsyncedChanges() async {
+        do {
+            let app = App(id: YOUR_APP_SERVICES_APP_ID)
+            let user = try await app.login(credentials: Credentials.anonymous)
             // :snippet-start: specify-client-reset-mode
             // Specify the clientResetMode when you create the SyncConfiguration.
-            // If you do not specify, this defaults to `.manual` mode.
-            var configuration = user.configuration(partitionValue: "myPartition", clientResetMode: .discardLocal(beforeClientResetBlock, afterClientResetBlock))
+            // If you do not specify, this defaults to `.recoverUnsyncedChanges` mode.
+            var configuration = user.flexibleSyncConfiguration(clientResetMode: .recoverUnsyncedChanges())
             // :snippet-end:
         } catch {
             print("Error logging in user: \(error.localizedDescription)")
+        }
+    }
+    
+    func testSpecifyDiscardUnsyncedChanges() async {
+        // :snippet-start: specify-discard
+        do {
+            let app = App(id: APP_ID)
+            let user = try await app.login(credentials: Credentials.anonymous)
+            var config = user.flexibleSyncConfiguration(clientResetMode: .discardUnsyncedChanges())
+        } catch {
+            print("Error logging in user: \(error.localizedDescription)")
+        }
+        // :snippet-end:
+    }
+    
+    func testSpecifyManual() async {
+        // :snippet-start: specify-manual
+        do {
+            let app = App(id: APP_ID)
+            let user = try await app.login(credentials: Credentials.anonymous)
+            var config = user.flexibleSyncConfiguration(clientResetMode: .manual())
+        } catch {
+            print("Error logging in user: \(error.localizedDescription)")
+        }
+        // :snippet-end:
+    }
+    
+    func testManualFallbackErrorHandler() async {
+        // :snippet-start: manual-fallback-error-handler
+        func handleClientReset() {
+            // Report the client reset error to the user, or do some custom logic.
+        }
+        
+        do {
+            let app = App(id: APP_ID)
+            let user = try await app.login(credentials: Credentials.anonymous)
+            var config = user.flexibleSyncConfiguration(clientResetMode: .recoverOrDiscardUnsyncedChanges())
+            // If client recovery fails,
+            app.syncManager.errorHandler = { error, session in
+                guard let syncError = error as? SyncError else {
+                    fatalError("Unexpected error type passed to sync error handler! \(error)")
+                }
+                switch syncError.code {
+                case .clientResetError:
+                    if let (path, clientResetToken) = syncError.clientResetInfo() {
+                        handleClientReset()
+                        SyncSession.immediatelyHandleError(clientResetToken, syncManager: app.syncManager)
+                    }
+                default:
+                    // Handle other errors...
+                    ()
+                }
+            }
+        } catch {
+            print("Error: \(error.localizedDescription)")
         }
         // :snippet-end:
     }
--- source/examples/generated/code/start/ClientReset.snippet.client-reset-with-blocks.swift
+++ source/examples/generated/code/start/ClientReset.snippet.client-reset-with-blocks.swift
@@ -9,9 +9,7 @@ let afterClientResetBlock: (Realm, Realm) -> Void = { before, after in
 do {
     let app = App(id: YOUR_APP_SERVICES_APP_ID)
     let user = try await app.login(credentials: Credentials.anonymous)
-    // Specify the clientResetMode when you create the SyncConfiguration.
-    // If you do not specify, this defaults to `.manual` mode.
-    var configuration = user.configuration(partitionValue: "myPartition", clientResetMode: .discardLocal(beforeClientResetBlock, afterClientResetBlock))
+    var configuration = user.flexibleSyncConfiguration(clientResetMode: .recoverOrDiscardUnsyncedChanges(beforeReset: beforeClientResetBlock, afterReset: afterClientResetBlock))
 } catch {
     print("Error logging in user: \(error.localizedDescription)")
 }
