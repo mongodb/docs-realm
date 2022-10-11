ecc873c06d3e05d83759e921a688e3e98641872a
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-10-11 17:22:32 -0400

    first frozen realm example

--- examples/dart/test/freeze_test.dart
+++ examples/dart/test/freeze_test.dart
@@ -1,10 +1,43 @@
 import 'package:realm_dart/realm.dart';
 import 'package:test/test.dart';
 import './utils.dart';
+import './schemas.dart';
 
 void main() {
   group("Frozen Objects - ", () {
-    test("Freeze a realm", () {});
+    late Realm realmToTearDown;
+    tearDown(() {
+      cleanUpRealm(realmToTearDown);
+    });
+    test("Freeze a realm", () {
+      // :snippet-start; freeze-realm
+      final config = Configuration.local([Person.schema, Scooter.schema]);
+      Realm realm = Realm(config);
+      // Add scooter ownded by Mace Windu
+      final maceWindu = Person(1, "Mace", "Windu");
+      final purpleScooter = Scooter(1, "Purple scooter", owner: maceWindu);
+      realm.write(() {
+        realm.add(purpleScooter);
+      });
+      // Create frozen snapshot of realm
+      final frozenRealm = realm.freeze();
+
+      // Update data in the realm
+      final quiGonJinn = Person(2, "Qui-Gon", "Jinn");
+      realm.write(() {
+        purpleScooter.owner = quiGonJinn;
+      });
+
+      // Data changes not in the frozen snapshot
+      final purpleScooterFrozen = frozenRealm.find<Scooter>(1);
+      print(purpleScooterFrozen!.owner!.firstName); // prints 'Mace'
+      // :snippet-end:
+      final purpleScooterLive = realm.find<Scooter>(1);
+      expect(purpleScooterLive!.owner!.firstName, 'Qui-Gon');
+      expect(purpleScooterFrozen.owner!.firstName, 'Mace');
+      realmToTearDown = realm;
+      frozenRealm.close();
+    });
     test("Freeze RealmResults", () {});
     test("Freeze a RealmObject", () {});
     test("Freeze a List in a RealmObject", () {});
