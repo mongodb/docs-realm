0ddc4716e613ba268a7e162dd75b76546200fbd5
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-10-11 16:38:24 -0400

    stub out test

---
+++ examples/dart/test/freeze_test.dart
@@ -0,0 +1,13 @@
+import 'package:realm_dart/realm.dart';
+import 'package:test/test.dart';
+import './utils.dart';
+
+void main() {
+  group("Frozen Objects - ", () {
+    test("Freeze a realm", () {});
+    test("Freeze RealmResults", () {});
+    test("Freeze a RealmObject", () {});
+    test("Freeze a List in a RealmObject", () {});
+    test("Check if data is frozen", () {});
+  });
+}
