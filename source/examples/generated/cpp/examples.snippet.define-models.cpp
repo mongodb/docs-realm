f87c4c1fe86852a15523eaba38cd0766e983a6f5
Chris Bush <chris.bush@mongodb.com>
2022-10-12 08:33:06 -0400

    (DOCSP-25729): Update for C++17 support (#2208)

--- source/examples/generated/cpp/examples.snippet.define-models.cpp
+++ source/examples/generated/cpp/examples.snippet.define-models.cpp
@@ -3,10 +3,9 @@ struct Dog : realm::object {
     realm::persisted<std::string> name;
     realm::persisted<int> age;
 
-    using schema = realm::schema<"Dog",
-        realm::property<"name", &Dog::name>,
-        realm::property<"age", &Dog::age>
-    >;
+    static constexpr auto schema = realm::schema("Dog",
+        realm::property<&Dog::name>("name"),
+        realm::property<&Dog::age>("age"));
 };
 
 struct Person : realm::object {
@@ -17,10 +16,9 @@ struct Person : realm::object {
     // Create relationships by pointing an Object field to another Class
     realm::persisted<std::optional<Dog>> dog;
 
-    using schema = realm::schema<"Person",
-        realm::property<"_id", &Person::_id, true>, // primary key
-        realm::property<"name", &Person::name>,
-        realm::property<"age", &Person::age>,
-        realm::property<"dog", &Person::dog>
-    >;
+    static constexpr auto schema = realm::schema("Person",
+        realm::property<&Person::_id, true>("_id"), // primary key
+        realm::property<&Person::name>("name"),
+        realm::property<&Person::age>("age"),
+        realm::property<&Person::dog>("dog"));
 };
