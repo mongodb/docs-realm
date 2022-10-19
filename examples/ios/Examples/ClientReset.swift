3998a939a7bb6aa3943453240eb1cfb117239816
Dachary Carey <dachary.carey@mongodb.com>
2022-10-19 17:35:16 -0400

    Update before and after blocks in code example, refactor manual error handler content

--- examples/ios/Examples/ClientReset.swift
+++ examples/ios/Examples/ClientReset.swift
@@ -30,6 +30,10 @@ class ClientReset: XCTestCase {
     func testSpecifyClientResetModeWithBeforeAfterBlock() async {
         let myRecoveryPath = URL(string: "some-file-url")
         // :snippet-start: client-reset-with-blocks
+        // A block called after a client reset error is detected, but before the
+        // client recovery process is executed.
+        // This block could be used for any custom logic, reporting, debugging etc.
+        // This is one example, but your usage may vary.
         let beforeClientResetBlock: (Realm) -> Void = { before in
             var recoveryConfig = Realm.Configuration()
             recoveryConfig.fileURL = myRecoveryPath
@@ -41,16 +45,17 @@ class ClientReset: XCTestCase {
              }
         }
 
+        // A block called after the client recovery process has executed.
+        // This block could be used for custom recovery, reporting, debugging etc.
+        // This is one example, but your usage may vary.
         let afterClientResetBlock: (Realm, Realm) -> Void = { before, after in
-            /// // This block could be used to add custom recovery logic, back-up a realm file, send reporting, etc. For illustration:
-            /// for object in before.objects(myClass.self) {
-            ///     let res = after.objects(myClass.self)
-            ///     if (res.filter("primaryKey == %@", object.primaryKey).first != nil) {
-            ///         // ...custom recovery logic...
-            ///     } else {
-            ///         // ...custom recovery logic...
-            ///     }
-            /// }
+            //     let res = after.objects(myClass.self)
+            //     if (res.filter("primaryKey == %@", object.primaryKey).first != nil) {
+            //         // ...custom recovery logic...
+            //     } else {
+            //         // ...custom recovery logic...
+            //     }
+            // }
         }
 
         do {
