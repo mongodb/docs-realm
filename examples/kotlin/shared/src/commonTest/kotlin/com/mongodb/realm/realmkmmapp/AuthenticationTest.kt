5f7d9afc347a815288b54ed274738676c4e128f3
cbullinger <cory.bullinger@mongodb.com>
2022-10-26 15:31:43 -0400

    fixed snippet and updated title

--- examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/AuthenticationTest.kt
+++ examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/AuthenticationTest.kt
@@ -12,10 +12,10 @@ class AuthenticationTest: RealmTest() {
         val email = getRandom()
         val password = getRandom()
         // :snippet-start: link-credentials
-        val app: App = App.create("mytutorialapp-kcavx") // Replace this with your App ID
+        val app: App = App.create(YOUR_APP_ID) // Replace this with your App ID
         runBlocking { // use runBlocking sparingly -- it can delay UI interactions
             val user = app.login(Credentials.anonymous()) // logs in with an anonymous user
-            // registering an email and password user
+            // registers an email and password user
             app.emailPasswordAuth.registerUser(email, password)
             // link anonymous user with email password credentials
             user.linkCredentials(Credentials.emailPassword(email, password));
