36b354fd5557d2a16eb3af9427166b01d9c4ad79
cbullinger <cory.bullinger@mongodb.com>
2022-10-26 15:31:43 -0400

    fixed snippet and updated title

--- source/examples/generated/kotlin/AuthenticationTest.snippet.link-credentials.kt
+++ source/examples/generated/kotlin/AuthenticationTest.snippet.link-credentials.kt
@@ -1,8 +1,8 @@
-val app: App = App.create("mytutorialapp-kcavx") // Replace this with your App ID
+val app: App = App.create(YOUR_APP_ID) // Replace this with your App ID
 runBlocking { // use runBlocking sparingly -- it can delay UI interactions
     val user = app.login(Credentials.anonymous()) // logs in with an anonymous user
-    // registering an email and password user
+    // registers an email/password user
     app.emailPasswordAuth.registerUser(email, password)
-    // link anonymous user with email password credentials
+    // links anonymous user with email/password credentials
     user.linkCredentials(Credentials.emailPassword(email, password));
 }
