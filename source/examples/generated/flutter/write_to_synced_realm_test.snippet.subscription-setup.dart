44c0b15fa84fc3ddb23bac11ac1666190b66060c
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-12-08 17:29:17 -0500

    draft tested code examples

---
+++ source/examples/generated/flutter/write_to_synced_realm_test.snippet.subscription-setup.dart
@@ -0,0 +1,13 @@
+final app = App(AppConfiguration(APP_ID));
+final user = await app.logIn(Credentials.anonymous());
+final config = Configuration.flexibleSync(user, [Car.schema]);
+final realm = Realm(config);
+
+// Add subscriptions
+realm.subscriptions.update((mutableSubscriptions) {
+  // Get Cars from Atlas that match the Realm Query Language query.
+  // Uses the queryable field `miles`.
+  final newCarQuery = realm.query<Car>("miles < 100 OR miles == \$0", [null]);
+  mutableSubscriptions.add(newCarQuery, name: "new-car-subscription");
+});
+await realm.subscriptions.waitForSynchronization();
