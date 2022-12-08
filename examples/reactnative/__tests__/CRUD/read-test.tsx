2e886c55a18ff24571f7eedd9df8d5226ff3fadf
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2022-12-08 18:11:02 -0500

    aded sort examples

--- examples/reactnative/__tests__/CRUD/read-test.tsx
+++ examples/reactnative/__tests__/CRUD/read-test.tsx
@@ -173,9 +173,8 @@ describe("Read Data Tests", () => {
         //   }
         // }
         const TaskList = () => {
-            const realm = useRealm();
             // retrieve the set of Task objects
-            const tasks = realm.objects("Task");
+            const tasks = useQuery("Task");
             // Sort tasks by name in ascending order
             const tasksByName = tasks.sorted("name");
             // Sort tasks by name in descending order
@@ -183,26 +182,26 @@ describe("Read Data Tests", () => {
             // Sort tasks by priority in descending order and then by name alphabetically
             const tasksByPriorityDescendingAndName = tasks.sorted([["priority", true], ["name", false],]);
             // Sort Tasks by Assignee's name.
-            const tasksByAssigneeName = realm.objects("Task").sorted("assignee.name");
+            const tasksByAssigneeName = tasks.sorted("assignee.name");
 
             return (
             <>
-            <Text>All tasks:</Text>
-            { tasks.map((task) => <Text testID='all-tasks-item' key={"all-tasks-" + task._id}>{task.name}</Text>)}
+                <Text>All tasks:</Text>
+                { tasks.map((task) => <Text testID='all-tasks-item'>{task.name}</Text>)}
 
-            <Text>Tasks sorted by name:</Text>
-            { tasksByName.map((task) => <Text testID='tasks-by-name-item' key={"tasks-by-name-" + task._id}>{task.name}</Text>)}
+                <Text>Tasks sorted by name:</Text>
+                { tasksByName.map((task) => <Text testID='tasks-by-name-item'>{task.name}</Text>)}
 
-            <Text>Tasks sorted by name descending:</Text>
-            { tasksByNameDescending.map((task) => <Text testID='tasks-by-name-descending-item' key={"tasks-by-name-desc-" + task._id}>{task.name}</Text>)}
+                <Text>Tasks sorted by name descending:</Text>
+                { tasksByNameDescending.map((task) => <Text testID='tasks-by-name-descending-item'>{task.name}</Text>)}
 
-            <Text>Tasks sorted by priority descending, and name alphabetically:</Text>
-            { tasksByPriorityDescendingAndName.map((task) => <Text testID='tasks-by-priority-descending-and-name-item' key={"tasks-by-priority-descending-and-name-" + task._id}>{task.name}</Text>)}
+                <Text>Tasks sorted by priority descending, and name alphabetically:</Text>
+                { tasksByPriorityDescendingAndName.map((task) => <Text testID='tasks-by-priority-descending-and-name-item'>{task.name}</Text>)}
 
-            <Text>Tasks sorted by assignee name:</Text>
-            { tasksByAssigneeName.map((task) => <Text testID='tasks-by-assignee-name-item' key={"tasks-by-assignee-name-" + task._id}>{task.name}</Text>)}
-
-            </>)
+                <Text>Tasks sorted by assignee name:</Text>
+                { tasksByAssigneeName.map((task) => <Text testID='tasks-by-assignee-name-item'>{task.name}</Text>)}
+            </>
+            )
         }
         // :replace-end:
         // :snippet-end:
--- source/examples/generated/reactnative/read-test.snippet.crud-read-sort-data.tsx
+++ source/examples/generated/reactnative/read-test.snippet.crud-read-sort-data.tsx
@@ -1,7 +1,6 @@
 const TaskList = () => {
-    const realm = useRealm();
     // retrieve the set of Task objects
-    const tasks = realm.objects("Task");
+    const tasks = useQuery("Task");
     // Sort tasks by name in ascending order
     const tasksByName = tasks.sorted("name");
     // Sort tasks by name in descending order
@@ -9,24 +8,24 @@ const TaskList = () => {
     // Sort tasks by priority in descending order and then by name alphabetically
     const tasksByPriorityDescendingAndName = tasks.sorted([["priority", true], ["name", false],]);
     // Sort Tasks by Assignee's name.
-    const tasksByAssigneeName = realm.objects("Task").sorted("assignee.name");
+    const tasksByAssigneeName = tasks.sorted("assignee.name");
 
     return (
     <>
-    <Text>All tasks:</Text>
-    { tasks.map((task) => <Text key={"all-tasks-" + task._id}>{task.name}</Text>)}
+        <Text>All tasks:</Text>
+        { tasks.map((task) => <Text>{task.name}</Text>)}
 
-    <Text>Tasks sorted by name:</Text>
-    { tasksByName.map((task) => <Text key={"tasks-by-name-" + task._id}>{task.name}</Text>)}
+        <Text>Tasks sorted by name:</Text>
+        { tasksByName.map((task) => <Text>{task.name}</Text>)}
 
-    <Text>Tasks sorted by name descending:</Text>
-    { tasksByNameDescending.map((task) => <Text key={"tasks-by-name-desc-" + task._id}>{task.name}</Text>)}
+        <Text>Tasks sorted by name descending:</Text>
+        { tasksByNameDescending.map((task) => <Text>{task.name}</Text>)}
 
-    <Text>Tasks sorted by priority descending, and name alphabetically:</Text>
-    { tasksByPriorityDescendingAndName.map((task) => <Text key={"tasks-by-priority-descending-and-name-" + task._id}>{task.name}</Text>)}
+        <Text>Tasks sorted by priority descending, and name alphabetically:</Text>
+        { tasksByPriorityDescendingAndName.map((task) => <Text>{task.name}</Text>)}
 
-    <Text>Tasks sorted by assignee name:</Text>
-    { tasksByAssigneeName.map((task) => <Text key={"tasks-by-assignee-name-" + task._id}>{task.name}</Text>)}
-
-    </>)
+        <Text>Tasks sorted by assignee name:</Text>
+        { tasksByAssigneeName.map((task) => <Text>{task.name}</Text>)}
+    </>
+    )
 }
