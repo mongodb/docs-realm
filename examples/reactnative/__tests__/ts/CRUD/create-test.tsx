b2b2c89c71252ebb39ebb43ddd307f69a2b320f3
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2022-12-08 19:07:27 -0500

    added js tests + add comments

--- examples/reactnative/__tests__/ts/CRUD/create-test.tsx
+++ examples/reactnative/__tests__/ts/CRUD/create-test.tsx
@@ -4,6 +4,8 @@ import { render, fireEvent, waitFor, act } from "@testing-library/react-native";
 import Realm from "realm";
 import { createRealmContext } from '@realm/react'
 
+
+// TODO: Replace `static schema` with TS-first models + realm-babel-plugin (https://www.npmjs.com/package/@realm/babel-plugin) approach once realm-babel-plugin version 0.1.2 releases with bug fixes
 class Dog extends Realm.Object {
     static schema = {
         name: "Dog",
