4b82ba5a2ef28355b271cf68e03e1045f9d6dacb
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2022-12-13 15:05:34 -0500

    (DOCSP-26946): React Native Unit Test Suite (#2379)
    ## Pull Request Info
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-26946
    
    ### Staged Changes
    
    N/A
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [N/A] Create Jira ticket for corresponding docs-app-services
    update(s), if any
    - [N/A] Checked/updated Admin API
    - [N/A] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)
    
    Co-authored-by: Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>

---
+++ examples/react-native/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  quoteProps: 'preserve',
+  jsxSingleQuote: true, // do not remove this line, otherwise the bluehawk replacement of testIDs won't work
+};
