fcafecb000310807555a34cc2b72e3a457b83ea7
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-12-19 14:02:23 -0500

    android <= 7

--- examples/dart/test/app_services_test.dart
+++ examples/dart/test/app_services_test.dart
@@ -1,5 +1,7 @@
 import 'package:test/test.dart';
 import 'package:realm_dart/realm.dart';
+import "dart:io";
+import "dart:convert";
 
 void main() {
   const APP_ID = "example-testers-kvjdy";
@@ -23,5 +25,42 @@ void main() {
       expect(app.currentUser, null);
       expect(appConfig.localAppVersion, '2.0');
     });
+
+    test("Custom SSL Certificate", () async {
+      // :snippet-start: custom-ssl-cert
+      // :uncomment-start:
+      // import 'package:realm_dart/realm.dart';
+      // import "dart:io";
+      // import "dart:convert";
+      // :uncomment-end:
+
+      HttpClient createCustomHttpsClient(String cert) {
+        SecurityContext context = SecurityContext.defaultContext;
+        try {
+          final bytes = utf8.encode(cert);
+          context.setTrustedCertificatesBytes(bytes);
+        } on TlsException catch (e) {
+          final message = e.osError?.message ?? "";
+          if (!message.contains('CERT_ALREADY_IN_HASH_TABLE')) {
+            rethrow;
+          }
+        }
+
+        return HttpClient(context: context);
+      }
+
+      App createAppWithCustomHttpsClient(
+          String letsEncryptCertificate, String appId) {
+        HttpClient httpClient = createCustomHttpsClient(letsEncryptCertificate);
+        final appConfig = AppConfiguration(appId, httpClient: httpClient);
+        return App(appConfig);
+      }
+      // :snippet-end:
+    },
+        skip:
+            """Skipping because this is quite challenging to test in our unit test suite
+    because our unit tests use a version of Dart that doesn't require a custom SSL certificate.
+
+    For more information on this, refer to https://stackoverflow.com/questions/69511057/flutter-on-android-7-certificate-verify-failed-with-letsencrypt-ssl-cert-after-s""");
   });
 }
