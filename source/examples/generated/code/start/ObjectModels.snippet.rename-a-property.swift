a7c97b39ed59398b4476ff0bdf44ba625ab8f7db
Dachary <dachary.carey@mongodb.com>
2022-12-21 11:04:08 -0500

    (DOCSP-26827): Update SwiftLint & fix a couple of linting errors (#2429)
    ## Pull Request Info
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-26827
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [x] Create Jira ticket for corresponding docs-app-services update(s),
    if any
    - [x] Checked/updated Admin API
    - [x] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

--- source/examples/generated/code/start/ObjectModels.snippet.rename-a-property.swift
+++ source/examples/generated/code/start/ObjectModels.snippet.rename-a-property.swift
@@ -2,7 +2,7 @@ class Person: Object {
     @Persisted var firstName = ""
     @Persisted var lastName = ""
     
-    override class public func propertiesMapping() -> [String : String] {
+    override class public func propertiesMapping() -> [String: String] {
         ["firstName": "first_name",
          "lastName": "last_name"]
     }
