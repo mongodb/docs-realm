9690c99d94fea2dab169379ac0dfeedf26e989e3
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-12-21 10:02:52 -0500

    (DOCSP-26512): Info about custom HTTP client for Android <= 7 (#2424)
    ## Pull Request Info
    
    Add documentation section about custom HTTP client for Android <= 7.
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-26512
    
    ### Staged Changes
    
    - [Connect to App Services > Connect Using Android 7 or Older
    section](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-26512/sdk/flutter/app-services/connect-to-app/)
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [x] Create Jira ticket for corresponding docs-app-services update(s),
    if any
    - [x] Checked/updated Admin API
    - [x] Checked/updated CLI reference
    
    ### Review Guidelines
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

---
+++ source/examples/generated/flutter/app_services_test.snippet.custom-ssl-cert.dart
@@ -0,0 +1,30 @@
+import 'package:realm_dart/realm.dart';
+import "dart:io";
+import "dart:convert";
+
+HttpClient createCustomHttpsClient(String cert) {
+  SecurityContext context = SecurityContext.defaultContext;
+  try {
+    final bytes = utf8.encode(cert);
+    context.setTrustedCertificatesBytes(bytes);
+  } on TlsException catch (e) {
+    final message = e.osError?.message ?? "";
+    if (!message.contains('CERT_ALREADY_IN_HASH_TABLE')) {
+      rethrow;
+    }
+  }
+
+  return HttpClient(context: context);
+}
+
+App createAppWithCustomHttpsClient(
+    String letsEncryptCertificate, String appId) {
+  HttpClient httpClient = createCustomHttpsClient(letsEncryptCertificate);
+  final appConfig = AppConfiguration(appId, httpClient: httpClient);
+  return App(appConfig);
+}
+
+final letsEncryptCertificate = "<LET'S ENCRYPT CERTIFICATE>";
+final appId = "<YOUR APP ID>";
+
+final app = createAppWithCustomHttpsClient(letsEncryptCertificate, appId);
