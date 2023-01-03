124d3fcdb809658eef164a7fdc7cffa053d2f8e6
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2023-01-03 17:12:27 -0500

    (DOCSP-26982): @realm/reactify: Dictionary - React Native SDK (#2401)
    ## Pull Request Info
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-26982
    
    ### Staged Changes
    
    - [Dictionaries - React Native
    SDK](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/docsp-26982-dictionaries/sdk/react-native/realm-database/schemas/dictionaries/)
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [N/A] Create Jira ticket for corresponding docs-app-services
    update(s), if any
    - [N/A] Checked/updated Admin API
    - [N/A] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)
    
    Co-authored-by: Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>

---
+++ examples/react-native/__tests__/ts/Models/HomeOwner.ts
@@ -0,0 +1,25 @@
+import Realm from 'realm';
+
+// TODO: Replace `static schema` with TS-first models + realm-babel-plugin (https://www.npmjs.com/package/@realm/babel-plugin) approach once realm-babel-plugin version 0.1.2 releases with bug fixes
+// :snippet-start: ts-homeowner-schema
+interface Home extends Realm.Dictionary {
+  address?: string;
+  color?: string;
+  price?: number;
+  yearRenovated?: number;
+}
+
+class HomeOwner extends Realm.Object<HomeOwner> {
+  name!: string;
+  home!: Home;
+
+  static schema = {
+    name: 'HomeOwner',
+    properties: {
+      name: 'string',
+      home: '{}',
+    },
+  };
+}
+// :snippet-end:
+export default HomeOwner;
---
+++ source/examples/generated/react-native/ts/dictionary-test.snippet.delete-members-of-a-dictionary.tsx
@@ -0,0 +1,19 @@
+const HomeInfo = ({homeOwnerName}: {homeOwnerName: string}) => {
+  const realm = useRealm();
+  const homeOwner = useQuery(HomeOwner).filtered(`name == '${homeOwnerName}'`)[0];
+
+  const deleteExtraHomeInfo = () => {
+    realm.write(() => {
+      // remove the 'yearRenovated' and 'color' field of the house
+      homeOwner.home.remove(['yearRenovated', 'color']);
+    });
+  };
+
+  return (
+    <View>
+      <Text>{homeOwner.name}</Text>
+      <Text>{homeOwner.home.address}</Text>
+      <Button onPress={deleteExtraHomeInfo} title='Delete extra home info' />
+    </View>
+  );
+};
