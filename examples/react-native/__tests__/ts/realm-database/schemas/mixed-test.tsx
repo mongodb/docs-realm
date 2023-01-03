5ee84bb76ca306f2aefedc71bca3c0eea901a7fe
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2023-01-03 16:48:49 -0500

    fix typo 'Dog' -> 'Cat'

--- examples/react-native/__tests__/ts/realm-database/schemas/mixed-test.tsx
+++ examples/react-native/__tests__/ts/realm-database/schemas/mixed-test.tsx
@@ -49,21 +49,21 @@ describe('Mixed Tests', () => {
       useEffect(() => {
         // Add data to the Realm when the component mounts
         realm.write(() => {
-          // create a Dog with a birthDate value of type string
+          // create a Cat with a birthDate value of type string
           new Cat(realm, {
             name: 'Euler',
             birthDate: 'December 25th, 2017',
           });
-          // create a Dog with a birthDate value of type date
+          // create a Cat with a birthDate value of type date
           new Cat(realm, {
             name: 'Blaise',
             birthDate: new Date('August 17, 2020'),
           });
 
-          // create a Dog with a birthDate value of type int
+          // create a Cat with a birthDate value of type int
           new Cat(realm, {name: 'Euclid', birthDate: 10152021});
 
-          // create a Dog with a birthDate value of type null
+          // create a Cat with a birthDate value of type null
           new Cat(realm, {name: 'Pythagoras', birthDate: null});
         });
       }, []);
