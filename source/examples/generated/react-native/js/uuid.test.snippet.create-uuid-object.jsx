45a555d6d63a15b0de3c3777c18d3079a9f4d9ab
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2023-01-04 12:36:17 -0500

    (DOCSP-26985): @realm/reactify: UUID - React Native SDK  (#2443)
    ## Pull Request Info
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-26985
    
    ### Staged Changes
    
    - [UUID - React Native
    SDK](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-26985-uuid-object/sdk/react-native/realm-database/schemas/uuid/)
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [N/A] Create Jira ticket for corresponding docs-app-services
    update(s), if any
    - [N/A] Checked/updated Admin API
    - [N/A] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)
    
    Co-authored-by: Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>

---
+++ source/examples/generated/react-native/js/uuid.test.snippet.create-uuid-object.jsx
@@ -0,0 +1,19 @@
+const CreateProfileInput = () => {
+  const realm = useRealm();
+  const [name, setName] = useState('');
+
+  // createProfile creates a new 'Profile' Realm Object with a new UUID based on user input
+  const createProfile = () => {
+    realm.write(() => {
+      new Profile(realm, {
+        name,
+        _id: new Realm.BSON.UUID(),
+      });
+    });
+  };
+  return (
+    <View>
+      <TextInput placeholder='Name' onChangeText={setName} />
+      <Button title='Create Profile' onPress={createProfile} />
+    </View>
+  );
