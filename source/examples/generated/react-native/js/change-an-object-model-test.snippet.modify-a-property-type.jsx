0a8fe70098b7193cfd0c7727aac3fcf2986f545e
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2023-01-04 13:58:02 -0500

    (DOCSP-26981): @realm/reactify - Change an Object Model - React Native (#2399)
    ## Pull Request Info
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-26981
    
    ### Staged Changes
    
    - [Change an Object Model - React
    Native](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-26981-Change-A-ROM/sdk/react-native/realm-database/change-an-object-model/)
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [N/A] Create Jira ticket for corresponding docs-app-services
    update(s), if any
    - [N/A] Checked/updated Admin API
    - [N/A] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)
    
    Co-authored-by: Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
    Co-authored-by: Ben Perlmutter <90647379+mongodben@users.noreply.github.com>

---
+++ source/examples/generated/react-native/js/change-an-object-model-test.snippet.modify-a-property-type.jsx
@@ -0,0 +1,37 @@
+class Task extends Realm.Object {
+  static schema = {
+    name: "Task",
+    properties: {
+      // update the data type of '_id' to be 'objectId' within the schema
+      _id: "objectId",
+      name: "string",
+      priority: "int?",
+      progressMinutes: "int?",
+      assignee: "Person?",
+    },
+    primaryKey: "_id",
+  };
+}
+
+const config = {
+  schema: [Task],
+  // increment the 'schemaVersion', since the property type of '_id'
+  // has been modified
+  schemaVersion: 2,
+  migration: (oldRealm, newRealm) => {
+    if (oldRealm.schemaVersion < 2) {
+      const oldObjects = oldRealm.objects(Task);
+      const newObjects = newRealm.objects(Task);
+      // loop through all objects and set the _id property
+      // in the new schema
+      for (const objectIndex in oldObjects) {
+        const oldObject = oldObjects[objectIndex];
+        const newObject = newObjects[objectIndex];
+        newObject._id = new Realm.BSON.ObjectId(oldObject._id);
+      }
+    }
+  },
+};
+// pass the configuration object with the updated
+// 'schemaVersion' and 'migration' function to createRealmContext()
+createRealmContext(config);
