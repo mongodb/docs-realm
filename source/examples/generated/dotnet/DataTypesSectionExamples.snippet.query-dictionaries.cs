6b26c85514b65a0f94e08d6b318eaee5caf64886
MongoCaleb <32645888+MongoCaleb@users.noreply.github.com>
2023-01-06 10:05:20 -0800

    Add info and code about querying dictionaries (#2463)
    ## Pull Request Info
    
    ### Jira
    
    https://jira.mongodb.org/browse/DOCSP-19341
    
    ### Staged Changes
    
    
    https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-19341/sdk/dotnet/model-data/data-types/dictionaries/#usage-example
    
    ### IMPORTANT
    We never manually edit files in the /generated/ directory. Code changes
    happen in the /examples/ directory only.
    
    Co-authored-by: Ferdinando Papale <4850119+papafe@users.noreply.github.com>

--- source/examples/generated/dotnet/DataTypesSectionExamples.snippet.query-dictionaries.cs
+++ source/examples/generated/dotnet/DataTypesSectionExamples.snippet.query-dictionaries.cs
@@ -3,36 +3,44 @@ var storeInventory = new Inventory()
     Id = ObjectId.GenerateNewId().ToString()
 };
 
-storeInventory.PlantDict.Add("Petunia", new Plant());
-storeInventory.NullableIntDict.Add("random things", 7);
-storeInventory.RequiredStringsDict.Add("foo", "bar");
+storeInventory.Plants.Add("Petunia", new Plant());
+storeInventory.NullableIntDictionary.Add("random things", 7);
+storeInventory.RequiredStringsDictionary.Add("foo", "bar");
 
 var storeInventory2 = new Inventory()
 {
     Id = ObjectId.GenerateNewId().ToString()
 };
 
-storeInventory2.RequiredStringsDict.Add("foo", "Bar");
+storeInventory2.RequiredStringsDictionary.Add("foo", "Bar");
 
 realm.Write(() =>
 {
-    realm.Add<Inventory>(storeInventory);
-    realm.Add<Inventory>(storeInventory2);
+    realm.Add(storeInventory);
+    realm.Add(storeInventory2);
 });
 
 // Find all Inventory items that have "Petunia"
-// as a key in their PlantDict.
+// as a key in their Plants dictionary.
 var petunias = realm.All<Inventory>()
-    .Filter("PlantDict.@keys == 'Petunia'");
+    .Filter("Plants.@keys == 'Petunia'");
 
 // Find all Inventory items that have at least one value in their
-// IntDict that is larger than 5
+// IntDictionary that is larger than 5 using RQL
 var matchesMoreThanFive = realm.All<Inventory>()
-    .Filter("NullableIntDict.@values > 5");
+    .Filter("NullableIntDictionary.@values > 5");
 
-// Find all Inventory items where any RequiredStringsDict has a key
+// Find all Inventory items where the RequiredStringsDictionary has a key
 // "Foo", and the value of that key contains the phrase "bar"
 // (case insensitive)
-var matches = realm.All<Inventory>().Filter("RequiredStringsDict['foo'] CONTAINS[c] 'bar'");
+var matches = realm.All<Inventory>()
+    .Filter("RequiredStringsDictionary['foo'] CONTAINS[c] 'bar'");
 // matches.Count() == 2
 
+// Query the Plants dictionary of an Inventory object
+// for a specific plant
+var myStoreInventory = realm
+    .All<Inventory>().FirstOrDefault();
+
+var petunia = myStoreInventory.Plants.AsRealmQueryable()
+    .Where(p => p.Name == "Petunia");
