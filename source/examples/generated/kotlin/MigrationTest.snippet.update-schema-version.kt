6cea2fed2f07bf810e97487e35e12efa41a62dc0
cbullinger <115956901+cbullinger@users.noreply.github.com>
2023-01-23 15:10:27 -0500

    Delete MigrationTest.snippet.update-schema-version.kt

--- source/examples/generated/kotlin/MigrationTest.snippet.update-schema-version.kt
+++
@@ -1,7 +0,0 @@
-// Use the configuration builder to open the realm using the newer schema version
-val config = RealmConfiguration.Builder(
-    schema = setOf(Person::class)
-)
-    .schemaVersion(2) // Set the new schema version
-    .build()
-val realm = Realm.open(config)
