a453e498a45242a5ad7637c4288192f2bc8180ce
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-01-26 15:06:22 -0500

    create code examples

---
+++ examples/react-native/__tests__/ts/app-services/use-app.test.tsx
@@ -0,0 +1,41 @@
+// :snippet-start: app-provider
+import React from 'react';
+import {useApp} from '@realm/react';
+import {Credentials} from 'realm';
+// :remove-start:
+import {App} from 'realm';
+import {AppProvider} from '@realm/react';
+import 'react-native';
+import {render, fireEvent, waitFor} from '@testing-library/react-native';
+import {View, Button} from 'react-native';
+const APP_ID = 'example-testers-kvjdy';
+function AppWrapper() {
+  return (
+    <View>
+      <AppProvider id={APP_ID}>
+        <MyApp />
+      </AppProvider>
+    </View>
+  );
+}
+// :remove-end:
+function MyApp() {
+  const app = useApp();
+  function logInAnonymousUser() {
+    app.logIn(Credentials.anonymous());
+  }
+  // ...
+  // :remove-start:
+  return <Button onPress={logInAnonymousUser} testID='test-use-app' title='Test Me!' />;
+  // :remove-end:
+}
+// :snippet-end:
+
+afterEach(() => App.getApp(APP_ID).currentUser?.logOut());
+
+test('useApp hook works correctly', async () => {
+  const {getByTestId} = render(<AppWrapper />);
+  const button = getByTestId('test-use-app');
+  fireEvent.press(button);
+  await waitFor(() => expect(App.getApp(APP_ID).currentUser).not.toBeNull());
+});
