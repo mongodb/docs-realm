44d613c377ab78546c48346af4b4bb96f025ef4a
Dachary Carey <dachary.carey@mongodb.com>
2023-01-27 11:49:00 -0500

    Create user authentication page, add anonymous user example

--- examples/cpp/authentication.cpp
+++ examples/cpp/authentication.cpp
@@ -16,11 +16,11 @@ auto random_string() {
     return std::to_string(random_number());
 }
 
-TEST_CASE("create and log in a user", "[realm][sync]") {
-    // :snippet-start: register-user
-    const std::string APP_ID = "cpp-tester-uliix";
+auto INSERT_APP_ID_HERE = "cpp-tester-uliix";
 
-    auto app = realm::App(APP_ID);
+TEST_CASE("create and log in an email/password user", "[realm][sync]") {
+    // :snippet-start: register-user
+    auto app = realm::App(INSERT_APP_ID_HERE);
 
     auto user_email = "testUser" + random_string() + "@example.com";
     auto user_password = "password1234";
@@ -32,8 +32,21 @@ TEST_CASE("create and log in a user", "[realm][sync]") {
     auto user = app.login(realm::App::credentials::username_password(user_email, user_password))
         .get_future().get();
     // :snippet-end:
+    REQUIRE(!user.access_token().empty());
 
     // :snippet-start: log-user-out
     user.log_out().get_future().get();
     // :snippet-end:
+    CHECK(user.state() == realm::user::state::logged_out);
+}
+
+TEST_CASE("create and log in an anonymous user", "[realm][sync]") {
+    // :snippet-start: anonymous-login
+    auto app = realm::App(INSERT_APP_ID_HERE);
+
+    auto user = app.login(realm::App::credentials::anonymous()).get_future().get();
+    // :snippet-end:
+    REQUIRE(!user.access_token().empty());
+    user.log_out().get_future().get();
+    CHECK(user.state() == realm::user::state::removed);
 }
