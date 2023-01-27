ab4a14e7bfd01e5e324b20fa1335b07b777bd2ee
krollins-mdb <kyle.rollins@mongodb.com>
2023-01-27 12:56:57 -0600

    Initial pass

---
+++ source/examples/generated/react-native/js/configure-realm-sync.test.snippet.configure-realm.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {AppProvider, UserProvider} from '@realm/react';
+
+import {RealmContext} from '../Models';
+function AppWrapperSync() {
+  const {RealmProvider} = RealmContext;
+
+  return (
+    <AppProvider id={APP_ID}>
+      <UserProvider>
+        <RealmProvider
+          sync={{flexible: true, onError: error => console.error(error)}}>
+          <MyApp />
+        </RealmProvider>
+      </UserProvider>
+    </AppProvider>
+  );
+}
