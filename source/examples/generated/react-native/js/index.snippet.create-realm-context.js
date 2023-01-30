53a252ee7870718c861c0b52c6c74fb1c9039c8e
krollins-mdb <kyle.rollins@mongodb.com>
2023-01-30 14:08:00 -0600

    Clean up from migration

--- source/examples/generated/react-native/js/index.snippet.create-realm-context.js
+++
@@ -1,9 +0,0 @@
-import {createRealmContext} from '@realm/react';
-// Import all of your models.
-import {Invoice} from './Invoice';
-import {Business} from './Business';
-
-export const RealmContext = createRealmContext({
-  // Pass all of your models into the schema value.
-  schema: [Invoice, Business],
-});
