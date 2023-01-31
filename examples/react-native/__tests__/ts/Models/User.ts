733eee35fed593feacf0a378035682671b52e1dc
Mohammad Hunan Chughtai <mohammad.hunan@mongodb.com>
2023-01-31 10:58:16 -0500

    (DOCSP-26992): @realm/react-ify "Relationships - React Native SDK" (#2473)

---
+++ examples/react-native/__tests__/ts/Models/User.ts
@@ -0,0 +1,24 @@
+import Realm from 'realm';
+import Post from './Post';
+// TODO: Replace `static schema` with TS-first models + realm-babel-plugin (https://www.npmjs.com/package/@realm/babel-plugin) approach once realm-babel-plugin version 0.1.2 releases with bug fixes
+// :snippet-start: ts-user-schema
+class User extends Realm.Object<User, "_id" | "name"> {
+  _id!: Realm.BSON.ObjectId;
+  name!: string;
+  birthdate?: Date;
+  posts!: Realm.List<Post>;
+
+  static schema = {
+    name: 'User',
+    properties: {
+      _id: 'objectId',
+      name: 'string',
+      birthdate: 'date?',
+      posts: 'Post[]',
+    },
+    primaryKey: '_id',
+  };
+}
+// :snippet-end:
+
+export default User;
