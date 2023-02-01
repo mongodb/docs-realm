21ec813fa0d66afb63fcce37bd8e17501228af26
Dachary <dachary.carey@mongodb.com>
2023-02-01 11:37:47 -0500

    (DOCSP-27630): C++: Add a Call a Function page (#2540)
    ## Pull Request Info
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-27630
    
    ### Staged Changes
    
    - [Call a
    Function](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-27630/sdk/cpp/app-services/call-a-function)
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [x] Create Jira ticket for corresponding docs-app-services update(s),
    if any
    - [x] Checked/updated Admin API
    - [x] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

--- examples/cpp/examples.cpp
+++ examples/cpp/examples.cpp
@@ -197,6 +197,32 @@ TEST_CASE("open a synced realm", "[realm][sync]") {
     // :snippet-end:
 }
 
+TEST_CASE("call a function", "[realm][sync]") {
+    // :snippet-start: call-a-function
+    // Connect to an App Services App and authenticate a user
+    auto app = realm::App(APP_ID);
+    auto user = app.login(realm::App::credentials::anonymous()).get_future().get();
+    auto sync_config = user.flexible_sync_configuration();
+
+    // If a function takes arguments, pass them as BSON
+    auto arg1 = realm::bson::Bson("john.smith");
+    auto arg2 = realm::bson::Bson("@companyemail.com");
+
+    // Call an App Services function as the logged-in user
+    auto result = user.call_function("concatenate", { arg1, arg2 }).get_future().get();
+    
+    // Verify that the result has a value
+    CHECK(result);
+    auto bsonResult = result.value();
+    
+    // Translate the BSON result back to a string
+    auto resultString = std::string(bsonResult);
+    // Prints "Calling the concatenate function returned john.smith@companyemail.com."
+    std::cout << "Calling the concatenate function returned " << resultString << ".\n";
+    // :snippet-end:
+    REQUIRE(resultString == "john.smith@companyemail.com");
+}
+
 #if 0
 // Can't currently get this to work.
 TEST_CASE("live objects", "[test]") {
