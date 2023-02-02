384942009044179e3be2e4943b332b4b88ffe3f8
Dachary <dachary.carey@mongodb.com>
2023-02-02 08:58:35 -0500

    (DOCSP-27632): C++: Add a Custom User Data page (#2546)
    ## Pull Request Info
    
    Now that we've got a few user-related pages, grouping them together in
    the ToC under a new "Manage Users" section. However, the directory
    structure for the pages hasn't changed so no redirects are needed.
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-27632
    
    ### Staged Changes
    
    - [Custom User
    Data](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-27632/sdk/cpp/users/custom-user-data/):
    New page
    - [Manage
    Users](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-27632/sdk/cpp/manage-users/):
    New ToC entry + relevant content from the corresponding Swift SDK page,
    minus the stuff that isn't implemented yet in the C++ SDK
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [x] Create Jira ticket for corresponding docs-app-services update(s),
    if any
    - [x] Checked/updated Admin API
    - [x] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

--- examples/cpp/examples.cpp
+++ examples/cpp/examples.cpp
@@ -223,6 +223,43 @@ TEST_CASE("call a function", "[realm][sync]") {
     REQUIRE(resultString == "john.smith@companyemail.com");
 }
 
+TEST_CASE("custom user data", "[realm][sync]") {
+    auto app = realm::App(APP_ID);
+
+    // :snippet-start: create-custom-user-data
+    auto user = app.login(realm::App::credentials::anonymous()).get_future().get();
+
+    // Functions take an argument of BsonArray, so initialize the custom data as a BsonDocument
+    auto customDataBson = realm::bson::BsonDocument({{"userId", user.identifier()}, {"favoriteColor", "gold"}});
+
+    // Call an Atlas Function to insert custom data for the user
+    auto result = user.call_function("updateCustomUserData", { customDataBson }).get_future().get();
+    // :snippet-end:
+    CHECK(result);
+
+    // :snippet-start: read-custom-user-data
+    // Custom user data could be stale, so refresh it before reading it
+    user.refresh_custom_user_data().get_future().get();
+    CHECK((*user.custom_data())["favoriteColor"] == "gold");
+    // :snippet-end:
+
+    // :snippet-start: update-custom-user-data
+    // Functions take an argument of BsonArray, so initialize the custom data as a BsonDocument
+    auto updatedDataBson = realm::bson::BsonDocument({{"userId", user.identifier()}, { "favoriteColor", "black" }});
+
+    // Call an Atlas Function to update custom data for the user
+    auto updateResult = user.call_function("updateCustomUserData", { updatedDataBson }).get_future().get();
+
+    // Refresh the custom user data before reading it to verify it succeeded
+    user.refresh_custom_user_data().get_future().get();
+    CHECK((*user.custom_data())["favoriteColor"] == "black");
+    // :snippet-end:
+    // :snippet-start: delete-custom-user-data
+    auto deleteResult = user.call_function("deleteCustomUserData", {}).get_future().get();
+    // :snippet-end:
+    CHECK(deleteResult);
+}
+
 #if 0
 // Can't currently get this to work.
 TEST_CASE("live objects", "[test]") {
