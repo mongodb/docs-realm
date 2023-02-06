af614b842538825c4a1a32cdf2f67f8244e59223
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-02-06 14:33:27 -0500

    (DOCSP-26118): updated bundle a realm page (#2528)
    ## Pull Request Info
    
    update the bundle a realm page to talk about synced realm.
    
    > **Note**
    >
    > The ticket specified to use Realm.writeCopy() for this, but based upon
    my understanding of that API and creating realms generally, I think it's
    actually more straightforward to just have the user create and copy a
    realm, as documented here.
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-26118
    
    ### Staged Changes
    
    - [Bundle a Realm
    (Flutter)](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-26118/sdk/flutter/realm-database/realm-files/bundle)
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [x] Create Jira ticket for corresponding docs-app-services update(s),
    if any
    - [x] Checked/updated Admin API
    - [x] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)
    
    ---------
    
    Co-authored-by: Dachary <dc@dacharycarey.com>

--- examples/dart/bin/myapp.dart
+++ examples/dart/bin/myapp.dart
@@ -1,7 +1,7 @@
 import 'models/car.dart';
 import 'package:realm_dart/realm.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   // :snippet-start: create-bundle
   print("Bundling realm");
   final config = Configuration.local([Car.schema], path: 'bundle.realm');
@@ -15,6 +15,51 @@ void main(List<String> arguments) {
   print("Bundled realm location: " + realm.config.path);
   realm.close();
   // :snippet-end:
+  Future<void> createSyncedBundle() async {
+    final APP_ID = 'flutter-flexible-luccm';
+    // :snippet-start: create-synced-bundle
+    print("Bundling synced realm");
+
+    // You must connect to the Device Sync server with an authenticated
+    // user to work with the synced realm.
+    final app = App(AppConfiguration(APP_ID));
+    // Check if current user exists and log anonymous user if not.
+    final user = app.currentUser ?? await app.logIn(Credentials.anonymous());
+
+    final config = Configuration.flexibleSync(user, [Car.schema]);
+    final realm = Realm(config);
+
+    // Add subscription that match the data being added
+    // and your app's backend permissions.
+    realm.subscriptions.update((mutableSubscriptions) {
+      mutableSubscriptions.add(realm.all<Car>());
+    });
+    await realm.subscriptions.waitForSynchronization();
+
+    // Add data to realm
+    realm.write(() => realm.deleteAll<Car>()); // :remove:
+    realm.write(() {
+      realm.add(Car(ObjectId(), "Audi", model: 'A8'));
+      realm.add(Car(ObjectId(), "Mercedes", model: 'G Wagon'));
+    });
+
+    // Sync changes with the server
+    await realm.syncSession.waitForUpload();
+    await realm.syncSession.waitForDownload();
+
+    // Create new configuration for the bundled realm.
+    // You must specify a path separate from the realm you
+    // are copying for Realm.writeCopy() to succeed.
+    final bundledConfig = Configuration.flexibleSync(user, [Car.schema],
+        path: 'sync_bundle.realm');
+    realm.writeCopy(bundledConfig);
+
+    print("Bundled realm location: " + bundledConfig.path);
+    realm.close();
+    // :snippet-end:
+  }
+
+  await createSyncedBundle();
 
   Realm.shutdown();
 }
