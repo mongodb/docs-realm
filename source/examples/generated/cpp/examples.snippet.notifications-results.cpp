8a1de9465d0a60cda338311ad25713808eda712f
Dachary <dachary.carey@mongodb.com>
2023-02-15 14:15:35 -0500

    (DOCSP-27936): C++: Add results observation to React to Changes page (#2593)
    ## Pull Request Info
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-27936
    
    ### Staged Changes
    
    - [React to
    Changes](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-27936/sdk/cpp/react-to-changes/#register-a-results-collection-change-listener):
    New "Register a Results Collection Change Listener" section
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [x] Create Jira ticket for corresponding docs-app-services update(s),
    if any
    - [x] Checked/updated Admin API
    - [x] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

---
+++ source/examples/generated/cpp/examples.snippet.notifications-results.cpp
@@ -0,0 +1,37 @@
+// Get a results collection to observe
+auto dogs = realm.objects<Dog>();
+//  Set up the listener & observe results notifications.
+auto token = dogs.observe([&](auto&& changes) {
+    try {
+        if (changes.collection_root_was_deleted) {
+            std::cout << "The collection was deleted.\n";
+        } else {
+            // Handle deletions, then insertions, then modifications.
+            for (auto& resultsChange : changes.deletions) {
+                std::cout << "The object at index " << std::to_string(resultsChange) << " was deleted\n";
+            }
+            for (auto& resultsChange : changes.insertions) {
+                std::cout << "The object at index " << std::to_string(resultsChange) << " was inserted\n";
+            }
+            for (auto& resultsChange : changes.modifications) {
+                std::cout << "The object at index " << std::to_string(resultsChange) << " was modified\n";
+            }
+        }
+    } catch (std::exception const& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+    }
+});
+
+// Delete and then add an object to see deletions and insertions.
+realm.write([&dog1, &dog2, &realm] {
+    realm.remove(dog1);
+    realm.add(dog2);
+});
+
+// Modify an object to see a modification.
+realm.write([&dog2, &realm] {
+    dog2.age = 2;
+});
+
+// Refresh the realm after the change to trigger the notification.
+realm.refresh();
