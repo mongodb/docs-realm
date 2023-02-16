a356fe4e04f90ed62a882c0915aa33de495e796a
Kyle Rollins <115574589+krollins-mdb@users.noreply.github.com>
2023-02-16 10:18:02 -0600

    Change "Model" directory to "model: (#2599)
    ## Pull Request Info
    
    This PR removes duplicate model directory that only has a letter case
    difference.
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

--- examples/react-native/__tests__/ts/realm-database/schemas/relationships-test.tsx
+++ examples/react-native/__tests__/ts/realm-database/schemas/relationships-test.tsx
@@ -3,8 +3,8 @@ import {View, Text} from 'react-native';
 import {render, waitFor} from '@testing-library/react-native';
 import Realm from 'realm';
 import {createRealmContext} from '@realm/react';
-import User from '../../Models/User';
-import Post from '../../Models/Post';
+import User from '../../models/User';
+import Post from '../../models/Post';
 
 const realmConfig = {
   schema: [User, Post],
