e110117314d0b67b4d7687965fdc6aa18b1cc14d
Chris Bush <chris.bush@mongodb.com>
2023-02-24 14:11:24 -0500

    Fix wonky name

--- examples/cpp/threading.cpp
+++ examples/cpp/threading.cpp
@@ -1,6 +1,6 @@
 // :replace-start: {
 //   "terms": {
-//     "ThreadingItem_": ""
+//     "ThreadingExample_": ""
 //   }
 // }
 #include <catch2/catch_test_macros.hpp>
@@ -33,15 +33,15 @@ TEST_CASE("thread safe reference", "[write]") {
 
     // :snippet-start: thread-safe-reference
     // Put an object instance into a thread safe reference
-    auto threadSafeThreadingExample_Item = realm::thread_safe_reference<ThreadingExample_Item>{item};
+    auto threadSafeItem = realm::thread_safe_reference<ThreadingExample_Item>{item};
     
     // Move the thread safe reference to a background thread
-    auto thread = std::thread([threadSafeThreadingExample_Item = std::move(threadSafeThreadingExample_Item)]() mutable {
+    auto thread = std::thread([threadSafeItem = std::move(threadSafeItem)]() mutable {
         // Open the realm again on the background thread
         auto realm = realm::open<ThreadingExample_Item>();
         
         // Resolve the ThreadingExample_Item instance via the thread safe reference
-        auto item = realm.resolve(std::move(threadSafeThreadingExample_Item));
+        auto item = realm.resolve(std::move(threadSafeItem));
 
         // ... use item ...
     });
--- source/examples/generated/cpp/threading.snippet.thread-safe-reference.cpp
+++ source/examples/generated/cpp/threading.snippet.thread-safe-reference.cpp
@@ -1,13 +1,13 @@
 // Put an object instance into a thread safe reference
-auto threadSafeThreadingExample_Item = realm::thread_safe_reference<ThreadingExample_Item>{item};
+auto threadSafeItem = realm::thread_safe_reference<Item>{item};
 
 // Move the thread safe reference to a background thread
-auto thread = std::thread([threadSafeThreadingExample_Item = std::move(threadSafeThreadingExample_Item)]() mutable {
+auto thread = std::thread([threadSafeItem = std::move(threadSafeItem)]() mutable {
     // Open the realm again on the background thread
-    auto realm = realm::open<ThreadingExample_Item>();
+    auto realm = realm::open<Item>();
     
-    // Resolve the ThreadingExample_Item instance via the thread safe reference
-    auto item = realm.resolve(std::move(threadSafeThreadingExample_Item));
+    // Resolve the Item instance via the thread safe reference
+    auto item = realm.resolve(std::move(threadSafeItem));
 
     // ... use item ...
 });
