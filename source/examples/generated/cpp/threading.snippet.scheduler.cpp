d65953064cea7e595b36d49f9028ff8450f0a5bd
Chris Bush <chris.bush@mongodb.com>
2023-02-27 15:24:12 -0500

    Gutted implementation

--- source/examples/generated/cpp/threading.snippet.scheduler.cpp
+++ source/examples/generated/cpp/threading.snippet.scheduler.cpp
@@ -1,51 +1,29 @@
-// Rudimentary scheduler example - NOT production ready
 struct MyScheduler : realm::scheduler {
-    MyScheduler(): _thread{[this] {
-        // Kick off the task processor thread and run until the scheduler
-        // goes out of scope.
-        while (!_done) {
-            // Get the next task in the queue if one is available
-            std::optional<std::function<void()>> maybe_task = _queue.try_pop();
-            if (maybe_task) {
-                // Execute the task
-                maybe_task.value()();
-            }
-            // Continue spinning the thread until we get a new task or
-            // request to terminate
-        }
-    }} {};
+    MyScheduler() {
+        // ... Kick off task processor thread(s) and run until the scheduler
+        // goes out of scope ...
+    }
 
     ~MyScheduler() override {
-        // Call in the processor thread and block until it returns.
-        _done = true;
-        _thread.join();
+        // ... Call in the processor thread(s) and block until return ...
     }
     
     void invoke(std::function<void()> &&task) override {
-        // Add the task to the lock-free queue.
-        _queue.push(std::move(task));
+        // ... Add the task to the (lock-free) processor queue ...
     }
 
     [[nodiscard]] bool is_on_thread() const noexcept override {
-        // Return true if the caller is on the same thread as the task thread.
-        return std::this_thread::get_id() == _thread.get_id();
+        // ... Return true if the caller is on the same thread as a processor thread ...
     }
 
     bool is_same_as(const realm::scheduler *other) const noexcept override {
-        // Compare scheduler instances.
-        return this == other;
+        // ... Compare scheduler instances ...
     }
 
     [[nodiscard]] bool can_invoke() const noexcept override {
-        // As long as this scheduler is in scope, it can invoke.
-        return !_done;
+        // ... Return true if the scheduler can accept tasks ...
     }
-
-private:
-    std::thread _thread;
-    std::atomic<bool> _done{false};
-    // Lock-free queue implementations can be found online.
-    LockFreeQueue<std::function<void()>> _queue;
+    // ...
 };
 
 int main() {
@@ -60,8 +38,9 @@ int main() {
     // Start the program main loop
     auto done = false;
     while (!done) {
-        // As long as scheduler is in scope, the processor thread
-        // is active and automatically processing events.
+        // This assumes the scheduler is implemented so that it
+        // continues processing tasks on background threads until
+        // the scheduler goes out of scope.
         
         // Handle input here
         // ...
