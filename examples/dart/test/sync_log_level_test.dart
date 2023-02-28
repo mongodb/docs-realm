17742c30d23a2bc87d8b8529acb362939fc98cac
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-02-28 13:33:58 -0500

    edits per eng feedback

--- examples/dart/test/sync_log_level_test.dart
+++ examples/dart/test/sync_log_level_test.dart
@@ -1,22 +1,24 @@
 import 'package:test/test.dart';
-// :replace-start: {
-//   "terms": {
-//     "realm_dart": "realm"
-//   }
-// }
-// :snippet-start: log-level
 import 'package:realm_dart/realm.dart';
-import 'package:logging/logging.dart';
+import './schemas.dart';
+import 'utils.dart';
 
-main() {
-  Realm.logger = Logger('Realm Error Logger')..level = RealmLogLevel.error;
-
-  // ...rest of app
-  // :remove-start:
+const APP_ID = "flutter-flexible-luccm";
+final SCHEMA_OBJECTS = [SyncSchema.schema];
+main() async {
   test("Add custom sync logger", () async {
-    expect(Realm.logger.name, 'Realm Error Logger');
+    // :snippet-start: log-level
+    // Must set log level before opening synced realm.
+    Realm.logger.level = RealmLogLevel.error;
+
+    // Initialize app and user before can open synced realm.
+    final app = App(AppConfiguration(APP_ID));
+    final user = await app.logIn(Credentials.anonymous());
+
+    // Synced realm writes logs according to log level set above.
+    final realm = Realm(Configuration.flexibleSync(user, SCHEMA_OBJECTS));
+    // :snippet-end:
+    expect(Realm.logger.level, RealmLogLevel.error);
+    cleanUpRealm(realm, app);
   });
-  // :remove-end:
 }
-// :snippet-end:
-// :replace-end:
--- source/examples/generated/flutter/sync_log_level_test.snippet.log-level.dart
+++ source/examples/generated/flutter/sync_log_level_test.snippet.log-level.dart
@@ -1,8 +1,9 @@
-import 'package:realm/realm.dart';
-import 'package:logging/logging.dart';
+// Must set log level before opening synced realm.
+Realm.logger.level = RealmLogLevel.error;
 
-main() {
-  Realm.logger = Logger('Realm Error Logger')..level = RealmLogLevel.error;
+// Initialize app and user before can open synced realm.
+final app = App(AppConfiguration(APP_ID));
+final user = await app.logIn(Credentials.anonymous());
 
-  // ...rest of app
-}
+// Synced realm writes logs according to log level set above.
+final realm = Realm(Configuration.flexibleSync(user, SCHEMA_OBJECTS));
