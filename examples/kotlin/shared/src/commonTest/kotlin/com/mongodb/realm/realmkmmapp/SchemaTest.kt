08ad038969a5ddf3a90c9f8dd51f684e88d6e09a
cbullinger <115956901+cbullinger@users.noreply.github.com>
2023-03-01 10:55:13 -0500

    Apply feedback

--- examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/SchemaTest.kt
+++ examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/SchemaTest.kt
@@ -98,8 +98,8 @@ class Post: RealmObject {
 class Horse : RealmObject {
     val _id: ObjectId = ObjectId()
     val name: String =""
-    @PersistedName("rider_name")
-    val riderName: Knight? = null
+    @PersistedName("rider_name") // Name persisted to realm
+    val riderName: Knight? = null // Kotlin name used in code
 }
 // :snippet-end:
 
--- source/examples/generated/kotlin/SchemaTest.snippet.persisted-name.kt
+++ source/examples/generated/kotlin/SchemaTest.snippet.persisted-name.kt
@@ -1,6 +1,6 @@
 class Horse : RealmObject {
     val _id: ObjectId = ObjectId()
     val name: String =""
-    @PersistedName("rider_name")
-    val riderName: Knight? = null
+    @PersistedName("rider_name") // Name persisted to realm
+    val riderName: Knight? = null // Kotlin name used in code
 }
