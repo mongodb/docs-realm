abab8f4e9947c07ed2211fa76384a653552efab4
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-03-20 14:15:52 -0400

    misc fixes

--- source/examples/generated/web/graphql.snippet.graphql-client.js
+++ source/examples/generated/web/graphql.snippet.graphql-client.js
@@ -17,7 +17,7 @@ function GraphQLProvider({ children }) {
       uri: process.env.NEXT_PUBLIC_GRAPHQL_API_ENDPOINT,
       // We get the latest access token on each request
       fetch: async (uri, options) => {
-        const accessToken = app?.currentUser?.accessToken;
+        const accessToken = app.currentUser?.accessToken;
         options.headers.Authorization = `Bearer ${accessToken}`;
         return fetch(uri, options);
       },
