b55fd024b23f1a24e6803cbab741544f47b58809
Kyle Rollins <115574589+krollins-mdb@users.noreply.github.com>
2023-03-22 15:13:22 -0500

    (DOCSP-26636) @realm/react-ify the Quick Start page (#2617)
    ## Pull Request Info
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-26636
    
    ### Staged Changes
    
    - [Quick
    Start](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-26636/sdk/react-native/quick-start/)
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [x] Create Jira ticket for corresponding docs-app-services update(s),
    if any
    - [x] Checked/updated Admin API
    - [x] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)
    
    ---------
    
    Co-authored-by: Ben Perlmutter <90647379+mongodben@users.noreply.github.com>

---
+++ source/examples/generated/react-native/ts/quickstart-sync.test.snippet.quickstart-setup.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Realm from 'realm';
+import {AppProvider, UserProvider, createRealmContext} from '@realm/react';
+
+// Define your object model
+class Profile extends Realm.Object<Profile> {
+  _id!: Realm.BSON.ObjectId;
+  name!: string;
+
+  static schema = {
+    name: 'Profile',
+    properties: {
+      _id: 'objectId',
+      name: 'string',
+    },
+    primaryKey: '_id',
+  };
+}
+
+// Create a configuration object
+const realmConfig: Realm.Configuration = {
+  schema: [Profile],
+};
+
+// Create a realm context
+const {RealmProvider, useRealm, useObject, useQuery} =
+  createRealmContext(realmConfig);
+
+// Expose a sync realm
+function AppWrapperSync() {
+  return (
+    <AppProvider id={APP_ID}>
+      <UserProvider fallback={LogIn}>
+        <RealmProvider
+          sync={{
+            flexible: true,
+            onError: console.error,
+            initialSubscriptions: {
+              update(subs, realm) {
+                subs.add(realm.objects('Profile'));
+              },
+            },
+          }}>
+          <RestOfApp />
+        </RealmProvider>
+      </UserProvider>
+    </AppProvider>
+  );
+}
