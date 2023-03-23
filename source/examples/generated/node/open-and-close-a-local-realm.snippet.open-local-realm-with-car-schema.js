f5616288c6161a673f07ac89ded53c495deb00cf
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2023-03-23 16:53:05 -0400

    remove unused snippets

--- source/examples/generated/node/open-and-close-a-local-realm.snippet.open-local-realm-with-car-schema.js
+++
@@ -1,9 +0,0 @@
-// Open a local realm file with a particular path & predefined Car schema
-try {
-  const realm = await Realm.open({
-    path: "myrealm",
-    schema: [Car],
-  });
-} catch (err) {
-  console.error("Failed to open the realm", err.message);
-}
