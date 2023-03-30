1c92849e9e4be784ec544fca555f081c560783e4
Caleb Thompson <caleb.thompson@mongodb.com>
2023-03-30 12:32:49 -0700

    update script

--- tools/findFilesThatContainImages.sh
+++ tools/findFilesThatContainImages.sh
@@ -1,26 +1,26 @@
-doit(){
-total_count="$(wc -l < "./imgs.txt")";    
-current_count=1;
-spin='←↖↑↗→↘↓↙'
-i=0;
-tput civis
-while read F; do
-    echo "Checking" $F "("$current_count "of" $total_count "image files)";
-    for f in $(find ~/Projects/mongocaleb/docs-realm/source -type f); do 
-    {
-        #echo $f
-        if n=$(grep -q $F $f); then
-            echo "→→→ "Image found in $f;
-            printf $F,$f"\n" >> results.csv;
-        fi
-    }
-    i=$(( (i+1) %8 ));
-    printf "\r${spin:$i:1}"
-    done
-    printf "\r✔\n"
-    ((current_count = current_count+1));
-done <./imgs.txt
+#!/bin/bash
+findFiles(){
+    total_count=$(find "`dirname $0`"/../source/images -type f | wc -l | tr -d ' ')
+    current_count=1;
+    spin='←↖↑↗→↘↓↙'
+    i=0;
+    while read F; do
+        echo "Checking $F ($current_count of $total_count image files)" 1>&2;
+
+        for f in $(find "`dirname $0`"/../source -type f); do 
+        {
+            if n=$(grep -q $F $f); then
+                result=`cut -d '/' -f5- <<< "$f"`;
+                echo "→→→ "Image found in $result 1>&2;
+                printf $F,$result"\n" 1>&1;
+            fi
+        }
+        i=$(( (i+1) %8 ));
+        printf "\r${spin:$i:1}" 1>&2;
+        done
+        printf "\r✔\n" 1>&2;
+        ((current_count = current_count+1));
+    done < <(find "`dirname $0`"/../source/images -type f -exec basename {} \;)
 }
-doit
-echo "done."
-tput cnorm
+findFiles
+echo "--done--"
