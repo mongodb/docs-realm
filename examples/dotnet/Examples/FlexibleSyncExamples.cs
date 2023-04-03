a86e63e09a3ccc52ce09c03dec6bdc2dfa65af36
Caleb Thompson <caleb.thompson@mongodb.com>
2023-04-03 15:36:36 -0700

    hopefully fixing borked unit test

--- examples/dotnet/Examples/FlexibleSyncExamples.cs
+++ examples/dotnet/Examples/FlexibleSyncExamples.cs
@@ -117,10 +117,10 @@ namespace Examples
 
             // These operations will throw an exception
             // on timeout or other transient sync session errors.
-            var realm = await Realm.GetInstanceAsync(config);
-
-            var session = realm.SyncSession;
             // :uncomment-start:
+            //var realm = await Realm.GetInstanceAsync(config);
+
+            //var session = realm.SyncSession;
             //await session.WaitForUploadAsync();
             //await session.WaitForDownloadAsync();
             // :uncomment-end:
