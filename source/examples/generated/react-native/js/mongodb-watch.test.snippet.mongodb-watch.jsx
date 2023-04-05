e8556206cded49c03dbea1549348311a965c32b8
krollins-mdb <kyle.rollins@mongodb.com>
2023-04-05 13:00:02 -0500

    First pass

---
+++ source/examples/generated/react-native/js/mongodb-watch.test.snippet.mongodb-watch.jsx
@@ -0,0 +1,70 @@
+import React, {useEffect} from 'react';
+import Realm from 'realm';
+import {useUser, useApp, AppProvider, UserProvider} from '@realm/react';
+
+function AppWrapper() {
+  return (
+    <AppProvider id={APP_ID}>
+      <UserProvider fallback={<LogIn />}>
+        <NotificationSetter />
+      </UserProvider>
+    </AppProvider>
+  );
+}
+
+function NotificationSetter() {
+  // Get currently logged in user
+  const user = useUser();
+
+  useEffect(() => {
+    const plants =
+      user.mongoClient('mongodb-atlas').db('example').collection <
+      Plant >
+      'plants';
+
+    // Set up notifications
+    watchForAllChanges(plants);
+  }, [user, watchForAllChanges]);
+
+  const watchForAllChanges = async plants => {
+    // Watch for changes to the plants collection
+    for await (const change of plants.watch()) {
+      switch (change.operationType) {
+        case 'insert': {
+          const {documentKey, fullDocument} = change;
+          console.log(
+            `new document: ${JSON.stringify(documentKey, null, 2)}`,
+            fullDocument,
+          );
+          break;
+        }
+        case 'update': {
+          const {documentKey, fullDocument} = change;
+          console.log(
+            `updated document: $${JSON.stringify(documentKey, null, 2)}`,
+            fullDocument,
+          );
+          break;
+        }
+        case 'replace': {
+          const {documentKey, fullDocument} = change;
+          console.log(
+            `replaced document: ${JSON.stringify(documentKey, null, 2)}`,
+            fullDocument,
+          );
+          break;
+        }
+        case 'delete': {
+          const {documentKey} = change;
+          console.log(
+            `deleted document: ${JSON.stringify(documentKey, null, 2)}`,
+          );
+          break;
+        }
+      }
+    }
+  };
+
+  // ... rest of component
+
+}
