feb139702180a5b820cd134ea02098a1755cdaa1
lindseymoore <lindsey.moore@mongodb.com>
2023-04-25 16:49:04 -0400

    print file size

--- examples/node/Examples/compacting.js
+++ examples/node/Examples/compacting.js
@@ -17,14 +17,13 @@ describe.skip("test compaction on launch", () => {
 
       // Compact if the file is over 100MB in size and less than 50% 'used'
       const oneHundredMB = 100 * 1024 * 1024;
+
       return totalBytes > oneHundredMB && usedBytes / totalBytes < 0.5;
     };
 
     const config = { shouldCompact };
 
     let realm = await Realm.open(config);
-
-    console.debug("bytes " + realm.usedBytes);
 
     //:snippet-end:
     expect(wasCalled).toBe(true);
@@ -52,64 +51,19 @@ describe.skip("test compaction on launch", () => {
 
       // Compact if the file is over 10KB in size and less than 50% 'used'
       const tenKB = 10 * 1024;
+      console.debug("used " + usedBytes);
+      console.debug("total " + totalBytes);
+      console.debug(totalBytes > tenKB && usedBytes / totalBytes < 0.5);
       return totalBytes > tenKB && usedBytes / totalBytes < 0.5;
     };
 
     const config = {
       path: "realm-files/compacting-realm",
       schema: [RockSchema],
+      shouldCompact,
     };
 
     let testRealm = await Realm.open(config);
-    let size = testRealm.totalBytes;
-    console.log("size", size);
-
-    //expect(wasCalled).toBe(true);
-
-    testRealm.write(() => {
-      testRealm.create("Rock", {
-        name: "Topaz",
-        luster: 10,
-      });
-      testRealm.create("Rock", {
-        name: "Sapphire",
-        luster: 7,
-      });
-      testRealm.create("Rock", {
-        name: "Ruby",
-        luster: 6,
-      });
-      testRealm.create("Rock", {
-        name: "Opal",
-        luster: 9,
-      });
-      testRealm.create("Rock", {
-        name: "Jade",
-        luster: 10,
-      });
-      testRealm.create("Rock", {
-        name: "Pearl",
-        luster: 10,
-      });
-      testRealm.create("Rock", {
-        name: "Granite",
-        luster: 10,
-      });
-      testRealm.create("Rock", {
-        name: "Tourmaline",
-        luster: 5,
-      });
-      testRealm.create("Rock", {
-        name: "Emerald",
-        luster: 5,
-      });
-      for (let i = 0; i < 1; i++) {
-        testRealm.create("Rock", {
-          name: "Gemq " + i.toString(),
-          luster: i,
-        });
-      }
-    });
 
     testRealm.close();
   });
