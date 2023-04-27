c03185394b62244c208cf174e41efb563da1a943
krollins-mdb <kyle.rollins@mongodb.com>
2023-04-27 15:28:00 -0500

    Update realm version and first test refactor

---
+++ examples/node/Examples/asymmetric-sync.ts
@@ -0,0 +1,107 @@
+import Realm, { BSON } from "realm";
+
+const app = new Realm.App({ id: "js-flexible-oseso" });
+
+// :snippet-start: asymmetric-sync
+class WeatherSensor extends Realm.Object<WeatherSensor> {
+  _id!: Realm.BSON.ObjectId;
+  deviceId!: string;
+  temperatureInFahrenheit!: number;
+  barometricPressureInHg!: number;
+  windSpeedInMph!: number;
+
+  static schema = {
+    name: "WeatherSensor",
+    // sync WeatherSensor objects one way from your device
+    // to your Atlas database.
+    asymmetric: true,
+    primaryKey: "_id",
+    properties: {
+      _id: "objectId",
+      deviceId: "string",
+      temperatureInFahrenheit: "int",
+      barometricPressureInHg: "float",
+      windSpeedInMph: "float",
+    },
+  };
+}
+// :snippet-end:
+
+describe("Asymmetric Sync", () => {
+  beforeAll(async () => {
+    const credentials = Realm.Credentials.anonymous();
+    await app.logIn(credentials);
+  });
+
+  afterAll(async () => {
+    app.currentUser?.logOut;
+  });
+
+  test("create asymmetric object", async () => {
+    console.log("START [create asymmetric object]");
+
+    if (!app.currentUser) {
+      return;
+    }
+
+    const realm = await Realm.open({
+      schema: [WeatherSensor],
+      sync: {
+        user: app.currentUser,
+        flexible: true,
+      },
+    });
+
+    // :snippet-start: write-asymmetric-object
+    realm.write(() => {
+      realm.create(WeatherSensor, {
+        _id: new BSON.ObjectID(),
+        deviceId: "WX1278UIT",
+        temperatureInFahrenheit: 66.7,
+        barometricPressureInHg: 29.65,
+        windSpeedInMph: 2,
+      });
+    });
+    // :snippet-end:
+
+    async function getAsymmetricObjects(): Promise<
+      Realm.Services.MongoDB.MongoDBCollection<WeatherSensor>
+    > {
+      return new Promise((resolve) => {
+        // Wait for asymmetric object to sync, then
+        // use mongo client to verify it was created.
+        setTimeout(() => {
+          const mongodb = app.currentUser!.mongoClient("mongodb-atlas");
+          const asyncWeatherSensors = mongodb
+            .db("js-flexible-oseso")
+            .collection<WeatherSensor>("WeatherSensor");
+
+          resolve(asyncWeatherSensors);
+        }, 400);
+      });
+    }
+    console.debug(performance.now(), "get collection");
+    const weatherSensorCollection = await getAsymmetricObjects();
+    console.debug(performance.now(), "collection got");
+    let weatherSensor;
+
+    try {
+      weatherSensor = await weatherSensorCollection.findOne({
+        deviceId: "WX1278UIT",
+      });
+    } catch (error) {
+      console.debug(error);
+    }
+
+    console.debug(performance.now(), weatherSensor);
+
+    expect(weatherSensor?.deviceId).toBe("WX1278UIT");
+
+    // Delete the asymmetric object.
+    await weatherSensorCollection.deleteMany({
+      deviceId: "WX1278UIT",
+    });
+
+    realm.close();
+  });
+});
