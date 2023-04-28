dd7a14beded83ebcc94726e0fc7074c9ab43d56b
lindseymoore <lindsey.moore@mongodb.com>
2023-04-28 17:08:58 -0400

    update code snippet to fix build error

--- examples/node/Examples/compacting.js
+++ examples/node/Examples/compacting.js
@@ -20,52 +20,22 @@ describe.skip("test compaction on launch", () => {
 
       return totalBytes > oneHundredMB && usedBytes / totalBytes < 0.5;
     };
+    //:uncomment-start:
+    // const config = { shouldCompact };
+    //:uncomment-end:
 
-    const config = { shouldCompact };
-
+    //:remove-start:
+    const config = {
+      path: "realm-files/compacting-realm",
+      shouldCompact,
+    };
+    //:remove-end:
     let realm = await Realm.open(config);
 
     //:snippet-end:
     expect(wasCalled).toBe(true);
 
     realm.close();
-  });
-
-  test("shouldCompact works as it should", async () => {
-    let wasCalled;
-
-    // TODO: Test that realm is compacted once it meets criteria
-
-    const RockSchema = {
-      name: "Rock",
-      properties: {
-        name: "string",
-        luster: "int",
-      },
-    };
-
-    const shouldCompact = (totalBytes, usedBytes) => {
-      // totalBytes refers to the size of the file on disk in bytes (data + free space)
-      // usedBytes refers to the number of bytes used by data in the file
-      wasCalled = true;
-
-      // Compact if the file is over 10KB in size and less than 50% 'used'
-      const tenKB = 10 * 1024;
-      console.debug("used " + usedBytes);
-      console.debug("total " + totalBytes);
-      console.debug(totalBytes > tenKB && usedBytes / totalBytes < 0.5);
-      return totalBytes > tenKB && usedBytes / totalBytes < 0.5;
-    };
-
-    const config = {
-      path: "realm-files/compacting-realm",
-      schema: [RockSchema],
-      shouldCompact,
-    };
-
-    let testRealm = await Realm.open(config);
-
-    testRealm.close();
   });
 
   test("compact", async () => {
