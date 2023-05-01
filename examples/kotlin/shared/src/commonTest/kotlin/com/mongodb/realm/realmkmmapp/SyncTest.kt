5ccd7ba61a5e4221dc894845b2a248bf01a8da20
cbullinger <115956901+cbullinger@users.noreply.github.com>
2023-05-01 08:09:32 -0400

    Remove manual reset code example

--- examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/SyncTest.kt
+++ examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/SyncTest.kt
@@ -542,50 +542,47 @@ class SyncTest: RealmTest() {
             realm.close()
         }
     }
-    @Test
-    fun manualClientFallbackTest() {
-        val credentials = Credentials.anonymous()
-        val app = App.create(yourFlexAppId)
-
-        runBlocking {
-            val user = app.login(credentials)
-            // :snippet-start: manual-recovery
-            val config = SyncConfiguration.Builder(user, setOf(Toad::class))
-                .syncClientResetStrategy(object : RecoverUnsyncedChangesStrategy {
-                    override fun onBeforeReset(realm: TypedRealm) {
-                        Log.i("Beginning client reset for " + realm.configuration.path)
-                    }
-                    override fun onAfterReset(before: TypedRealm, after: MutableRealm) {
-                        Log.i("Finished client reset for " + before.configuration.path)
-                    }
-                    override fun onManualResetFallback(session: SyncSession, exception: ClientResetRequiredException) {
-                        Log.i(
-                            "Couldn't handle the client reset automatically." +
-                                    " Falling back to manual client reset execution: "
-                                    + exception.message
-                        )
-                        // Clemente: how do you close the realm before executing the reset in kotlin sdk??
-                        // realm.close()
-                        try {
-                            Log.w("About to execute the client reset.")
-                            // execute the client reset, moving the current realm to a backup file
-                            exception.executeClientReset()
-                            Log.w("Executed the client reset.")
-                        } catch (exception: IllegalStateException) {
-                            Log.e("Failed to execute the client reset: " + exception.message)
-                        }
-                            // ... resetDialog
-
-                        // ... open realm
-                    }
-                })
-                .build()
-            val realm = Realm.open(config) // :remove:
-            // Close the realm before attempting the client reset
-            realm.close()
-            // :snippet-end:
-        }
-    }
-
+//    @Test
+//    fun manualClientFallbackTest() {
+//        val credentials = Credentials.anonymous()
+//        val app = App.create(yourFlexAppId)
+//
+//        runBlocking {
+//            val user = app.login(credentials)
+//            val config = SyncConfiguration.Builder(user, setOf(Toad::class))
+//                .syncClientResetStrategy(object : RecoverUnsyncedChangesStrategy {
+//                    override fun onBeforeReset(realm: TypedRealm) {
+//                        Log.i("Beginning client reset for " + realm.configuration.path)
+//                    }
+//                    override fun onAfterReset(before: TypedRealm, after: MutableRealm) {
+//                        Log.i("Finished client reset for " + before.configuration.path)
+//                    }
+//                    override fun onManualResetFallback(session: SyncSession, exception: ClientResetRequiredException) {
+//                        Log.i(
+//                            "Couldn't handle the client reset automatically." +
+//                                    " Falling back to manual client reset execution: "
+//                                    + exception.message
+//                        )
+//                        // Clemente: how do you close the realm before executing the reset in kotlin sdk??
+//                        // realm.close()
+//                        try {
+//                            Log.w("About to execute the client reset.")
+//                            // execute the client reset, moving the current realm to a backup file
+//                            exception.executeClientReset()
+//                            Log.w("Executed the client reset.")
+//                        } catch (exception: IllegalStateException) {
+//                            Log.e("Failed to execute the client reset: " + exception.message)
+//                        }
+//                            // ... resetDialog
+//
+//                        // ... open realm
+//                    }
+//                })
+//                .build()
+//            val realm = Realm.open(config) // :remove:
+//            // Close the realm before attempting the client reset
+//            realm.close()
+//        }
+//    }
 }
 // :replace-end:
