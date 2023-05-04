f73182c92ca7877ba4df97f56df8c50ab8f24db2
Dachary <dachary.carey@mongodb.com>
2023-05-04 11:05:07 -0400

    (DOCSP-29615): Swift Log Level Docs - Incorporate Tech Review Feedback (#2776)
    ## Pull Request Info
    
    This is a fast follow to #2773 to incorporate Diana's technical review
    feedback.
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-29615
    
    ### Staged Changes
    
    - [Set the Client Log
    Level](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/swift-log-level-feedback/sdk/swift/sync/log-level/)
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [x] Create Jira ticket for corresponding docs-app-services update(s),
    if any
    - [x] Checked/updated Admin API
    - [x] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

--- source/examples/generated/code/start/Sync.snippet.set-default-logger.swift
+++ source/examples/generated/code/start/Sync.snippet.set-default-logger.swift
@@ -1,9 +1,8 @@
 // Access your app
 let app = App(id: YOUR_APP_SERVICES_APP_ID)
 
-var logs: String = ""
 let logger = Logger(level: .info) { level, message in
-    logs += "\(Date.now) \(level) \(message) \n"
+    "REALM DEBUG: \(Date.now) \(level) \(message) \n"
 }
 
 // Set a logger as the default
