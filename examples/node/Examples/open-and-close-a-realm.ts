3c7a5049b30a822424f61dabf2035c3202591ad8
krollins-mdb <kyle.rollins@mongodb.com>
2023-05-09 15:44:25 -0500

    Add React Native content and test

--- examples/node/Examples/open-and-close-a-realm.ts
+++ examples/node/Examples/open-and-close-a-realm.ts
@@ -16,13 +16,13 @@ describe("Open realm at different paths", () => {
   };
 
   test("should open a realm at an absolute path", async () => {
-    const absolutePath = `${__dirname}/testFiles/${new Realm.BSON.UUID().toHexString()}`;
+    const customPath = `${__dirname}/testFiles/${new Realm.BSON.UUID().toHexString()}`;
 
     // Check to make sure the path for the realm doesn't already exist.
-    expect(existsSync(absolutePath)).toBe(false);
+    expect(existsSync(customPath)).toBe(false);
 
     // :snippet-start: set-absolute-path
-    const app = new Realm.App({ id: APP_ID, baseFilePath: absolutePath });
+    const app = new Realm.App({ id: APP_ID, baseFilePath: customPath });
     const user = await app.logIn(Realm.Credentials.anonymous());
 
     const realm = await Realm.open({
@@ -35,16 +35,49 @@ describe("Open realm at different paths", () => {
     // :snippet-end:
 
     // Check that realm exists at absolute path.
-    expect(existsSync(absolutePath)).toBe(true);
+    expect(existsSync(customPath)).toBe(true);
 
     // Check that the realm's path starts with the absolute path.
-    expect(realm.path.startsWith(absolutePath));
+    expect(realm.path.startsWith(customPath));
 
     await app.currentUser?.logOut();
 
     realm.close();
 
     // Remove realm files that are generated for this test.
-    rmSync(absolutePath, { recursive: true });
+    rmSync(customPath, { recursive: true });
+  });
+
+  test("should open a realm at a relative path", async () => {
+    const customPath = `testFiles`;
+
+    // Check to make sure the path for the realm doesn't already exist.
+    expect(existsSync(customPath)).toBe(false);
+
+    // :snippet-start: set-absolute-path
+    const app = new Realm.App({ id: APP_ID, baseFilePath: customPath });
+    const user = await app.logIn(Realm.Credentials.anonymous());
+
+    const realm = await Realm.open({
+      schema: [Car],
+      sync: {
+        flexible: true,
+        user,
+      },
+    });
+    // :snippet-end:
+
+    // Check that realm exists at absolute path.
+    expect(existsSync(customPath)).toBe(true);
+
+    // Check that the realm's path starts with the absolute path.
+    expect(realm.path.startsWith(customPath));
+
+    await app.currentUser?.logOut();
+
+    realm.close();
+
+    // Remove realm files that are generated for this test.
+    rmSync(customPath, { recursive: true });
   });
 });
