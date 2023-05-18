1a0ed0e8ddafcef00b2b915f6774fbec4fb43f5f
Dachary Carey <dachary.carey@mongodb.com>
2023-05-18 17:47:29 -0400

    Add tests for Kotlin auth code examples

--- examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/AuthenticationTest.kt
+++ examples/kotlin/shared/src/commonTest/kotlin/com/mongodb/realm/realmkmmapp/AuthenticationTest.kt
@@ -7,6 +7,7 @@ import io.realm.kotlin.mongodb.Credentials
 import io.realm.kotlin.mongodb.LoggedIn
 import io.realm.kotlin.mongodb.LoggedOut
 import io.realm.kotlin.mongodb.Removed
+import io.realm.kotlin.mongodb.User
 import io.realm.kotlin.mongodb.ext.call
 import io.realm.kotlin.mongodb.ext.customDataAsBsonDocument
 import kotlinx.coroutines.CoroutineScope
@@ -14,15 +15,26 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.mongodb.kbson.BsonDocument
+import org.mongodb.kbson.BsonInt32
+import org.mongodb.kbson.BsonString
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+import kotlin.test.assertIs
+import kotlin.test.assertNotEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 // :replace-start: {
 //   "terms": {
-//     "updatedUserData2": "updatedUserData"
+//     "updatedUserData2": "updatedUserData",
+//     "hideNotReusingAnonymousUser": "Credentials.anonymous()"
 //   }
 // }
 class AuthenticationTest: RealmTest() {
+
+    private val hideNotReusingAnonymousUser = Credentials.anonymous(reuseExisting = false)
 
     @Test
     fun linkCredentialsTest() {
@@ -31,11 +43,16 @@ class AuthenticationTest: RealmTest() {
         // :snippet-start: link-credentials
         val app: App = App.create(YOUR_APP_ID) // Replace this with your App ID
         runBlocking { // use runBlocking sparingly -- it can delay UI interactions
-            val user = app.login(Credentials.anonymous()) // logs in with an anonymous user
+            val user = app.login(hideNotReusingAnonymousUser) // logs in with an anonymous user
             // registers an email/password user
             app.emailPasswordAuth.registerUser(email, password)
             // links anonymous user with email/password credentials
             user.linkCredentials(Credentials.emailPassword(email, password));
+            // :remove-start:
+            // Login with the email/password user, and confirm it is the same user as the anonymous user
+            val emailPasswordUser = app.login(Credentials.emailPassword(email, password))
+            assertEquals(user, emailPasswordUser)
+            // :remove-end:
         }
         // :snippet-end:
     }
@@ -46,6 +63,7 @@ class AuthenticationTest: RealmTest() {
         val app: App = App.create(YOUR_APP_ID) // Replace this with your App ID
         runBlocking { // use runBlocking sparingly -- it can delay UI interactions
             val user = app.login(Credentials.anonymous())
+            assertEquals(User.State.LOGGED_IN, user.state) // :remove:
         }
         // :snippet-end:
 
@@ -53,10 +71,15 @@ class AuthenticationTest: RealmTest() {
         runBlocking {
             // Logs in with anonymous user
             val anonUser = app.login(Credentials.anonymous())
+            assertEquals(User.State.LOGGED_IN, anonUser.state)// :remove:
 
             // Creates a new anonymous user
             val otherAnonUser =
                 app.login(Credentials.anonymous(reuseExisting = false))
+            // :remove-start:
+            assertEquals(User.State.LOGGED_IN, otherAnonUser.state)
+            assertNotEquals(anonUser, otherAnonUser)
+            // :remove-end:
         }
         // :snippet-end:
     }
@@ -72,6 +95,7 @@ class AuthenticationTest: RealmTest() {
             app.emailPasswordAuth.registerUser(email, password)
             // :remove-end:
             val user = app.login(Credentials.emailPassword(email, password))
+            assertEquals(User.State.LOGGED_IN, user.state) // :remove:
         }
         // :snippet-end:
     }
@@ -90,8 +114,10 @@ class AuthenticationTest: RealmTest() {
             val user = app.login(credentials)
             // use the user object ...
 
+            assertEquals(User.State.LOGGED_IN, user.state) // :remove:
             // later, delete the user object
             user.delete() // regardless of which provider you used to login, you can logout using `delete()`
+            assertEquals(User.State.REMOVED, user.state) // :remove:
         }
         // :snippet-end:
     }
@@ -109,12 +135,14 @@ class AuthenticationTest: RealmTest() {
             // Register and login a temporary user to create an API key
             app.emailPasswordAuth.registerUser(email, password)
             val tempUser = app.login(Credentials.emailPassword(email, password))
+            assertEquals(User.State.LOGGED_IN, tempUser.state)
             val apiKey = tempUser.apiKeyAuth.create(randomKeyString)
             // API Key value in string form
             val key = apiKey.value!!
             // :remove-end:
             val user = app.login(Credentials.apiKey(key))
             // :remove-start:
+            assertEquals(User.State.LOGGED_IN, user.state)
             // delete the key so we're not constantly creating new user api keys
             tempUser.apiKeyAuth.delete(apiKey.id)
             tempUser.logOut()
@@ -128,12 +156,12 @@ class AuthenticationTest: RealmTest() {
         val idToken = getRandom()
         // :snippet-start: apple-authentication
         val app: App = App.create(YOUR_APP_ID) // Replace this with your App ID
+        assertFails(message = "token contains an invalid number of segments", block = { // :remove:
         runBlocking { // use runBlocking sparingly -- it can delay UI interactions
-            // :uncomment-start:
-            // val user = app.login(Credentials.apple(idToken))
-            // :uncomment-end:
+            val user = app.login(Credentials.apple(idToken))
         }
         // :snippet-end:
+        } )
     }
 
     @Test
@@ -151,12 +179,12 @@ class AuthenticationTest: RealmTest() {
         val jwtToken = getRandom()
         // :snippet-start: jwt-authentication
         val app: App = App.create(YOUR_APP_ID) // Replace this with your App ID
+        assertFails(message = "token contains an invalid number of segments", block = { // :remove:
         runBlocking { // use runBlocking sparingly -- it can delay UI interactions
-            // :uncomment-start:
-            // val user = app.login(Credentials.jwt(jwtToken))
-            // :uncomment-end:
+            val user = app.login(Credentials.jwt(jwtToken))
         }
         // :snippet-end:
+        } )
     }
 
     @Test
@@ -167,12 +195,15 @@ class AuthenticationTest: RealmTest() {
             // :snippet-start: access-token-get
             val token = user.accessToken
             // :snippet-end:
+            assertNotNull(token)
             // :snippet-start: access-token-refresh
             // Gets the current refresh token for the user
             fun getRefreshToken(): String {
                 return user.refreshToken
             }
             // :snippet-end:
+            val refreshToken = getRefreshToken()
+            assertNotNull(refreshToken)
             user.logOut()
         }
 
@@ -190,9 +221,11 @@ class AuthenticationTest: RealmTest() {
             // Pass the generated credential to app.login()
             val currentUser = app.login(customCredentials)
             // :snippet-end:
+            assertEquals(User.State.LOGGED_IN, currentUser.state)
             // :snippet-start: retrieve-current-user
             val user = app.currentUser
             // :snippet-end:
+            assertNotNull(user)
         }
     }
 
@@ -200,12 +233,13 @@ class AuthenticationTest: RealmTest() {
     fun customUserDataTest() {
         val app: App = App.create(FLEXIBLE_APP_ID)
         runBlocking {
-            app.login(Credentials.anonymous())
+            app.login(Credentials.anonymous(reuseExisting = false))
             // :snippet-start: read-custom-user-data
             val user = app.currentUser!!
             val customUserData = user.customDataAsBsonDocument()
             // :snippet-end:
-
+            // At this point, there is no custom user data
+            assertNull(customUserData)
             // :snippet-start: refresh-custom-user-data
             // Update the custom data object
             user.refreshCustomData()
@@ -214,6 +248,8 @@ class AuthenticationTest: RealmTest() {
             // updated data object
             val updatedUserData = user.customDataAsBsonDocument()
             // :snippet-end:
+            // Still no custom user data because we haven't written it
+            assertNull(updatedUserData)
 
             // :snippet-start: write-custom-user-data
             // Write the custom user data through a call
@@ -223,27 +259,37 @@ class AuthenticationTest: RealmTest() {
                     mapOf("userId" to user.id, "favoriteColor" to "blue")
                 )
 
+            assertIs<BsonDocument>(functionResponse) // :remove:
             // Refreshed custom user data contains updated
             // `favoriteColor` value added in above Atlas Function call
             user.refreshCustomData()
             val updatedUserData2 = user.customDataAsBsonDocument()
             // :snippet-end:
+            assertNotNull(updatedUserData2)
+            assertEquals(BsonString("blue"), updatedUserData2["favoriteColor"])
             // :snippet-start: delete-custom-user-data
             val deleteResponse = user.functions
                 .call<BsonDocument>("deleteCustomUserData")
             // :snippet-end:
-
+            assertNotNull(deleteResponse)
+            assertEquals(BsonInt32(1), deleteResponse["deletedCount"])
             user.logOut()
         }
     }
     @Test
     fun logoutTest() {
+        val email = getRandom()
+        val password = getRandom()
         val app: App = App.create(YOUR_APP_ID) // Replace this with your App ID
         runBlocking { // use runBlocking sparingly -- it can delay UI interactions
-            val user = app.login(Credentials.anonymous())
+            // This has to be an email/password user because logging out an anonymous user
+            // sets their state to REMOVED instead of LOGGED_OUT
+            app.emailPasswordAuth.registerUser(email, password)
+            val user = app.login(Credentials.emailPassword(email, password))
             // :snippet-start: log-out
             user.logOut()
             // :snippet-end:
+            assertEquals(User.State.LOGGED_OUT, user.state)
         }
     }
 
