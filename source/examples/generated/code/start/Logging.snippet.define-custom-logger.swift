dc52385b62fa1b2b53e228c8ca1c777e7cbd51c8
Dachary Carey <dachary.carey@mongodb.com>
2023-06-01 16:28:26 -0400

    Refine Swift logging docs to better match other SDKs

--- source/examples/generated/code/start/Logging.snippet.define-custom-logger.swift
+++ source/examples/generated/code/start/Logging.snippet.define-custom-logger.swift
@@ -1,6 +1,3 @@
-// Access your app
-let app = App(id: YOUR_APP_SERVICES_APP_ID)
-
 // Create an instance of `Logger` and define the log function to invoke.
 let logger = Logger(level: .detail) { level, message in
     "REALM DEBUG: \(Date.now) \(level) \(message) \n"
