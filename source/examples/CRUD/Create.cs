5f47a41a3716ce7cb495f21791adb18b0ef36165
MongoCaleb <32645888+MongoCaleb@users.noreply.github.com>
2023-06-05 09:04:51 -0700

    Update Async Guidance  (#2826)
    ## Pull Request Info
    
    ### Jira
    
    https://jira.mongodb.org/browse/DOCSP-23219
    
    ### Staged Changes
    
    - [Write
    Transactions](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/docswp-23219/sdk/dotnet/crud/write-transactions/)
    -
    [Create](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/docswp-23219/sdk/dotnet/crud/create/)
    -
    [Threading](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/docswp-23219/sdk/dotnet/crud/threading/)

--- source/examples/CRUD/Create.cs
+++
@@ -1,9 +0,0 @@
-// Open a thread-safe transaction.
-realm.Write(() =>
-{
-    // Instantiate a class, as normal.
-    var dog = new Dog { Name = "Max", Age = 5 };
-
-    // Add the instance to the realm.
-    realm.Add(dog);
-});
