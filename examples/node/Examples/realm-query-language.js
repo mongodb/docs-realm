a3d9d2632e649995a4354802106da24f2f6ddcb0
lindseymoore <lindsey.moore@mongodb.com>
2023-06-12 20:00:13 -0400

    reorganized table and comments

--- examples/node/Examples/realm-query-language.js
+++ examples/node/Examples/realm-query-language.js
@@ -116,6 +116,8 @@ describe("Realm Query Language Reference", () => {
 
     const highPriorityItems = items.filtered(
       // :snippet-start: comparison-operators
+      // Find high priority to-do items by comparing the value of the ``priority``
+      // property value with a threshold number, above which priority can be considered high.
       "priority > $0", 5
       // :remove-start:
     );
@@ -124,6 +126,7 @@ describe("Realm Query Language Reference", () => {
     const longRunningItems = items.filtered(
       // :remove-end:
 
+      // Find long-running to-do items by seeing if the progressMinutes property is at or above a certain value.
       "progressMinutes > $0", 120
       // :remove-start:
     );
@@ -132,6 +135,7 @@ describe("Realm Query Language Reference", () => {
     const unassignedItems = items.filtered(
       // :remove-end:
 
+      // Find unassigned to-do items by finding items where the assignee property is equal to null.
       "assignee == $0", null
       // :remove-start:
     );
@@ -140,6 +144,8 @@ describe("Realm Query Language Reference", () => {
     const progressMinutesRange = items.filtered(
       // :remove-end:
 
+      // Find to-do items within a certain time range by finding items 
+      // where the progressMinutes property is between two numbers.
       "progressMinutes BETWEEN { $0 , $1 }", 30, 60
       // :remove-start:
     );
@@ -148,6 +154,7 @@ describe("Realm Query Language Reference", () => {
     const progressMinutesIn = items.filtered(
       // :remove-end:
 
+      // Find to-do items with a certain amount of progressMinutes from the given list.
       "progressMinutes IN { $0, $1, $2, $3, $4, $5 }", 10, 20, 30, 40, 50, 60
       // :snippet-end:
     );
