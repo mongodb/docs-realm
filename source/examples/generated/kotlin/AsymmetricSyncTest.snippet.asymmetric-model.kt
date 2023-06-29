f72c5a61d5d74ee1827e219514fc98ec765e236b
cbullinger <115956901+cbullinger@users.noreply.github.com>
2023-06-29 15:21:44 -0400

    Add blurb to Flex Sync Subscriptions page

--- source/examples/generated/kotlin/AsymmetricSyncTest.snippet.asymmetric-model.kt
+++ source/examples/generated/kotlin/AsymmetricSyncTest.snippet.asymmetric-model.kt
@@ -1,7 +1,7 @@
 class WeatherSensor : AsymmetricRealmObject {
     @PersistedName("_id")
     @PrimaryKey
-    var id: ObjectId = BsonObjectId()
+    var id: ObjectId = ObjectId()
     var deviceId: String = ""
     var temperatureInFarenheit: Float = 0.0F
     var barometricPressureInHg: Float = 0.0F
--- source/examples/generated/kotlin/AsymmetricSyncTest.snippet.create-asymmetric-object.kt
+++ source/examples/generated/kotlin/AsymmetricSyncTest.snippet.create-asymmetric-object.kt
@@ -1,6 +1,5 @@
 asymmetricRealm.write {
     insert(WeatherSensor().apply {
-        id = ObjectId()
         deviceId = "WX1278UIT"
         temperatureInFarenheit = 6.7F
         barometricPressureInHg = 29.65F
