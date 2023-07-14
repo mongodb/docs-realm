da82153f554911bd43bc1fa3624610386a64477d
krollins-mdb <kyle.rollins@mongodb.com>
2023-07-14 15:29:24 -0500

    Move samples back into original directory

--- examples/node/legacy/scripts/gen_snippets_legacy.sh
+++ examples/node/legacy/scripts/gen_snippets_legacy.sh
@@ -4,12 +4,12 @@ PROJECT=$(git rev-parse --show-toplevel)
 INPUT_DIRECTORY=$PROJECT/examples/node/legacy
 OUTPUT_DIRECTORY=$PROJECT/source/examples/generated/node/legacy
 
-echo "$INPUT_DIRECTORY"
+echo $INPUT_DIRECTORY
 
 # standard bluehawking
-rm -f "$OUTPUT_DIRECTORY"/*
+rm -f $OUTPUT_DIRECTORY/*
 echo "Bluehawking Node unit test examples..."
-bluehawk snip "$INPUT_DIRECTORY"/__tests__ -o "$OUTPUT_DIRECTORY"
+bluehawk snip $INPUT_DIRECTORY/Examples -o $OUTPUT_DIRECTORY
 
 # TODO: for using Detype to generate JS from TS.
 # npx detype "$OUTPUT_DIRECTORY" "$OUTPUT_DIRECTORY"
--- examples/node/v12/scripts/gen_snippets_v12.sh
+++ examples/node/v12/scripts/gen_snippets_v12.sh
@@ -5,9 +5,9 @@ INPUT_DIRECTORY=$PROJECT/examples/node/v12
 OUTPUT_DIRECTORY=$PROJECT/source/examples/generated/node/v12
 
 # standard bluehawking
-rm -f "$OUTPUT_DIRECTORY"/*
+rm -f $OUTPUT_DIRECTORY/*
 echo "Bluehawking Node unit test examples..."
-bluehawk snip "$INPUT_DIRECTORY"/__tests__ -o "$OUTPUT_DIRECTORY"
+bluehawk snip $INPUT_DIRECTORY/__tests__ -o $OUTPUT_DIRECTORY
 
 # TODO: for using Detype to generate JS from TS.
 # npx detype "$OUTPUT_DIRECTORY" "$OUTPUT_DIRECTORY"
