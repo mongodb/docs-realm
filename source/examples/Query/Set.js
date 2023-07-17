ec008d197b45166d30c1b5a846a90239f75623f0
lindseymoore <71525840+lindseymoore@users.noreply.github.com>
2023-07-17 17:51:45 -0400

    (DOCSP-30702): Node.js: Parameterize Queries (#2890)
    ## Pull Request Info
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-30702
    
    ### Staged Changes
    
    -
    [Read](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-30702/sdk/node/crud/read/)
    - [Query
    Data](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-30702/sdk/node/crud/query-data/)
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [ ] Create Jira ticket for corresponding docs-app-services update(s),
    if any
    - [ ] Checked/updated Admin API
    - [ ] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

--- source/examples/Query/Set.js
+++ source/examples/Query/Set.js
@@ -1,8 +1,8 @@
 console.log(
   "Number of projects with no complete tasks: " +
-    projects.filtered("ALL tasks.isComplete == false").length
+    projects.filtered("ALL tasks.isComplete == $0", false).length
 );
 console.log(
   "Number of projects with any top priority tasks: " +
-    projects.filtered("ANY tasks.priority == 10").length
+    projects.filtered("ANY tasks.priority == $0", 10).length
 );
