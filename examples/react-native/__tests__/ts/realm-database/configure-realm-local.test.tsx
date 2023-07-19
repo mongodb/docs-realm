b0f340baf950c8bcf11fa32b4da53f5032a10275
krollins-mdb <kyle.rollins@mongodb.com>
2023-07-19 15:42:53 -0500

    Refactor configure realm test

--- examples/react-native/__tests__/ts/realm-database/configure-realm-local.test.tsx
+++ examples/react-native/__tests__/ts/realm-database/configure-realm-local.test.tsx
@@ -1,11 +1,17 @@
 // :snippet-start: configure-realm
 import React from 'react';
-import {RealmContext} from '../RealmConfig';
+import {Realm, RealmProvider, useRealm} from '@realm/react';
 // :remove-start:
-import {render} from '@testing-library/react-native';
+import {render, waitFor} from '@testing-library/react-native';
 import {View, Text} from 'react-native';
 
+let isRealmClosed = true;
+
 function MyApp() {
+  const realm = useRealm();
+
+  isRealmClosed = realm.isClosed;
+
   return (
     <View>
       <Text>Foo</Text>
@@ -14,17 +20,37 @@ function MyApp() {
 }
 // :remove-end:
 
-function AppWrapperLocal() {
-  const {RealmProvider} = RealmContext;
+class Turtle extends Realm.Object {
+  _id!: string;
+  owner_id!: string;
+  name!: string;
+  birthDate?: Realm.Mixed;
 
+  static schema = {
+    name: 'Turtle',
+    properties: {
+      _id: 'string',
+      name: 'string',
+      birthDate: 'mixed',
+      owner_id: 'string',
+    },
+    primaryKey: '_id',
+  };
+}
+
+function AppWrapperLocal() {
   return (
-    <RealmProvider>
+    <RealmProvider schema={[Turtle]}>
       <MyApp />
     </RealmProvider>
   );
 }
 // :snippet-end:
 
-test('Instantiate RealmProvider correctly', () => {
+test('Instantiate RealmProvider correctly', async () => {
   render(<AppWrapperLocal />);
+
+  await waitFor(() => {
+    expect(isRealmClosed).toBe(false);
+  });
 });
--- source/examples/generated/react-native/ts/configure-realm-local.test.snippet.configure-realm.tsx
+++ source/examples/generated/react-native/ts/configure-realm-local.test.snippet.configure-realm.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import {RealmContext} from '../RealmConfig';
+import {Realm, RealmProvider, useRealm} from '@realm/react';
+
+class Turtle extends Realm.Object {
+  _id!: string;
+  owner_id!: string;
+  name!: string;
+  birthDate?: Realm.Mixed;
+
+  static schema = {
+    name: 'Turtle',
+    properties: {
+      _id: 'string',
+      name: 'string',
+      birthDate: 'mixed',
+      owner_id: 'string',
+    },
+    primaryKey: '_id',
+  };
+}
 
 function AppWrapperLocal() {
-  const {RealmProvider} = RealmContext;
-
   return (
-    <RealmProvider>
+    <RealmProvider schema={[Turtle]}>
       <MyApp />
     </RealmProvider>
   );
