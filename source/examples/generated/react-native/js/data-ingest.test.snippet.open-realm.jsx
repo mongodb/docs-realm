a3763d27d551c9746dd4007c88ddc32e3df418f5
amalhotra-mdb <135728846+amalhotra-mdb@users.noreply.github.com>
2023-07-27 11:35:49 -0400

    (DOCSP-29578) Document asymmetric sync/stream Swift style (#2870)
    ## Pull Request Info
    
    Same work as outlined for Node.js SDK, but with `realm/react` in mind.
    
    The React Native SDK's "[Stream Data to
    Atlas](https://www.mongodb.com/docs/realm/sdk/react-native/sync-data/stream-data-to-atlas/)"
    page should be re-worked to be similar to the updated Node.js SDK's
    "[Stream Data to
    Atlas](https://www.mongodb.com/docs/realm/sdk/node/sync/stream-data-to-atlas/)"
    page. This will involve:
    
    Using the `.. procedure::` and `.. step::` Snooty directives.
    Removing the unnecessary "Example" heading.
    Modify the Node.js SDK's "Stream Data to Atlas" content to apply to the
    React Native SDK. Keep in mind that with `@realm/react`, we "configure"
    realms instead of opening and closing them. We also use hooks and
    providers.
    Removing the current React Native example for Data Ingest:
    `/examples/generated/react-native/ts/Invoice.snippet.ts-invoice-schema.ts`.
    This sample was never part of any actual test files.
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-29578
    
    ### Staged Changes
    
    - [Stream Data to Atlas - React Native
    SDK](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-29578/sdk/react-native/sync-data/stream-data-to-atlas/)
    - [Define a Realm Object Model - React Native
    SDK](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-29578/sdk/react-native/model-data/define-a-realm-object-model/#define-an-asymmetric-object)
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

---
+++ source/examples/generated/react-native/js/data-ingest.test.snippet.open-realm.jsx
@@ -0,0 +1,23 @@
+// Create a configuration object
+const realmConfig = { schema: [WeatherSensor] };
+  
+// Create a realm context
+const {RealmProvider, useRealm, useObject, useQuery} =
+    createRealmContext(realmConfig);
+  
+// Expose a sync realm
+function AppWrapperSync() {
+    return (
+        <AppProvider id={APP_ID}>
+            <UserProvider fallback={LogIn}>
+                <RealmProvider
+                    sync={{
+                        flexible: true,
+                        onError: console.error
+                    }}>
+                    <App />
+                </RealmProvider>
+            </UserProvider>
+        </AppProvider>
+    );
+}
