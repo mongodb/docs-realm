c6d39c8451b453e8603059707ec2c4215ee8a950
Dachary <dachary.carey@mongodb.com>
2023-07-31 16:25:32 -0400

    (DOCSP-31727): Java SDK Migrate PBS to FS Updates (#2921)
    ## Pull Request Info
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-31727
    
    ### Staged Changes
    
    - [Sync
    Data](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-31727/sdk/java/sync/):
    Remove PBS mentions
    - [Partition-Based
    Sync](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-31727/sdk/java/sync/partition-based-sync/):
    New page with moved PBS info and PBS to FS migration info
    - [Configure & Open a Synced
    Realm](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-31727/sdk/java/sync/configure-and-open-a-synced-realm/):
    Remove PBS info, update the "Synchronous Reads & Writes on the UI
    Thread" `Important` callout to show FS code examples
    
    ### Reminder Checklist
    
    If your PR modifies the docs, you might need to also update some
    corresponding
    pages. Check if completed or N/A.
    
    - [x] Create Jira ticket for corresponding docs-app-services update(s),
    if any
    - [x] Checked/updated Admin API
    - [x] Checked/updated CLI reference
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

--- examples/java/sync/app/src/androidTest/java/com/mongodb/realm/examples/java/FlexibleSyncTest.java
+++ examples/java/sync/app/src/androidTest/java/com/mongodb/realm/examples/java/FlexibleSyncTest.java
@@ -97,6 +97,59 @@ public class FlexibleSyncTest extends RealmTest {
     }
 
     @Test
+    public void openARealmForReadWriteUIThread() {
+        Expectation expectation = new Expectation();
+        activity.runOnUiThread (() -> {
+            // instantiate a Realm App connection
+            String appID = YOUR_APP_ID; // replace this with your App ID
+            App app = new App(new AppConfiguration.Builder(appID)
+                    .build());
+            // authenticate a user
+            Credentials credentials = Credentials.anonymous();
+            app.loginAsync(credentials, it -> {
+                if (it.isSuccess()) {
+                    User user = it.get();
+                    // add an initial subscription to the sync configuration
+                    // :snippet-start: fs-allow-queries-on-ui-thread
+                    SyncConfiguration config = new SyncConfiguration.Builder(user)
+                            .allowQueriesOnUiThread(true)
+                            .allowWritesOnUiThread(true)
+                            .initialSubscriptions(new SyncConfiguration.InitialFlexibleSyncSubscriptions() {
+                                @Override
+                                public void configure(Realm realm, MutableSubscriptionSet subscriptions) {
+                                    subscriptions.add(Subscription.create("springPeepers",
+                                            realm.where(Frog.class)
+                                                    .equalTo("species", "spring peeper")));
+                                }
+                            })
+                            // :remove-start:
+                            .inMemory()
+                            .waitForInitialRemoteData(2112, TimeUnit.MILLISECONDS)
+                            // :remove-end:
+                            .build();
+
+                    Realm.getInstanceAsync(config, new Realm.Callback() {
+                        @Override
+                        public void onSuccess(Realm realm) {
+                            Log.v(
+                                    "EXAMPLE",
+                                    "Successfully opened a realm with reads and writes allowed on the UI thread.");
+                            // :remove-start:
+                            realm.close();
+                            expectation.fulfill();
+                            // :remove-end:
+                        }
+                    });
+                    // :snippet-end:
+                } else {
+                    Log.e("EXAMPLE", "Failed to log in: " + it.getError().getErrorMessage());
+                }
+            });
+        });
+        expectation.await();
+    }
+
+    @Test
     public void explicitlyNamedSubscription() {
         Expectation expectation = new Expectation();
         activity.runOnUiThread (() -> {
