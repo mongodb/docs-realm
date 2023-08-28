bcd9633264474fbe0230033aba95e6b52ee63783
Kyle Rollins <115574589+krollins-mdb@users.noreply.github.com>
2023-08-28 09:43:43 -0500

    (DOCSP-31852) Geospatial Queries for Points - React Native (#2972)
    ## Pull Request Info
    
    This PR includes a couple things:
    - A new Geospatial page and tests
    - A new testing approach for React Native
    
    I needed a real example to work with for the new approach and it seems
    to work well. The basic idea is this:
    - Create a real React Native app, with new components for each feature
    that needs to be tested.
    - Create test files for each component.
    
    This separation makes it easier to write and maintain tests. Eventually,
    we'll also be able to run full end-to-end tests this way.
    
    **CI Tests**
    The CI test doesn't seem to be able to handle imports correctly for some
    reason. Not sure what to do about it.
    
    https://github.com/mongodb/docs-realm/actions/runs/5967802925/job/16190257356?pr=2972
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-31852
    
    ### Staged Changes
    
    - [Geospatial - React
    Nativel](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-31852/sdk/react-native/model-data/data-types/geospatial/)
    - [Geospatial -
    Node.js](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-31852/sdk/node/model-data/data-types/geospatial/).
    Add an include about Flexible Sync.
    - [Geospatial -
    .NET](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-31852/sdk/dotnet/model-data/data-types/geospatials/).
    Add an include about Flexible Sync.
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)
    
    ### Animal Wearing a Hat
    
    <img
    src="https://cdn.eyeem.com/thumb/b62716d62756471677a3a3a383938333538353533353834313d273665336430383161313036603336346661653838303932603535353433313938313535653333633a3a3a3/800/800"
    width=500>

--- examples/react-native/v12/TestApp/App.tsx
+++ examples/react-native/v12/TestApp/App.tsx
@@ -1,82 +1,23 @@
-import React, {useState, useEffect} from 'react';
-import {RealmProvider, useRealm} from '@realm/react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import {HomeScreen} from './src/screens/HomeScreen';
+import {Geospatial} from './src/components/data-types/Geospatial';
 
-function Section(): JSX.Element {
-  const realm = useRealm();
-  const [realmState, setRealmState] = useState('closed');
-  console.debug('Realm status:', realm.isClosed ? 'closed' : 'open');
-
-  useEffect(() => {
-    setRealmState(realm.isClosed ? 'closed' : 'open'); // :emphasize:
-  }, [realm.isClosed]);
-
-  return (
-    <View style={styles.sectionContainer}>
-      <Text>Realm status: {realmState}</Text>
-    </View>
-  );
-}
+const Drawer = createDrawerNavigator();
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <RealmProvider>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <Header />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            }}>
-            <Section />
-          </View>
-        </ScrollView>
-      </RealmProvider>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Geospatial" component={Geospatial} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  // :emphasize-start:
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  // :emphasize-end:
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
