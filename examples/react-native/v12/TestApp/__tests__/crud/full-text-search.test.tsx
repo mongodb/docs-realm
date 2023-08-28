f7e031ffca8709e4cf67383c3892033bfd5e55b6
lindseymoore <lindsey.moore@mongodb.com>
2023-08-28 14:49:13 -0400

    Starting remodel of test

--- examples/react-native/v12/TestApp/__tests__/crud/full-text-search.test.tsx
+++ examples/react-native/v12/TestApp/__tests__/crud/full-text-search.test.tsx
@@ -3,69 +3,11 @@ import {View, Text, InteractionManagerStatic} from 'react-native';
 import Realm, {ObjectSchema} from 'realm';
 import {createRealmContext} from '@realm/react';
 import {render, screen, waitFor} from '@testing-library/react-native';
-import {RealmProvider, useRealm, useQuery} from '@realm/react';
-import {useEffect} from 'react';
-
-// :snippet-start: rn-fts-annotation
-class Book extends Realm.Object {
-  name!: string;
-  price?: number;
-
-  static schema: ObjectSchema = {
-    name: 'Book',
-    properties: {
-      name: {type: 'string', indexed: 'full-text'},
-      price: 'int?',
-    },
-  };
-}
-// :snippet-end:
+import {FtsQuery} from '../../examples/react-native/v12/TestApp/__tests__/crud/full-text-search'; //how to get file path
 
 describe('Full text search query',  () => {
 
   test('FTS query', async() => {
-
-    <RealmProvider schema={[Book]}>
-
-      <FtsQuery />
-      
-    </RealmProvider>
-
-    // create component to test fts
-    function FtsQuery(): JSX.Element {
-
-      const realm = useRealm();
-
-      // function to create books
-      const writeBooks = (bookName: string, price: number) => {
-        realm.write(() => {
-          realm.create('Book', {
-            name: bookName,
-            price: price,
-          });
-        });
-      };
-
-      // create the books
-      writeBooks("The Hunger Games", 10.99);
-      writeBooks("Lord of the Rings", 21.99);
-      writeBooks("Dune", 16.99);
-
-      // then query on them
-      // get the book objects
-      const books = realm.objects("Book");
-      // fts query the books
-      const booksWithThe = books.filtered("name TEXT \$0", ["the"]);
-
-      // Return the number of books in query
-      return (
-        <View>
-          <Text> Books with 'the': {booksWithThe.length} </Text>
-        </View>
-      )
-
-    }
-
     // render the query component
     render(<FtsQuery />);
 
@@ -82,4 +24,4 @@ describe('Full text search query',  () => {
 
 });
 
-// also where is the api for filtered?
+// also where is the api for filtered? under results
