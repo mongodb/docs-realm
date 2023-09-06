2292df27bc8776d6fa25683b28d5d31f12868ffc
cbullinger <cory.bullinger@mongodb.com>
2023-09-06 12:57:55 -0400

    Update FTS with prefix search

--- source/examples/generated/kotlin/ReadTest.snippet.kotlin-fts-query.kt
+++ source/examples/generated/kotlin/ReadTest.snippet.kotlin-fts-query.kt
@@ -1,5 +1,11 @@
-// Find all the books with science fiction as the genre
-var scienceFiction = realmFts.query<Book>("genre TEXT $0", "science fiction").find()
+// Find all books with "science fiction" as the genre
+val scienceFiction =
+    realm.query<Book>("genre TEXT $0", "science fiction").find()
 
-// Find all the books with fiction but not science in the genre
-var fictionNotScience = realmFts.query<Book>("genre TEXT $0", "fiction -science").find()
+// Find all books with "fiction" but not "science" in the genre
+val fictionNotScience =
+    realm.query<Book>("genre TEXT $0", "fiction -science").find()
+
+// Find all books with "sci-" and "fi-" prefixes in the genre
+val sciFi =
+    realm.query<Book>("genre TEXT $0", "sci* fi*").find()
