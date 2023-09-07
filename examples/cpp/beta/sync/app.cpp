3a97a9c412c49c73aea2772bc0da9994dcb83658
Dachary Carey <dachary.carey@mongodb.com>
2023-09-07 12:40:59 -0400

    Add version to open a realm page, don't require experimental namespace

--- examples/cpp/beta/sync/app.cpp
+++ examples/cpp/beta/sync/app.cpp
@@ -1,16 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
 #include <cpprealm/sdk.hpp>
-#include <cpprealm/experimental/sdk.hpp>
 
 static const std::string APP_ID = "cpp-tester-uliix";
-
-using namespace realm::experimental;
 
 TEST_CASE("test custom headers compile", "[realm][sync]") {
     // :snippet-start: set-custom-headers-for-app
     std::map<std::string, std::string> customHttpHeaders;
     customHttpHeaders.insert(std::pair<std::string, std::string>("CUSTOM_HEADER_NAME", "CUSTOM_HEADER_VALUE"));
-    
+
     auto app = realm::App(APP_ID, std::nullopt, std::nullopt, customHttpHeaders);
     // :snippet-end:
 
