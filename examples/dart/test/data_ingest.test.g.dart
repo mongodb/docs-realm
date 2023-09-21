461e34bce857eab3e2e4e373352086049bfeaec5
Kyle Rollins <115574589+krollins-mdb@users.noreply.github.com>
2023-09-21 09:15:20 -0500

    (DOCSP-33149) Add Data Ingest docs for Flutter (#3017)
    ## Pull Request Info
    
    This PR adds docs and tests for Data Ingest. Note that the Flutter SDK
    doesn't have a mongoClient yet. It's not really feasible to test that
    asymmetric objects make it to the backend without the mongoClient.
    However, we can test that the write occurs and the object is deleted
    after it's synced.
    
    ### Jira
    
    - https://jira.mongodb.org/browse/DOCSP-33149
    
    ### Staged Changes
    
    - [Stream Data to
    Atlas](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-33149/sdk/flutter/sync/stream-data-to-atlas/).
    New page.
    - [Define a Realm Object
    Schema](https://docs-mongodbcom-staging.corp.mongodb.com/realm/docsworker-xlarge/DOCSP-33149/sdk/flutter/realm-database/model-data/define-realm-object-schema/#define-an-asymmetric-object).
    New section for defining an asymmetric object.
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)
    
    ### Animal Wearing a Hat
    
    
    ![](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5G5snnQTWgTYB60srJ4j_mry27nCnine685JRnPjF_tokRZuoBX9YQiKz0QgvlAevFTE&usqp=CAU)
    
    ---------
    
    Co-authored-by: cbullinger <115956901+cbullinger@users.noreply.github.com>

---
+++ examples/dart/test/data_ingest.test.g.dart
@@ -0,0 +1,82 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'data_ingest.test.dart';
+
+// **************************************************************************
+// RealmObjectGenerator
+// **************************************************************************
+
+class WeatherSensor extends _WeatherSensor
+    with RealmEntity, RealmObjectBase, AsymmetricObject {
+  WeatherSensor(
+    ObjectId id,
+    String deviceId,
+    double modtemperatureInFahrenheitel,
+    double barometricPressureInHg,
+    double windSpeedInMph,
+  ) {
+    RealmObjectBase.set(this, '_id', id);
+    RealmObjectBase.set(this, 'deviceId', deviceId);
+    RealmObjectBase.set(
+        this, 'modtemperatureInFahrenheitel', modtemperatureInFahrenheitel);
+    RealmObjectBase.set(this, 'barometricPressureInHg', barometricPressureInHg);
+    RealmObjectBase.set(this, 'windSpeedInMph', windSpeedInMph);
+  }
+
+  WeatherSensor._();
+
+  @override
+  ObjectId get id => RealmObjectBase.get<ObjectId>(this, '_id') as ObjectId;
+  @override
+  set id(ObjectId value) => RealmObjectBase.set(this, '_id', value);
+
+  @override
+  String get deviceId =>
+      RealmObjectBase.get<String>(this, 'deviceId') as String;
+  @override
+  set deviceId(String value) => RealmObjectBase.set(this, 'deviceId', value);
+
+  @override
+  double get modtemperatureInFahrenheitel =>
+      RealmObjectBase.get<double>(this, 'modtemperatureInFahrenheitel')
+          as double;
+  @override
+  set modtemperatureInFahrenheitel(double value) =>
+      RealmObjectBase.set(this, 'modtemperatureInFahrenheitel', value);
+
+  @override
+  double get barometricPressureInHg =>
+      RealmObjectBase.get<double>(this, 'barometricPressureInHg') as double;
+  @override
+  set barometricPressureInHg(double value) =>
+      RealmObjectBase.set(this, 'barometricPressureInHg', value);
+
+  @override
+  double get windSpeedInMph =>
+      RealmObjectBase.get<double>(this, 'windSpeedInMph') as double;
+  @override
+  set windSpeedInMph(double value) =>
+      RealmObjectBase.set(this, 'windSpeedInMph', value);
+
+  @override
+  Stream<RealmObjectChanges<WeatherSensor>> get changes =>
+      RealmObjectBase.getChanges<WeatherSensor>(this);
+
+  @override
+  WeatherSensor freeze() => RealmObjectBase.freezeObject<WeatherSensor>(this);
+
+  static SchemaObject get schema => _schema ??= _initSchema();
+  static SchemaObject? _schema;
+  static SchemaObject _initSchema() {
+    RealmObjectBase.registerFactory(WeatherSensor._);
+    return const SchemaObject(
+        ObjectType.asymmetricObject, WeatherSensor, 'WeatherSensor', [
+      SchemaProperty('id', RealmPropertyType.objectid,
+          mapTo: '_id', primaryKey: true),
+      SchemaProperty('deviceId', RealmPropertyType.string),
+      SchemaProperty('modtemperatureInFahrenheitel', RealmPropertyType.double),
+      SchemaProperty('barometricPressureInHg', RealmPropertyType.double),
+      SchemaProperty('windSpeedInMph', RealmPropertyType.double),
+    ]);
+  }
+}
