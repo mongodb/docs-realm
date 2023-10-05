2bdf5b4d59311f21a7f31352b0822027b0d3579e
Dachary Carey <dachary.carey@mongodb.com>
2023-10-05 11:34:18 -0400

    Clarify requirement to set a logger before initializing an App

--- examples/ios/Examples/Logging.swift
+++ examples/ios/Examples/Logging.swift
@@ -68,6 +68,9 @@ class Logging: XCTestCase {
         // Create an instance of `Logger` and define the log function to invoke.
         let logger = Logger(level: .detail) { level, message in
             logs += "\(Date.now) \(level) \(message) \n" // :remove:
+            // You may pass log information to a logging service, or
+            // you could simply print the logs for debugging. Define
+            // the log function that makes sense for your application.
             "REALM DEBUG: \(Date.now) \(level) \(message) \n"
         }
         // :snippet-end:
