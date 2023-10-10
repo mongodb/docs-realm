9454cdf74a4a5bb0d3ddd8eb660aec4b1bcc9021
cbullinger <cory.bullinger@mongodb.com>
2023-10-10 11:02:54 -0400

    Initial draft

--- source/examples/generated/kotlin/UpdateTest.snippet.update-realm-dictionary.kt
+++ source/examples/generated/kotlin/UpdateTest.snippet.update-realm-dictionary.kt
@@ -1,14 +1,9 @@
 // Find frogs who have forests with favorite ponds
-val frogs = realm.query<Frog>().find()
-val frogsWithFavoritePonds = frogs.query("favoritePondsByForest.@count > 1").find()
-val thisFrog = frogsWithFavoritePonds.first()
+val thisFrog = realm.query<RealmDictionary_Frog>("favoritePondsByForest.@count > 1").find().first()
 // Update the value for a key if it exists
 if (thisFrog.favoritePondsByForest.containsKey("Hundred Acre Wood")) {
     realm.write {
-        findLatest(thisFrog)?.favoritePondsByForest?.set(
-            "Hundred Acre Wood",
-            "Lily Pad Pond"
-        )
+        findLatest(thisFrog)?.favoritePondsByForest?.set("Lothlorien", "Lily Pad Pond")
     }
 }
 // Add a new key-value pair
