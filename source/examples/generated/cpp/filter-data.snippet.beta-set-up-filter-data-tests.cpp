3f2e382c239bd7308ee2527c0bda928eaa676aa4
Dachary <dachary.carey@mongodb.com>
2023-12-01 11:26:59 -0500

    (DOCSP-34629): C++ SDK: Add sort documentation (#3095)
    ## Pull Request Info
    
    Jira ticket: https://jira.mongodb.org/browse/DOCSP-34629
    
    Staged changes:
    
    -
    [CRUD/Read](https://preview-mongodbdacharyc.gatsbyjs.io/realm/DOCSP-34629/sdk/cpp/crud/read/#sort-lists-and-query-results):
    Add a "Sort Lists and Query Results" section
    - [CRUD/Filter
    Data](https://preview-mongodbdacharyc.gatsbyjs.io/realm/DOCSP-34629/sdk/cpp/crud/filter-data/):
    Remove the note about not yet supporting sort, and link to the sort
    example on the Read page
    
    ### Reminder Checklist
    
    Before merging your PR, make sure to check a few things.
    
    - [x] Describe your PR's changes in the Release Notes section
    - [x] Create a Jira ticket for corresponding docs-realm
    docs-app-services, if any
    
    ### Release Notes
    
    
    - **C++ SDK**
      - CRUD
    - Read: Add a "Sort Lists and Query Results" section with tested,
    Bluehawked code examples for sorting in C++ SDK.
    - Filter Data: Remove the note about not yet supporting sort, and link
    to the sort example on the Read page.
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

--- source/examples/generated/cpp/filter-data.snippet.beta-set-up-filter-data-tests.cpp
+++ source/examples/generated/cpp/filter-data.snippet.beta-set-up-filter-data-tests.cpp
@@ -1,23 +1,17 @@
 auto config = realm::db_config();
 auto realm = db(std::move(config));
 
-auto item1 = Item {
-    .name = "Save the cheerleader",
-    .assignee = std::string("Peter"),
-    .isComplete = false,
-    .priority = 6,
-    .progressMinutes = 30
-};
+auto item1 = Item{.name = "Save the cheerleader",
+                       .assignee = std::string("Peter"),
+                       .isComplete = false,
+                       .priority = 6,
+                       .progressMinutes = 30};
 
-auto project = Project {
-    .name = "New project"
-};
+auto project = Project{.name = "New project"};
 
 project.items.push_back(&item1);
 
-realm.write([&] {
-    realm.add(std::move(project));
-});
+realm.write([&] { realm.add(std::move(project)); });
 
 auto items = realm.objects<Item>();
 auto projects = realm.objects<Project>();
