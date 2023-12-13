037c9d77472ed0bcf934de7f42ea0ceb2f7ec705
Dachary Carey <dachary.carey@mongodb.com>
2023-12-13 14:23:35 -0500

    Upgrade fragment-ktx to 1.6.2 - builds and tests work

--- examples/kotlin/shared/build.gradle.kts
+++ examples/kotlin/shared/build.gradle.kts
@@ -88,7 +88,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity-ktx:1.8.1")
-    implementation("androidx.fragment:fragment-ktx:1.3.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("com.facebook.android:facebook-login:latest.release")
     implementation("com.google.android.material:material:1.6.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
