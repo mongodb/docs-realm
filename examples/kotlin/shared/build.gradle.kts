d094a8e9779faf01645d4702736c81bb0e725f03
Dachary Carey <dachary.carey@mongodb.com>
2023-12-13 14:13:53 -0500

    Upgrade to appcompat 1.6.1 - builds and tests work

--- examples/kotlin/shared/build.gradle.kts
+++ examples/kotlin/shared/build.gradle.kts
@@ -86,7 +86,7 @@ android {
 }
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.activity:activity-ktx:1.2.0")
     implementation("androidx.fragment:fragment-ktx:1.3.0")
     implementation("com.facebook.android:facebook-login:latest.release")
