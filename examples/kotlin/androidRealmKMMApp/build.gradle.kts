cd3b0a3d451df0dc7b0655e58c2c6caaac125880
Dachary Carey <dachary.carey@mongodb.com>
2023-12-13 14:40:42 -0500

    Upgrade constraintlayout to 2.1.4 - tests build and work

--- examples/kotlin/androidRealmKMMApp/build.gradle.kts
+++ examples/kotlin/androidRealmKMMApp/build.gradle.kts
@@ -24,6 +24,6 @@ dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     compileOnly(libs.realm.sync)
 }
