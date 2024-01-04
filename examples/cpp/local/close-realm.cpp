c37200e8578c5e615d214cdb73c51e917535c065
Dachary Carey <dachary.carey@mongodb.com>
2024-01-04 14:05:55 -0500

    Move close realm example to local test suite

--- examples/cpp/local/close-realm.cpp
+++ examples/cpp/local/close-realm.cpp
@@ -1,14 +1,15 @@
 #include <catch2/catch_test_macros.hpp>
-#include <cpprealm/experimental/sdk.hpp>
 #include <cpprealm/sdk.hpp>
 
-using namespace realm::experimental;
+namespace realm {
 
 struct Dog {
   std::string name;
   int64_t age;
 };
 REALM_SCHEMA(Dog, name, age)
+
+}  // namespace realm
 
 TEST_CASE("Close a realm example", "[write]") {
   auto relative_realm_path_directory = "open-close-realm/";
@@ -21,15 +22,15 @@ TEST_CASE("Close a realm example", "[write]") {
   // Create a database configuration.
   auto config = realm::db_config();
   config.set_path(path);  // :remove:
-  auto realm = db(config);
+  auto realm = realm::db(config);
 
   // Use the database...
   // :remove-start:
-  auto dog = Dog{.name = "Maui", .age = 3};
+  auto dog = realm::Dog{.name = "Maui", .age = 3};
 
   realm.write([&] { realm.add(std::move(dog)); });
 
-  auto managedDogs = realm.objects<Dog>();
+  auto managedDogs = realm.objects<realm::Dog>();
   auto specificDog = managedDogs[0];
   REQUIRE(specificDog.name == "Maui");
   REQUIRE(specificDog.age == static_cast<long long>(3));
@@ -48,14 +49,14 @@ TEST_CASE("Close a realm example", "[write]") {
   CHECK(specificDog.is_invalidated());
   // :snippet-end:
 
-  auto newDBInstance = db(config);
-  auto sameDogsNewInstance = newDBInstance.objects<Dog>();
+  auto newDBInstance = realm::db(config);
+  auto sameDogsNewInstance = newDBInstance.objects<realm::Dog>();
   auto anotherSpecificDog = sameDogsNewInstance[0];
   REQUIRE(anotherSpecificDog.name == "Maui");
   REQUIRE(sameDogsNewInstance.size() == 1);
 
   newDBInstance.write([&] { newDBInstance.remove(anotherSpecificDog); });
 
-  auto managedDogsAfterDelete = newDBInstance.objects<Dog>();
+  auto managedDogsAfterDelete = newDBInstance.objects<realm::Dog>();
   REQUIRE(managedDogsAfterDelete.size() == 0);
 }
