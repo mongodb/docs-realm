e3bd445ca1a536254eb8fd6c5777b5548eb7a31b
Dachary <dachary.carey@mongodb.com>
2024-01-10 17:07:05 -0500

    (DOCSP-35394): Web SDK: Upgrade dependencies and fix failing unit test (#3152)
    ## Pull Request Info
    
    Jira ticket: https://jira.mongodb.org/browse/DOCSP-35394
    
    ### Release Notes
    
    - **Web SDK**
      - Internal: Upgrade test suite dependencies and fix failing test.
    
    ### Review Guidelines
    
    
    [REVIEWING.md](https://github.com/mongodb/docs-realm/blob/master/REVIEWING.md)

--- examples/web/src/setupTests.ts
+++ examples/web/src/setupTests.ts
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from "util";
+global.TextEncoder = TextEncoder;
+(global as any).TextDecoder = TextDecoder;
