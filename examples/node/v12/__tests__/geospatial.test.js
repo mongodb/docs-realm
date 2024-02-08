85d880f43d361a1876f826c22d3e277b5fb02681
Lindsey Moore <lindsey.moore@mongodb.com>
2024-02-08 13:33:11 -0500

    revert formatting

--- examples/node/v12/__tests__/geospatial.test.js
+++ examples/node/v12/__tests__/geospatial.test.js
@@ -173,9 +173,7 @@ describe("Geospatial", () => {
 
     const companiesInLargeCircle = realm
       .objects(Company)
-      .filtered(
-        "location geoWithin $0", largeCircle
-        );
+      .filtered("location geoWithin $0", largeCircle);
     console.debug(`Companies in largeCircle: ${companiesInLargeCircle.length}`);
     // :snippet-end:
     // :snippet-start: geobox-query
