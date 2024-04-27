9478fcb9a4e003c9281c6b117b75e27053e4a376
Lindsey Moore <lindsey.moore@mongodb.com>
2024-04-26 20:53:03 -0400

    fix code copy

--- examples/dart/test/serialization_test.dart
+++ examples/dart/test/serialization_test.dart
@@ -27,7 +27,7 @@ main() {
   // Serialize to ejson by passing the object as a paramter to the method or ...
   EJsonValue serializeByParam = toEJson(spider);
 
-  // ... calling the method directly on the object
+  // ... by calling the method directly on the object
   EJsonValue serializeWithCall = spider.toEJson();
   // :snippet-end:
 
