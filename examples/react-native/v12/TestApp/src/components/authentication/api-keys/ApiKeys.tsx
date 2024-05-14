344fe1c3138e51caae44631c6b305e7e1d33737b
krollins-mdb <kyle.rollins@mongodb.com>
2024-05-14 12:18:57 -0500

    Initial refactor

--- examples/react-native/v12/TestApp/src/components/authentication/api-keys/ApiKeys.tsx
+++ examples/react-native/v12/TestApp/src/components/authentication/api-keys/ApiKeys.tsx
@@ -1,105 +1,139 @@
-import React, { useEffect } from 'react';
-import {AppProvider, AuthOperationName, UserProvider, useEmailPasswordAuth, useUser} from '@realm/react';
-import {useApp} from '@realm/react';
-import {useAuth} from '@realm/react';
-import {Text, View, Pressable, StyleSheet, Button, FlatList} from 'react-native';
-import {APP_ID} from '../../../../appServicesConfig';
-import { Credentials } from 'realm';
+import React, {useEffect, useState} from 'react';
+import {
+  Text,
+  View,
+  Pressable,
+  StyleSheet,
+  Button,
+  FlatList,
+} from 'react-native';
+import {
+  AppProvider,
+  AuthOperationName,
+  UserProvider,
+  useEmailPasswordAuth,
+  useUser,
+  useApp,
+  useAuth,
+} from '@realm/react';
 
-const userInputs = {
-  email: 'test-rn-api-key@example.com',
-  password: 'abc123',
-};
-const credentials = Credentials.emailPassword(userInputs.email, userInputs.password);
+import {APP_ID} from '../../../../appServicesConfig';
 
 export const ApiKeyAuth = () => {
-    return (
-      <AppProvider id={APP_ID}>
-        <InitialLogIn />
-          <UserProvider>
-            <ManageAPIKeys />
-            <APIKeyLogIn />
-              <DoAppThings />
-          </UserProvider>
-      </AppProvider>
-    );
+  const [apiKeyKey, setApiKeyKey] = useState('');
+  const testUser = {
+    email: 'test-rn-api-key@example.com',
+    password: 'abc123',
+  };
+
+  return (
+    <AppProvider id={APP_ID}>
+      <InitialLogin
+        email={testUser.email}
+        password={testUser.password}
+      />
+      <UserProvider>
+        <ManageApiKeys setApiKeyKey={setApiKeyKey} />
+        <ApiKeyLogin apiKey={apiKeyKey} />
+        <DoAppThings />
+      </UserProvider>
+    </AppProvider>
+  );
 };
 
-let userApiKey: Realm.Auth.ApiKey;
-let apiKeyKey = "";
+interface userInfo {
+  email: string;
+  password: string;
+}
 
 // You can only create API keys for a user who has already
 // logged in. Perform initial login with another auth type.
-const InitialLogIn = () => {
+const InitialLogin = ({email, password}: userInfo) => {
   const {logOut, result, logIn} = useEmailPasswordAuth();
 
   // Log in the user after successful registration
   useEffect(() => {
     if (result.success && result.operation === AuthOperationName.Register) {
-      logIn({email: userInputs.email, password: userInputs.password});
+      logIn({email: email, password: password});
     }
-  }, [result, logIn, userInputs.email, userInputs.password]);
+  }, [result, logIn, email, password]);
 
   // const performRegistration = () => {
   //   register({email: userInputs.email, password: userInputs.password});
   // };
 
   const loginEmailPasswordUser = () => {
-    logIn({email: userInputs.email, password: userInputs.password});
+    logIn({email: email, password: password});
   };
 
   return (
     <View>
-      <Text>First, log out existing user and log in with email and password</Text>
-      <Button onPress={logOut} title='Logout Existing User' />
-      <Button onPress={loginEmailPasswordUser} title='Log in email/password user' />
+      <Text>
+        First, log out existing user and log in with email and password
+      </Text>
+      <Button
+        onPress={logOut}
+        title="Logout Existing User"
+      />
+      <Button
+        onPress={loginEmailPasswordUser}
+        title="Log in email/password user"
+      />
     </View>
   );
 };
 
-const ManageAPIKeys = () => {
+interface ManageApiKeysProps {
+  setApiKeyKey: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const ManageApiKeys = ({setApiKeyKey}: ManageApiKeysProps) => {
   const user = useUser();
 
-  async function createUserAPIKey() {
-    const apiKey = await user?.apiKeys.create('mySecretKey');
+  const [apiKeyId, setApiKeyId] = useState('');
+
+  async function createUserApiKey() {
+    const {_id, key, name, disabled} =
+      await user?.apiKeys.create('mySecretKey');
+
     // ...Do something with API key like save it
     // or share it with external service that authenticates
     // on user's behalf.
     // :remove-start:
-    userApiKey = apiKey!;
-    apiKeyKey = apiKey.key;
+    setApiKeyId(_id);
+    setApiKeyKey(key);
     // :remove-end:
   }
 
-  async function fetchUserAPIKey() {
+  async function fetchUserApiKey() {
     // List all of a user's keys
     const keys = await user.apiKeys.fetchAll();
     // Get a specific key by its ID
-    const key = await user.apiKeys.fetch(userApiKey._id);
+    const key = await user.apiKeys.fetch(apiKeyId);
   }
 
-  async function disableUserAPIKey() {
+  async function disableUserApiKey() {
     // Get the user's API key
     const keys = await user.apiKeys.fetchAll();
-    const keyId = keys[0]["_id"];
+    const keyId = keys[0]['_id'];
 
     // Enable the User API Key
     await user.apiKeys.disable(keyId);
   }
 
-  async function enableUserAPIKey() {
+  async function enableUserApiKey() {
     // Get the user's API key
     const keys = await user.apiKeys.fetchAll();
-    const keyId = keys[0]["_id"];
+    const keyId = keys[0]['_id'];
 
     // Enable the User API Key
     await user.apiKeys.enable(keyId);
   }
 
-  async function deleteUserAPIKey() {
+  async function deleteUserApiKey() {
     // Get the user's API key
     const keys = await user.apiKeys.fetchAll();
-    const keyId = keys[0]["_id"];
+    const keyId = keys[0]['_id'];
 
     // Enable the User API Key
     await user.apiKeys.delete(keyId);
@@ -111,42 +145,61 @@ const ManageAPIKeys = () => {
       <Text>Logged-in user ID is: {user.id}</Text>
       <Text>User's state is {user.state}</Text>
       <View style={styles.buttonGroup}>
-        <Button onPress={createUserAPIKey} title='Create API Key' />
-        <Button onPress={fetchUserAPIKey} title='Fetch API Keys' />
-        <Button onPress={disableUserAPIKey} title='Disable API Key' />
-        <Button onPress={enableUserAPIKey} title='Enable API Key' />
-        <Button onPress={deleteUserAPIKey} title='Delete API Key' />
+        <Button
+          onPress={createUserApiKey}
+          title="Create API Key"
+        />
+        <Button
+          onPress={fetchUserApiKey}
+          title="Fetch API Keys"
+        />
+        <Button
+          onPress={disableUserApiKey}
+          title="Disable API Key"
+        />
+        <Button
+          onPress={enableUserApiKey}
+          title="Enable API Key"
+        />
+        <Button
+          onPress={deleteUserApiKey}
+          title="Delete API Key"
+        />
       </View>
     </View>
   );
   // :remove-end:
 };
 
-const APIKeyLogIn = () => {
-  const {logIn, result} = useAuth();
+const ApiKeyLogin = ({apiKey}: {apiKey: string}) => {
+  const {result, logInWithApiKey} = useAuth();
 
   const performLogin = () => {
-    let apiKeyCredential = Realm.Credentials.apiKey(apiKeyKey);
-    logIn(apiKeyCredential);
+    let apiKeyCredential = logInWithApiKey(apiKey);
   };
-  
+
   return (
     <View>
       <Text>The API Key isn't logged in yet.</Text>
-      <Button onPress={performLogin} title='Log in with API Key' />
+      <Button
+        onPress={performLogin}
+        title="Log in with API Key"
+      />
     </View>
   );
 };
 
 function DoAppThings() {
-    const app = useApp();
-    // Proceed to app logic...
-    // :remove-start:
-    return (
-      <Text testID="logged-in-user-id">"Logged in as user with ID: {app.currentUser?.id}"</Text>
-    );
-    // :remove-end:
-  }
+  const app = useApp();
+  // Proceed to app logic...
+  // :remove-start:
+  return (
+    <Text testID="logged-in-user-id">
+      "Logged in as user with ID: {app.currentUser?.id}"
+    </Text>
+  );
+  // :remove-end:
+}
 
 const styles = StyleSheet.create({
   section: {
