ba516743d4cd879c532a4010e314bcbb5beb9580
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-06-04 12:00:05 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-curl https://raw.githubusercontent.com/mongodb/docs-worker-pool/netlify-poc/scripts/build-dev-site.sh -o build-site.sh 
-sh build-site.sh
+curl https://raw.githubusercontent.com/mongodb/docs-worker-pool/netlify-poc/scripts/build-dev-site.sh -o build-dev-site.sh 
+sh build-dev-site.sh
