00542ef401f3c7243c89c7c6484b551a8662bd47
Lindsey Moore <lindsey.moore@mongodb.com>
2024-06-06 15:42:54 -0400

    add typed example for deserialize

--- examples/dart/test/serialization_test.dart
+++ examples/dart/test/serialization_test.dart
@@ -62,6 +62,9 @@ main() {
 
     // :snippet-start: deserialize
     // Pass the serialized object to the method
+    final deserializeFromEjsonWithExplicitType = fromEJson<Pet>(serializeByParam);
+
+    // The method can also infer the object type
     Pet deserializeFromEjson = fromEJson(serializeByParam);
     // :snippet-end: 
 
