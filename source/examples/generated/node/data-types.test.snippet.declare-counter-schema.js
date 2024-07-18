135a8daae703516a6255484611ef932e5cef169b
osharaf-mdb <omar.sharaf@mongodb.com>
2024-07-18 11:15:20 -0400

    applied changes from secound round of reviews

--- source/examples/generated/node/data-types.test.snippet.declare-counter-schema.js
+++ source/examples/generated/node/data-types.test.snippet.declare-counter-schema.js
@@ -8,8 +8,10 @@ class myClass extends Realm.Object {
         primaryKey: "_id",
         properties: {
             _id: { type: "objectId", default: () => new BSON.ObjectId() },
-            myCounter: { type: "int", presentation: "counter" }, // or myCounter: "counter"
-            nullableCounter: { type: "int", presentation: "counter", optional: true }, // or nullableCounter: "counter?" 
+            myCounter: { type: "int", presentation: "counter" }, 
+            // or myCounter: "counter"
+            nullableCounter: { type: "int", presentation: "counter", optional: true }, 
+            // or nullableCounter: "counter?" 
         },
     }
 };
