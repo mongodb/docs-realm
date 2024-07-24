d258a6dce45999d3a1a967f1f456f4b41aea2ba9
krollins-mdb <kyle.rollins@mongodb.com>
2024-07-24 15:41:57 -0500

    - Rename src to source - Add middleware to redirect WASM request to node_modules

--- examples/frameworks/react-test-app/vite.config.ts
+++ examples/frameworks/react-test-app/vite.config.ts
@@ -1,9 +1,34 @@
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { promises as fs } from "fs";
+
+const wasmContentTypePlugin: Plugin = {
+  name: "wasm-content-type-plugin",
+  configureServer(server) {
+    server.middlewares.use(async (req, res, next) => {
+      if (req.url && req.url.endsWith(".wasm")) {
+        res.setHeader("Content-Type", "application/wasm");
+
+        const wasmPath = path.join(
+          __dirname,
+          "node_modules/realm/dist",
+          path.basename(req.url)
+        );
+        const realmWasm = await fs.readFile(wasmPath);
+
+        res.write(realmWasm);
+        res.end();
+      } else {
+        next();
+      }
+    });
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasmContentTypePlugin],
   esbuild: {
     supported: {
       "top-level-await": true,
