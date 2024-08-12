472380064370f855a4352025f9fa584fba42fb9e
MongoCaleb <caleb.thompson@mongodb.com>
2024-08-12 10:26:01 -0700

    Update PR template, small fix to netflify action path, reorganize page structure, update code comments

--- examples/dotnet/Examples/WorkWithRealm.cs
+++ examples/dotnet/Examples/WorkWithRealm.cs
@@ -213,17 +213,20 @@ namespace Examples
 
             // Use one of these equivalent declarations to 
             // specify the fields you want to monitor for changes:
+
             kpc = KeyPathsCollection.Of("Email", "Name");
             kpc = new List<KeyPath> {"Email", "Name"};
             
             // To get all notifications for top-level properties
             // and 4 nested levels of properties, use the `Full` 
             // static value:
+
             kpc = KeyPathsCollection.Full;
 
             // To receive notifications for changes to the
             // collection only and none of the properties,
             // use the `Shallow` static value:
+            
             kpc = KeyPathsCollection.Shallow;
 
             query.SubscribeForNotifications(notificationCallback, kpc);
